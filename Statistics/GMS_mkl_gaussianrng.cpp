ea04a404e439d64afab6ea134b647f9890a9443d
Bernard Gingold <beniekg@gmail.com>
2019-10-16 19:44:24 +0200

    Wrapper for the MKL Gaussian distribution.

---
+++ Statistics/GMS_mkl_gaussianrng.cpp
@@ -0,0 +1,225 @@
+
+#include "GMS_mkl_gaussianrng.h"
+#if defined _WIN64
+     #include "../GMS_common.h"
+     #include "../GMS_malloc.h"
+     #include "../GMS_error_macros.h"
+     #include "../Math/GMS_constants.h"
+#elif defined __linux
+     #include "GMS_common.h"
+     #include "GMS_malloc.h"
+     #include "GMS_error_macros.h"
+     #include "GMS_constants.h"
+#endif
+//
+//	Implementation
+//
+
+gms::math::stat::
+MKLGaussianRNG::MKLGaussianRNG() {
+	using namespace gms::math::constants;
+	m_rvec  = NULL;
+	m_a     = dinf;
+	m_sigma = dinf;
+	m_nvalues = 0;
+	m_brng    = 0;
+	m_seed    = 0;
+	m_error   = 1;
+}
+
+
+gms::math::stat::
+MKLGaussianRNG::
+MKLGaussianRNG(	const MKL_INT nvalues,
+		const MKL_UINT brng,
+		const MKL_INT seed,
+		const double a,
+		const double sigma) {
+	using namespace gms::common;
+#if defined _WIN64
+	m_rvec  = gms_edmalloca(static_cast<size_t>(nvalues), align64B);
+#elif defined __linux
+	m_rvec  = gms_edmalloca(static_cast<size_t>(nvalues), align64B);
+#endif
+	m_a     = a;
+	m_sigma = sigma;
+	m_nvalues = nvalues;
+	m_brng    = brng;
+	m_seed    = seed;
+	m_error   = 1;
+#if defined __AVX512F__
+        avx512_init_unroll8x_pd(&m_rvec[0], static_cast<int64_t>(m_nvalues), 0.0);
+#else
+	avx256_init_unroll8x_pd(&m_rvec[0], static_cast<int64_t>(m_nvalues), 0.0);
+#endif
+}
+
+
+gms::math::stat::
+MKLGaussianRNG::
+MKLGaussianRNG(const MKLGaussianRNG &x) {
+	using namespace gms::common;
+#if defined _WIN64
+	m_rvec    = gms_edmalloca(static_cast<size_t>(x.m_nvalues), align64B);
+#elif defined __linux
+	m_rvec    = gms_edmalloca(static_cast<size_t>(x.m_nvalues), align64B);
+#endif
+	m_a       = x.m_a;
+	m_sigma   = x.m_sigma;
+	m_nvalues = x.m_nvalues;
+	m_brng    = x.m_brng;
+	m_seed    = x.m_seed;
+	m_error   = x.m_error;
+#if defined __AVX512F__
+     #if (USE_NT_STORES) == 1
+	 avx512_memcpy8x_nt_pd(&m_rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+     #else
+	 avx512_memcpy8x_pd(&m_rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+     #endif
+#else
+     #if (USE_NT_STORES) == 1
+	avx256_memcpy8x_nt_pd(&m_rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+     #else
+	avx256_memcpy8x_pd(&m_rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+     #endif
+#endif
+}
+
+
+gms::math::stat::
+MKLGaussianRNG::
+MKLGaussianRNG(MKLGaussianRNG &&x) {
+	m_rvec = &x.m_rvec[0];
+	m_a = x.m_a;
+	m_sigma = x.m_sigma;
+	m_nvalues = x.m_nvalues;
+	m_brng = x.m_brng;
+	m_seed = x.m_seed;
+	m_error = x.m_error;
+	x.m_rvec = NULL;
+	x.m_nvalues = 0;
+}
+
+
+gms::math::stat::
+MKLGaussianRNG::
+~MKLGaussianRNG() {
+	if (NULL != m_rvec) _mm_free(m_rvec); m_rvec = NULL;
+}		
+		
+	
+
+
+gms::math::stat::MKLGaussianRNG &
+gms::math::stat::MKLGaussianRNG
+::operator=(const MKLGaussianRNG &x) {
+	using namespace gms::common;
+	if (this == &x) return (*this);
+	_mm_free(m_rvec);
+	m_a       = x.m_a; // Should be present in L1D for small tests.
+	m_sigma   = x.m_sigma;
+	m_nvalues = x.m_nvalues;
+	m_brng    = x.m_brng;
+	m_seed    = x.m_seed;
+	m_error   = x.m_error;
+#if defined _WIN64
+	double * __restrict rvec{ gms_edmalloca(static_cast<size_t>(m_nvalues), align64B) };
+#elif defined __linux
+	double * __restrict rvec{gms_edmalloca(static_cast<size_t>(m_nvalues), align64B) };
+#endif
+#if defined __AVX512F__
+     #if (USE_NT_STORES) == 1
+	    avx512_memcpy8x_nt_pd(&rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+     #else
+	    avx512_memcpy8x_pd(&rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+     #endif
+#else
+     #if (USE_NT_STORES) == 1
+	    avx256_memcpy8x_nt_pd(&rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+     #else
+	    avx256_memcpy8x_pd(&rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+     #endif
+#endif
+	m_rvec = &rvec[0];
+	return (*this);
+}	
+	
+
+
+gms::math::stat::MKLGaussianRNG &
+gms::math::stat::MKLGaussianRNG
+::operator=(MKLGaussianRNG &&x) {
+	if (this == &x) return (*this);
+	_mm_free(m_rvec);
+	m_rvec = &x.m_rvec[0];
+	m_a = x.m_a;
+	m_sigma = x.m_sigma;
+	m_nvalues = x.m_nvalues;
+	m_brng = x.m_brng;
+	m_seed = x.m_seed;
+	m_error = x.m_error;
+	x.m_nvalues = 0;
+	x.m_rvec = NULL;
+	return (*this);
+}	
+	
+
+
+void
+gms::math::stat::MKLGaussianRNG::
+compute_rand_distribution(const MKL_INT method){
+	VSLStreamStatePtr streamptr;
+	m_error = vslNewStream(&streamptr, m_brng, m_seed);
+	if (VSL_ERROR_OK != m_error) {
+		PRINT_ERROR_VALUE("vslNewStream -- failed with an error value: ", m_error)
+		DUMP_CALLSTACK_ON_ERROR
+		return;
+	}
+	m_error = vdRngGaussian(method, streamptr, m_nvalues, &m_rvec[0],m_a,m_sigma);
+	if (VSL_ERROR_OK != m_error) {
+		PRINT_ERROR_VALUE("vdRngGaussian -- failed with an error value: ", m_error)
+		DUMP_CALLSTACK_ON_ERROR
+		return;
+	}
+	m_error = vslDeleteStream(&streamptr);
+	if (VSL_ERROR_OK != m_error) {
+		PRINT_ERROR_VALUE("vslDeleteStream -- failed with an error value: ", m_error)
+		DUMP_CALLSTACK_ON_ERROR
+	    return;
+	}
+}
+
+void
+gms::math::stat::MKLGaussianRNG::
+compute_rand_distribution(VSLStreamStatePtr stream,
+			   const MKL_INT method) {
+	// VSLStreamStatePtr must deallocated by the caller of compute_rand_distribution.
+	m_error = vslNewStream(&stream,m_brng,m_seed);
+	if (VSL_ERROR_OK != m_error) {
+		PRINT_ERROR_VALUE("vslNewStream -- failed with an error value: ", m_error)
+		DUMP_CALLSTACK_ON_ERROR
+		return;
+	}
+	m_error = vdRngGaussian(method, stream, m_nvalues, &m_rvec[0],m_a,m_sigma);
+	if (VSL_ERROR_OK != m_error) {
+		PRINT_ERROR_VALUE("vdRngGaussian -- failed with an error value: ", m_error)
+		DUMP_CALLSTACK_ON_ERROR
+		return;
+	}
+	// stream is deleted by the caller.
+}
+
+std::ostream &
+gms::math::stat::
+operator<<(std::ostream &os,
+	   const MKLGaussianRNG &x) {
+	
+	for (MKL_INT i = 0; i != x.m_nvalues; ++i) {
+		os << std::setprecision(15) << std::showpoint <<
+			"Randomly gaussian distributed: m_rvec = " << x.m_rvec[i] << std::endl;
+	}
+	return (os);
+}		
+
+
+
---
+++ Statistics/GMS_mkl_gaussianrng.h
@@ -0,0 +1,125 @@
+
+#ifndef __GMS_MKL_GAUSSIANRNG_H__
+#define __GMS_MKL_GAUSSIANRNG_H__
+
+namespace file_info {
+
+#if defined _WIN64
+    #include "../GMS_version.h"
+#elif defined __linux
+    #include "GMS_version.h"
+#endif
+
+const unsigned int gGMS_MKL_GAUSSIANRNG_MAJOR = gms::common::gVersionInfo.m_VersionMajor;
+
+const unsigned int gGMS_MKL_GAUSSIANRNG_MINOR = gms::common::gVersionInfo.m_VersionMinor;
+
+const unsigned int gGMS_MKL_GAUSSIANRNG_MICRO = gms::common::gVersionInfo.m_VersionMicro;
+
+const unsigned int gGMS_MKL_GAUSSIANRNG_FULLVER = 
+	1000U*gGMS_MKL_GAUSSIANRNG_MAJOR + 100U*gGMS_MKL_GAUSSIANRNG_MINOR + 10U*gGMS_MKL_GAUSSIANRNG_MICRO;
+
+const char * const pgGMS_MKL_GAUSSIANRNG_CREATE_DATE = "22-04-2018 12:16 +00200 (SUN 22 APR 2018 GMT+2)";
+
+const char * const pgGMS_MKL_GAUSSIANRNG_BUILD_DATE = "00-00-0000 00:00";
+
+const char * const pgGMS_MKL_GAUSSIANRNG_AUTHOR = "Programmer: Bernard Gingold, e-mail: beniekg@gmail.com";
+
+const char * const pgGMS_MKL_GAUSSIANRNG_DESCRIPT = "C++ wrappers around Intel MKL random number generator procedures.";
+
+}
+
+
+#include <iostream>
+#if defined _WIN64
+    #include "../GMS_config.h"
+#elif defined __linux
+    #include "GMS_config.h"
+#endif
+#if (USE_MKL) == 1
+#include <mkl_vsl.h>
+#endif
+
+namespace gms {
+	namespace math {
+		namespace stat {
+			
+				// Wrapper around MKL vsrnggaussian( method, stream, n, r, a, sigma )
+#if defined _WIN64
+			__declspec(align(64))struct MKLGaussianRNG{
+#elif defined __linux
+			__attribute__((align(64))) struct MKLGaussianRNG{
+#endif
+#if defined _WIN64			  
+				    _Field_size_(m_nvalues) double * __restrict m_rvec;
+#elif defined __linux
+			                double * __restrict m_rvec;
+#endif
+					double m_a;
+
+					double m_sigma;
+
+#if (USE_STRUCT_PADDING) == 1
+					PAD_TO(1,8)
+#endif
+
+					MKL_INT m_nvalues;
+#if (USE_STRUCT_PADDING) == 1
+					PAD_TO(2,4)
+#endif
+					MKL_UINT m_brng;
+#if (USE_STRUCT_PADDING) == 1
+					PAD_TO(3,4)
+#endif
+					MKL_INT  m_seed;
+#if (USE_STRUCT_PADDING) == 1
+					PAD_TO(4,4)
+#endif
+					MKL_INT  m_error;
+#if (USE_STRUCT_PADDING) == 1
+					PAD_TO(5,4)
+#endif
+					
+					
+					
+
+					//
+					//	Construction and Destruction
+					//
+					MKLGaussianRNG();
+
+					MKLGaussianRNG(const MKL_INT, 
+						       const MKL_UINT,
+						       const MKL_INT,
+						       const double,
+						       const double );
+
+					MKLGaussianRNG(const MKLGaussianRNG &);
+
+					MKLGaussianRNG(MKLGaussianRNG &&) noexcept(true);
+
+					~MKLGaussianRNG() noexcept(true);
+
+					MKLGaussianRNG & operator=(const MKLGaussianRNG &);
+
+					MKLGaussianRNG & operator=(const MKLGaussianRNG & );
+
+					MKLGaussianRNG & operator=(MKLGaussianRNG &&)noexcept(true);
+
+					void compute_rand_distribution(const MKL_INT); // Should be an interface in case of static polymorphism.
+
+					void compute_rand_distribution(VSLStreamStatePtr, const MKL_INT);
+			};
+
+			std::ostream & operator<<(std::ostream &,
+						   const MKLGaussianRNG &);
+
+
+
+
+		}
+	}
+}
+
+
+#endif /*__GMS_MKL_RANDOM_H__*/
