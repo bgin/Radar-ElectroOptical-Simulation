cf736cfac205a707f3ea7bd20d86b14056bdd06b
Bernard Gingold <beniekg@gmail.com>
2019-12-28 19:10:01 +0100

    Adding a compilation path for GCC (automatic vectorization)

--- Math/GMS_tree_scatterer.cpp
+++ Math/GMS_tree_scatterer.cpp
@@ -91,14 +91,14 @@ TreeScatterer::TreeScatterer() {
       m_tsc.leaves_thick        = gms_avxvec8_emalloca(static_cast<size_t>(m_tsc.nleaves),64);                          \
       m_tsc.leaves_dens         = gms_avxvec8_emalloca(static_cast<size_t>(m_tsc.nleaves),64);                          \
       m_tsc.leaves_incang       = gms_avxvec8_emalloca(static_cast<size_t>(2*m_tsc.nleaves),64);                        \
-      m_tsc.leaves_xparam       = gms_avxvec8_emalloca(static_cast<size_t>(m_tsc.leaves_param_npts*m_tsc.nleaves),64);  \
-      m_tsc.leaves_yparam       = gms_avxvec8_emalloca(static_cast<size_t>(m_tsc.leaves_param_npts*m_tsc.nleaves),64);  \
+      m_tsc.leaves_xparam       = gms_avxvec8_emalloca(static_cast<size_t>(m_tsc.nleaves*m_tsc.leaves_param_npts),64);  \
+      m_tsc.leaves_yparam       = gms_avxvec8_emalloca(static_cast<size_t>(m_tsc.nleaves*m_tsc.leaves_param_npts),64);  \
       m_tsc.branches_thick      = gms_avxvec8_emalloca(static_cast<size_t>(m_tsc.nbranches),64);                        \
       m_tsc.branches_dens       = gms_avxvec8_emalloca(static_cast<size_t>(m_tsc.nbranches),64);                        \
       m_tsc.branches_incang     = gms_avxvec8_emalloca(static_cast<size_t>(2*m_tsc.nbranches),64);                      \
-      m_tsc.branches_xparam     = gms_avxvec8_emalloca(static_cast<size_t>(m_tsc.branches_param_npts*m_tsc.nbranches),64);   \
-      m_tsc.branches_yparam     = gms_avxvec8_emalloca(static_cast<size_t>(m_tsc.branches_param_npts*m_tsc.nbranches),64);   \
-      m_tsc.branches_zparam     = gms_avxvec8_emalloca(static_cast<size_t>(m_tsc.branches_param_npts*m_tsc.nbranches),64);         
+      m_tsc.branches_xparam     = gms_avxvec8_emalloca(static_cast<size_t>(m_tsc.nbranches*m_tsc.branches_param_npts),64);   \
+      m_tsc.branches_yparam     = gms_avxvec8_emalloca(static_cast<size_t>(m_tsc.nbranches*m_tsc.branches_param_npts),64);   \
+      m_tsc.branches_zparam     = gms_avxvec8_emalloca(static_cast<size_t>(m_tsc.nbranches*m_tsc.branches_param_npts),64);         
 #endif
 
 #if !defined(GMS_TREE_SCATTERER_HOT_ALLOC_CTOR)
@@ -431,7 +431,7 @@ SetThicknessDensAng_ymm8r4(const AVXVec8 * __restrict bradii) {
     gms::common::avxvec8_init_unroll8x(&m_tsc.leaves_dens[0],
                                        static_cast<int64_t>(m_tsc.nleaves),
 				       AVXVec8{});
-    result = _rdrand_32_step(&seed)
+    result = _rdrand32_step(&seed)
     if(!result) seed = 1458963254U;
     engine = svrng_new_mt19937_engine(seed);
     uniform1 = svrng_new_uniform_distribution_float(0.1f,0.7f);
@@ -508,24 +508,13 @@ SetThicknessDensAng_ymm8r4(const AVXVec8 * __restrict bradii) {
       float * __restrict __ATTR_ALIGN__(64) pbincang = NULL;
       const int32_t leaves_len = 8*m_tsc.nleaves;
       const int32_t branch_len = 8*m_tsc.nbranches;
-      plthick  = _mm_malloc(static_cast<size_t>(leaves_len)*sizeof(float),64);
-      if(plthick == NULL && leaves_len != 0) {
-         std::exit(EXIT_FAILURE);
-      }
-      pldense = _mm_malloc(static_cast<size_t>(leaves_len)*sizeof(float),64);
-      if(pldense == NULL && leaves_len != 0) {
-         std::exit(EXIT_FAILURE);
-      }
-      plincang = _mm_malloc(static_cast<size_t>(2*8*m_tsc.nleaves)*sizeof(float),64);
-      if(plincang == NULL) {
-         std::exit(EXIT_FAILURE);
-      }
+     
+      plthick  = gms::common::gms_efmalloca(static_cast<size_t>(leaves_len),64);
+      pldense  = gms::common::gms_efmalloca(static_cast<size_t>(leaves_len),64);
+      plincang = gms::common::gms_efmalloca(static_cast<size_t>(2*leaves_len),64);
+      pbincang = gms::common::gms_efmalloca(static_cast<size_t>(2*branch_len),64);
       pbincang = _mm_malloc(static_cast<size_t>(2*8*m_tsc.nbranches)*sizeof(float),64);
-      if(pbincang == NULL) {
-         std::exit(EXIT_FAILURE);
-      }
-      //
-      //
+      
       std::clock_t seed;
       seed = std::clock();
       auto srand1 = std::bind(std::uniform_real_distribution<float>(0.1f,0.7f),
@@ -628,9 +617,16 @@ SetThicknessDensAng_ymm8r4(const AVXVec8 * __restrict bradii) {
 
 void
 gms::math::TreeScatterer::
-ComputeLeavesParamEq_ymm8r4(const AVXVec8 va,
-                            const AVXVec8 vb) {
-
+ComputeLeavesParamEq_ymm8r4(
+#if defined __ICC || defined __INTEL_COMPILER
+const AVXVec8 va,
+const AVXVec8 vb
+#elif defined __GNUC__ && defined __INTEL_COMPILER
+const float a,
+const float b
+#endif
+) {
+#if defined __ICC || defined __INTEL_COMPILER
      struct _T0_ {
        AVXVec8 vthinc0;
        AVXVec8 vthinc1;
@@ -651,13 +647,355 @@ ComputeLeavesParamEq_ymm8r4(const AVXVec8 va,
        AVXVec8 vC;
        AVXVec8 tmp;
      } __ATTR_ALIGN__(64) t2;
-#if defined __ICC || defined __INTEL_COMPILER
-     svrng_float8_t vrand1, vrand2;
-     svrng_engine_t engine;
-     svrng_distribution_t uniform1, uniform2;
-     uint32_t seed;
-     int32_t result;
+
+     struct _T3_ {
+       AVXVec8 tva;
+       AVXVec8 tvb;
+     } __ATTR_ALIGN__(64) t3;
+     AVXVec8 vNPTS;
+     const int64_t xyparam_len = static_cast<int64_t>(m_tsc.nleaves*m_tsc.leaves_param_npts);
+     
+
+     svrng_float8_t vrandx, vrandy;
+     svrng_engine_t enginex,enginey;
+     svrng_distribution_t uniformx, uniformy;
+     uint32_t seedx,seedy;
+     int32_t resultx,resulty;
      // Locals first memory-touch
-     t0.vthinc0 = ZERO;
-     t0.vthinc1 = ZERO;
+     t0.vthinc0  = ZERO;
+     t0.vthinc1  = ZERO;
+     t0.vthinc2  = ZERO;
+     t0.vthinc3  = ZERO;
+     t1.vtheta0  = ZERO;
+     t1.vtheta1  = ZERO;
+     t1.vtheta2  = ZERO;
+     t1.vtheta3  = ZERO;
+     t2.vsqrt    = ZERO;
+     t2.vsqrtarg = ZERO;
+     t2.vC       = ZERO;
+     t2.tmp      = ZERO;
+     vNPTS       = ZERO;
+     t3.tva      = va;
+     t3.tvb      = vb;
+     // Memory first touch
+     gms::common::avxvec8_init_unroll8x(&m_tsc.leaves_xparam[0],
+                                        xyparam_len,
+					ZERO);
+     gms::common::avxvec8_init_unroll8x(&m_tsc.leaves_yparam[0],
+                                        xyparam_len,
+     					ZERO);
+     vNPTS = AVXVec8{static_cast<float>(m_tsc.leaves_param_npts)};
+     for(int32_t i = 0; i != m_tsc.nleaves; ++i) {
+           // loop over leaves
+           seedx = 0U;
+           resultx = -9999;
+           result = _rdrand32_step(&seedx);
+           if(!resultx) seedx = 195647856U;
+           enginex = svrng_new_mt19937_engine(seedx);
+	   unformx = svrng_new_uniform_distribution_float(0.1f,1.0f);
+	   seedy = 0U;
+	   resulty = -9999;
+	   if(!resulty) seedy = 126588965U;
+	   enginey     = svrng_new_mt19937_engine(seedy);
+	   uniformy    = svrng_new_uniform_distribution_float(0.1f,1.0f);
+	   vrandx      = svrng_generate8_float(enginex,uniformx);
+	   t3.tva      = t3.tva + *(AVXVec8*)&vrandx;
+	   vrandy      = svrng_generate8_float(enginey,uniformy);
+	   t3.tvb      = t3.tvb + *(AVXVec8*)&vrandy;
+	   t2.vsqrtarg = TWO*(t3.tva*t3.tva+t3.tvb*t3.tvb);
+	   t2.vsqrt    = sqrt(t2.vsqrt);
+	   t2.vC       = PI*t2.vsqrt;
+	   t2.tmp      = t2.vC/vNPTS;
+	   t0.vthinc0  = t2.tmp;
+	   t0.vthinc0  += VINC0;
+	   t0.vthinc1  = t2.tmp;
+	   t0.vthinc1  += VINC1;
+	   t0.vthinc2  = t2.tmp;
+	   t0.vthinc2  += VINC2;
+	   t0.vthinc3  = t2.tmp;
+	   t0.vthinc3  += VINC3;
+	   t1.vtheta0  = ZERO;
+	   t1.vtheta1  = ZERO;
+	   t1.vtheta2  = ZERO;
+	   t1.vtheta3  = ZERO;
+           __assume_aligned(m_tsc.leaves_xparam,64);
+	   __assume_aligned(m_tsc.leaves_yparam,64);
+#pragma vector always
+#pragma vector vectorlength(8)
+	   for(int32_t j = 0; j != m_tsc.leaves_param_npts-3; i += 4) {
+               // for each leaf -- do ...
+	       t1.vtheta0  = t1.vtheta0+t0.vthinc0;
+	       m_tsc.leaves_xparam[Ix2D(i,m_tsc.leaves_param_npts,j+0)] = t3.tva*cos(t1.vtheta0);
+	       m_tsc.leaves_yparam[Ix2D(i,m_tsc.leaves_param_npts,j+0)] = t3.tvb*sin(t1.vtheta0);
+	       t1.vtheta1  = t1.vtheta1+t0.vthinc1;
+	       m_tsc.leaves_xparam[Ix2D(i,m_tsc.leaves_param_npts,j+1)] = t3.tva*cos(t1.vtheta1);
+	       m_tsc.leaves_yparam[Ix2D(i,m_tsc.leaves_param_npts,j+1)] = t3.tvb*sin(t1.vtheta1);
+	       t1.vtheta2  = t1.vtheta2+t0.vthinc2
+	       m_tsc.leaves_xparam[Ix2D(i,m_tsc.leaves_param_npts,j+2)] = t3.tva*cos(t1.vtheta2);
+	       m_tsc.leaves_yparam[Ix2D(i,m_tsc.leaves_param_npts,j+2)] = t3.tvb*sin(t1.vtheta2);
+	       t1.vtheta3  = t1.vtheta3+t0.vthinc3;
+	       m_tsc.leaves_xparam[Ix2D(i,m_tsc.leaves_param_npts,j+3)] = t3.tva*cos(t1.vtheta3);
+	       m_tsc.leaves_yparam[Ix2D(i,m_tsc.leaves_param_npts,j+3)] = t3.tva*sin(t1.vtheta3);
+	   }
+     }
+#elif defined __GNUC__ && !defined __INTEL_COMPILER
+       // Scalar version for GCC compiler
+      
+         float thinc0;
+         float theta0;
+         float sqr;
+	 float sqrarg;
+	 float C;
+	 float tmp;
+         // 
+         float ta;
+	 float tb;
+	 float inc;
+	
+      
+    
+       const int64_t xyparamlen = static_cast<int64_t>(m_tsc.nleaves*8*m_tsc.leaves_param_npts);
+       const int32_t xynpts     = 8*m_tsc.leaves_param_npts;
+       const int32_t xyflatlen  = m_tsc.nleaves*m_tsc.leaves_param_npts;
+       float * __restrict __ATTR_ALIGN__(64) pxparam = NULL;
+       float * __restrict __ATTR_ALIGN__(64) pyparam = NULL;
+       std::clock_t seedx,seedy;
+       float nPTS;
+       // Locals first memory-touch
+       thinc0 = 0.0f;
+       theta0 = 0.0f;
+       sqr    = 0.0f;
+       sqrarg = 0.0f;
+       C      = 0.0f;
+       tmp    = 0.0f;
+       ta     = a;
+       tb     = b;
+       inc    = 1.0f;
+       //
+       nPTS      = 0.0f;
+       pxparam   = gms::common::gms_efmalloca(static_cast<size_t>(xyparamlen),64);
+       pyparam   = gms::common::gms_efmalloca(static_cast<size_t>(xyparamlen),64);
+       // Memory first touch
+       avx256_init_unroll4x_ps(&pxparam[0],
+                               xyparamlen,
+			       0.0f);
+       avx256_init_unroll4x_ps(&pyparam[0],
+                               xyparamlen,
+			       0.0f);
+       nPTS = static_cast<float>(xynpts);
+       for(int32_t i = 0; i != m_tsc.nleaves; ++i) {
+
+           seedx = std::clock();
+	   auto    srandx = std::bind(std::uniform_real_distribution<float>(0.1f,1.0f),
+	                              std::mt19937(seedx);
+	   seedy = std::clock();
+	   auto    srandy = std::bind(std::uniform_real_distribution<float>(0.1f.1.0f),
+	                              std::mt19937(seedy);
+	   ta     += srandx();
+	   tb     += srandy();
+	   sqrarg =  2.0f*(ta*ta+tb*tb);
+	   sqr    =  sqrt(sqrarg);
+	   C      =  3.141592653589793f*sqr;
+	   tmp    =  C/nPTS;
+	   thinc0 =  tmp;
+	   thinc0 += inc;
+	   theta0 = 0.0f;
+#pragma omp simd aligned(pxparam,pyparam:64)
+	   for(int32_t j = 0; j != xynpts; ++j) {
+               theta0 += thinc0;
+	       pxparam[Ix2D(i,xynpts,j)] = ta*cos(theta0);
+	       pyparam[Ix2D(i,xynpts,j)] = tb*sin(theta0);
+	   }
+       }
+       // Memory first touch
+       gms::common::avxvec8_init_unroll8x(&m_tsc.leaves_xparam[0],
+                                        xyparam_len,
+					ZERO);
+       gms::common::avxvec8_init_unroll8x(&m_tsc.leaves_yparam[0],
+                                        xyparam_len,
+     					ZERO);
+       // Copy results back to member arrays.
+       for(int32_t i = 0; i != xyflatlen; ++i) {
+#pragma omp simd aligned(m_tsc.leaves_xparam,pxparam,m_tsc.leaves_yparam,pyparam:64)
+            for(int32_t j = 0; j != 8; ++j) {
+                m_tsc.leaves_xparam[i].m256_f32[j] = pxparam[i*8+j];
+		m_tsc.leaves_yparam[i].m256_f32[j] = pyparam[i*8+j];
+	    }
+       }
+
+       _mm_free(pxparam);
+       pxparam = NULL;
+       _mm_free(pyparam);
+       pyparam = NULL;
+#endif
+     
+}
+
+
+void
+gms::math::TreeScatterer::
+ComputeBranchesParamEq_ymm8r4(const AVXVec8 vrad,
+                              const AVXVec8 vz,
+			      int32_t nzpts) {
+#if defined __ICC || defined __INTEL_COMPILER
+    
+     struct _T0_ {
+       AVXVec8 vtheta0;
+       AVXVec8 vtheta1;
+       AVXVec8 vtheta2;
+       AVXVec8 vtheta3;
+     } __ATTR_ALIGN__(64) t0;
+
+     struct _T1_ {
+       AVXVec8 vthinc0;
+       AVXVec8 vthinc1;
+       AVXVec8 vthinc2;
+       AVXVec8 vthinc3;
+     } __ATTR_ALIGN__(64) t1;
+
+     struct _T2_ {
+       AVXVec8 vhinc0;
+       AVXVec8 vhinc1;
+       AVXVec8 vhinc2;
+       AVXVec8 vhinc3;
+     } __ATTR_ALIGN__(64) t2;
+
+     struct _T3_ {
+       AVXVec8 vhinit0;
+       AVXVec8 vhinit1;
+       AVXVec8 vhinit2;
+       AVXVec8 vhinit3;
+     } __ATTR_ALIGN__(64) t3;
+
+     struct _T4_ {
+       AVXVec8 vNPTS;
+       AVXVec8 vNZPTS;
+       AVXVec8 tmp1;
+       AVXVec8 tmp2;
+     } __ATTR_ALIGN__(64) t4;
+
+     struct _T5_ {
+       AVXVec8 tvrad;
+       AVXVec8 tvz;
+     } __ATTR_ALIGN__(64) t5;
+     const int64_t xyznpts = static_cast<int64_t>(m_tsc.nbranches*m_tsc.branches_param_npts);
+     svrng_float8_t vzrand,vrrand;
+     svrng_engine_t enginer,enginez;
+     svrng_distribution_t uniformr, uniformz;
+     uint32_t seedr,seedz;
+     int32_t resultr,resultz;
+     //Locals first-touch
+     t0.vtheta0 = ZERO;
+     t0.vtheta1 = ZERO;
+     t0.vtheta2 = ZERO;
+     t0.vtheta3 = ZERO;
+     t1.vthinc0 = ZERO;
+     t1.vthinc1 = ZERO;
+     t1.vthinc2 = ZERO;
+     t1.vthinc3 = ZERO;
+     t2.vhinc0  = ZERO;
+     t2.vhinc1  = ZERO;
+     t2.vhinc2  = ZERO;
+     t2.vhinc3  = ZERO;
+     t3.vhinit0 = ZERO;
+     t3.vhinit1 = ZERO;
+     t3.vhinit2 = ZERO;
+     t3.vhinit3 = ZERO;
+     t4.vNPTS   = ZERO;
+     t4.vNZPTS  = ZERO;
+     t4.tmp1    = ZERO;
+     t4.tmp2    = ZERO;
+     t4.vNPTS   = AVXVec8{static_cast<float>(m_tsc.branches_param_npts)};
+     t4.vNZPTS  = AVXVec8{static_cast<float>(m_tsc.branches_param_npts+nzpts)};
+     t4.tmp1    = PI/t4.vNPTS;
+     t5.tvrad   = ZERO;
+     t5.tvz     = ZERO;
+   
+     // Memory first touch
+     gms::common::avxvec8_init_unroll8x(&m_tsc.branches_xparam[0],
+                                       xyznpts,
+				       ZERO);
+     t1.vthinc0 = t4.tmp1;
+     t1.vthinc0 += VINC0;
+     t1.vthinc1 = t4.tmp1;
+     t1.vthinc1 += VINC1;
+     gms::common::avxvec8_init_unroll8x(&m_tsc.branches_yparam[0],
+                                        xyznpts,
+					ZERO);
+     t1.vthinc2 = t4.tmp1;
+     t1.vthinc2 += VINC2;
+     t1.vthinc3 = t4.tmp1;
+     t1.vthinc3 += VINC3;
+     gms::common::avxvec8_init_unroll8x(&m_tsc.branches_zparam[0],
+                                        xyznpts,
+					ZERO);
+     t5.tvrad = vrad;
+     t5.tvz   = vz;
+     for(int32_t i = 0; i != m_tsc.nbranches; ++i) {
+         // Loop over branches -- do...
+         seedr    = 0U;
+	 resultr  = -9999;
+	 resultr  = _rdrand32_step(&seedr);
+	 if(!resultr) seedr = 105685421U;
+	 enginer  = svrng_new_mt19937_engine(seedr);
+	 uniformr = svrng_new_uniform_distribution_float(0.1f,1.0f);
+	 seedz    = 0U;
+	 resultz  = -9999;
+	 resultz  = _rdrand32_step(&seedz);
+	 if(!resultz) seedz = 115654785U;
+	 enginez  = svrng_new_mt19937_engine(seedz);
+	 uniformz = svrng_new_uniform_distribution_float(0.1f,1.0f);
+	 vrrand   = svrng_generate8_float(enginer,uniformr);
+	 t5.tvrad = t5.tvrad + *(AVXVec8*)&vrrand;
+	 vzrand   = svrng_generate8_float(enginez,uniformz);
+	 t5.tvz   = t5.tvz + *(AVXVec8*)&vzrand;
+	 t4.tmp2  = t5.tvz/t4.vNZPTS;
+	 t2.vhinc0 = t4.tmp2;
+	 t2.vhinc0 += VINC0;
+	 t2.vhinc1 = t4.tmp2;
+	 t2.vhinc1 += VINC1;
+	 t2.vhinc2 = t4.tmp2;
+	 t2.vhinc2 += VINC2;
+	 t2.vhinc3 = t4.tmp2;
+	 t2.vhinc3 += VINC3;
+	 t0.vtheta0 = ZERO;
+	 t3.vhinit0 = ZERO;
+	 t0.vtheta1 = ZERO;
+	 t3.vhinit1 = ZERO;
+	 t0.vtheta2 = ZERO;
+	 t3.vhinit2 = ZERO;
+	 t0.vtheta3 = ZERO;
+	 t3.vhinit3 = ZERO;
+	 __assume_aligned(m_tsc.branches_xparam,64);
+	 __assume_aligned(m_tsc.branches_yparam,64);
+	 __assume_aligned(m_tsc.branches_zparam,64);
+#pragma vector always
+#pragma vector vectorlength(8)
+        for(int32_t j = 0; j != m_tsc.branches_param_npts-3; j += 4) {
+            // for every point -- do...
+	    t0.vtheta0 = t0.vtheta0+t1.vthinc0;
+	    m_tsc.branches_xparam[Ix2D(i,m_tsc.branches_param_npts,j+0)] = t5.tvrad*cos(t0.vtheta0);
+	    m_tsc.branches_yparam[Ix2D(i,m_tsc.branches_param_npts,j+0)] = t5.tvrad*sin(t0.vtheta0);
+	    t3.vhinit0 = t3.vhinit0+t2.vhinc0;
+	    m_tsc.branches_zparam[Ix2D(i,m_tsc.branches_param_npts,j+0)] = t3.vhinit0;
+	    t0.vtheta1 = t0.vtheta1+t1.vthinc1;
+	    m_tsc.branches_xparam[Ix2D(i,m_tsc.branches_param_npts,j+1)] = t5.tvrad*cos(t0.vtheta1);
+	    m_tsc.branches_yparam[Ix2D(i,m_tsc.branches_param_npts,j+1)] = t5.tvrad*sin(t0.vtheta1);
+	    t3.vhinit1 = t3.vhinit1+t2.vhinc1;
+	    m_tsc.branches_zparam[Ix2D(i,m_tsc.branches_param_npts,j+1)] = t3.vhinit1;
+	    t0.vtheta2 = t0.vtheta2+t1.vthinc2;
+	    m_tsc.branches_xparam[Ix2D(i,m_tsc.branches_param_npts,j+2)] = t5.tvrad*cos(t0.vtheta2);
+	    m_tsc.branches_yparam[Ix2D(i,m_tsc.branches_param_npts,j+2)] = t5.tvrad*sin(t0.vtheta2);
+	    t3.vhinit2 = t3.vhinit2+t2.vhinc2;
+	    m_tsc.branches_zparam[Ix2D(i,m_tsc.branches_param_npts,j+2)] = t3.vhinit2;
+	    t0.vtheta3 = t0.vtheta3+t1.vthinc3;
+	    m_tsc.branches_xparam[Ix2D(i,m_tsc.branches_param_npts,j+3)] = t5.tvrad*cos(t0.vtheta3);
+	    m_tsc.branches_yparam[Ix2D(i,m_tsc.branches_param_npts,j+3)] = t5.tvrad*sin(t0.vtheta3);
+	    t3.vhinit3 = t3.vhinit3+t2.vhinc3;
+	    m_tsc.branches_zparam[Ix2D(i,m_tsc.branches_param_npts,j+3)] = t3.vhinit3;
+	}
+     }
+#elif defined __GNUC__ && !defined __INTEL_COMPILER
+      // Scalar version for GCC (no support for vectorised random number generators)
+#endif
 }
