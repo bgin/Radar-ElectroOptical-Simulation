7ab93fb7456c6e80f48ddfddcbaa0a55950db883
Bernard Gingold <beniekg@gmail.com>
2020-08-11 17:59:15 +0200

    Redefining the statistical function.

--- System/GMS_pmc_meter.cpp
+++ System/GMS_pmc_meter.cpp
@@ -110,3 +110,82 @@ run_benchmark() {
      pfcFini();
      return (0);
 }
+
+#if (USE_ACCURATE_IEEE754_2008_FP) == 1
+bool
+gms::system
+::PMCMeter
+::compute_stats(const std::vector<PFC_CNT> &data,
+		double &mean,
+		double &adev,
+		double &sdev,
+		double &skew,
+		double &kurt) {
+      using namespace gms::math::constants;
+      PFC_CNT s{ 0LL }, prev{ 0LL };
+      std::size_t vlen{};
+      __declspec(align(64)) struct {
+		double len{}, sum{}, var{}, t{}, t2{}, t3{},
+		t4{}, t5{}, isdev{}, tmp{};
+      }r8_loc;
+      vlen = data.size();
+      r8_loc.len = __binary64_from_uint64(vlen);
+	// Compute mean (guard against an overflow)
+      for (std::size_t i = 0Ui64; i != vlen; ++i) {
+		if (s < prev) return (false);
+		prev = s;
+		s += data[i];
+      }
+      mean = __binary64_from_uint64(s) / r8_loc.len;
+      for (std::size_t i = 0Ui64; i != vlen; ++i) {
+		if (zero != data[i]) {
+			r8_loc.tmp = __binary64_from_uint64(data[i]);
+			r8_loc.t = __binary64_abs(__binary64_sub_binary64_binary64(r8_loc.tmp,mean));
+			adev = __binary64_add_binary64_binary64(adev,r8_loc.t);
+			r8_loc.t2 = __binary64_mul_binary64_binary64(
+								__binary64_sub_binary64_binary64(r8_loc.tmp,mean),
+								__binary64_sub_binary64_binary64(r8_loc.tmp,mean));
+			r8_loc.var = __binary64_add_binary64_binary64(r8_loc.var,r8_loc.t2);
+		}
+	}
+	adev = __binary64_div_binary64_binary64(adev,r8_loc.len);
+        if (__binary64_quiet_less_binary64(r8_loc.var, 0.0)) {
+		std::printf(" PMCMeter::compute_stats: Invalid variance: %.15f\n", r8_loc.var);
+		    mean = dinf, adev = dinf,
+		    sdev = dinf, skew = dinf,
+		    kurt = dinf;
+		return (false);
+	}
+	r8_loc.var = __binary64_div_binary64_binary64(r8_loc.var,r8_loc.len);
+	sdev = __binary64_sqrt_binary64(r8_loc.var);
+	r8_loc.isdev = __binary64_div_binary64_binary64(1.0,sdev);
+	r8_loc.tmp = 0.0;
+	for (std::size_t i = 0Ui64; i != vlen; ++i) {
+		if (zero != data[i]) {
+			r8_loc.tmp = __binary64_from_uint64(data[i]);
+			r8_loc.t3 = __binary64_mul_binary64_binary64(
+								__binary64_sub_binary64_binary64(r8_loc.tmp,mean),r8_loc.isdev);
+			r8_loc.t4 = __binary64_mul_binary64_binary64(r8_loc.t3, 
+								__binary64_mul_binary64_binary64(r8_loc.t3,r8_loc.t3));
+			skew = __binary64_add_binary64_binary64(skew,r8_loc.t4);
+			r8_loc.t5 = __binary64_mul_binary64_binary64(r8_loc.t4,r8_loc.t3);
+			kurt = __binary64_add_binary64_binary64(kurt,r8_loc.t5);
+		}
+	}
+	skew = __binary64_div_binary64_binary64(skew,r8_loc.len);
+	kurt = __binary64_div_binary64_binary64(kurt,
+					__binary64_sub_binary64_binary64(r8_loc.len,3.0));
+	if (__binary64_quiet_less_equal_binary64(kurt, 1.0)) {
+		std::printf(" PMCMeter::compute_stats: Invalid kurtosis: %.15f\n", kurt);
+			mean = dinf, adev = dinf,
+			sdev = dinf, skew = dinf,
+			kurt = dinf;
+		return (false);
+	}
+       return (true);
+}
+#else
+
+
+
+#endif
