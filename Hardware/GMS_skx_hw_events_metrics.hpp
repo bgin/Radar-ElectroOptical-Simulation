d2e99e8738c5179104540095d8dc2559826d3eea
Bernard Gingold <beniekg@gmail.com>
2021-07-19 16:56:56 +0200

    Skylake-X perf event metrics.
    Work in progress.

--- Hardware/GMS_skx_hw_events_metrics.hpp
+++ Hardware/GMS_skx_hw_events_metrics.hpp
@@ -1518,7 +1518,665 @@ double skx_mem_page_hits_all_req(const double UNC_M_PRE_COUNT_RD_u0xc,
 }
 
 
+/*
+   Memory percentage  Cycles where all DRAM ranks are in PPD mode.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_DRAM_PPD_mode_cycles(const double UNC_M_POWER_CHANNEL_PPD,
+                                const double UNC_M_CLOCKTICKS) {
 
+       return (100.0*UNC_M_POWER_CHANNEL_PPD/
+                                UNC_M_CLOCKTICKS);
+}
+
+
+/*
+   Memory percentage Cycles all ranks in critical thermal throttle.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_mem_cycles_thermal_throttled(const double UNC_M_POWER_CRITICAL_THROTTLE_CYCLES,
+                                        const double UNC_M_CLOCKTICKS) {
+
+       return (100.0*UNC_M_POWER_CRITICAL_THROTTLE_CYCLES/
+                                           UNC_M_CLOCKTICKS);
+}
+
+
+/*
+   Memory  Cycles Memory is in self refresh power mode
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_mem_cycles_self_refresh(const double UNC_M_POWER_SELF_REFRESH,
+                                   const double UNC_M_CLOCKTICKS) {
+
+       return (100.0*UNC_M_POWER_SELF_REFRESH/
+                                  UNC_M_CLOCKTICKS);
+}
+
+
+/*
+   Uops delivered from decoded Icache (DSB).
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_DSB_uops_delivered(const double IDQ_DSB_UOPS,
+                              const double IDQ_MITE_UOPS,
+			      const double IDQ_MS_UOPS,
+			      const double LSD_UOPS) {
+
+        const double term1 = IDQ_DSB_UOPS+
+	                     IDQ_MITE_UOPS+
+			     IDQ_MS_UOPS+
+			     LSD_UOPS;
+	return (IDQ_DSB_UOPS/term1);
+}
+
+
+/*
+   Uops delivered from legacy decode pipeline (MITE).
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_MITE_uops_delivered(const double IDQ_DSB_UOPS,
+                              const double IDQ_MITE_UOPS,
+			      const double IDQ_MS_UOPS,
+			      const double LSD_UOPS) {
+
+        const double term1 = IDQ_DSB_UOPS+
+	                     IDQ_MITE_UOPS+
+			     IDQ_MS_UOPS+
+			     LSD_UOPS;
+        return (IDQ_MITE_UOPS/term1);
+}
+
+
+/*
+    Uops delivered from microcode sequencer (MS).
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_MS_uops_delivered(const double IDQ_DSB_UOPS,
+                              const double IDQ_MITE_UOPS,
+			      const double IDQ_MS_UOPS,
+			      const double LSD_UOPS) {
+
+        const double term1 = IDQ_DSB_UOPS+
+	                     IDQ_MITE_UOPS+
+			     IDQ_MS_UOPS+
+			     LSD_UOPS;
+        return (IDQ_MS_UOPS/term1);
+}
+
+
+/*
+   Uops delivered from loop stream detector (LSD). 
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_LSD_uops_delivered(const double IDQ_DSB_UOPS,
+                              const double IDQ_MITE_UOPS,
+			      const double IDQ_MS_UOPS,
+			      const double LSD_UOPS) {
+
+        const double term1 = IDQ_DSB_UOPS+
+	                     IDQ_MITE_UOPS+
+			     IDQ_MS_UOPS+
+			     LSD_UOPS;
+        return (LSD_UOPS/term1);
+}
+
+
+/*
+    FP scalar single-precision FP instructions retired per instruction.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_fp32_scalar_retired(const double FP_ARITH_INST_RETIRED_SCALAR_SINGLE,
+                                const double INST_RETIRED_ANY) {
+
+       return (FP_ARITH_INST_RETIRED_SCALAR_SINGLE/
+                                        INST_RETIRED_ANY);
+}
+
+
+/*
+   FP scalar double-precision FP instructions retired per instruction.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_fp64_scalar_retired(const double FP_ARITH_INST_RETIRED_SCALAR_DOUBLE,
+                                const double INST_RETIRED_ANY) {
+
+       return (FP_ARITH_INST_RETIRED_SCALAR_DOUBLE/
+                                        INST_RETIRED_ANY);
+}
+
+
+/*
+   FP 128-bit packed single-precision FP instructions retired per instruction.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_fp32_vec128b_retired(const double FP_ARITH_INST_RETIRED_128B_PACKED_SINGLE,
+                                const double INST_RETIRED_ANY) {
+
+       return (FP_ARITH_INST_RETIRED_128B_PACKED_SINGLE/
+                                    INST_RETIRED_ANY);
+}
+
+
+/*
+   FP 128-bit packed double-precision FP instructions retired per instruction.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_fp64_vec128b_retired(const double FP_ARITH_INST_RETIRED_128B_PACKED_DOUBLE,
+                                const double INST_RETIRED_ANY) {
+
+       return (FP_ARITH_INST_RETIRED_128B_PACKED_DOUBLE/
+                                    INST_RETIRED_ANY);
+}
+
+
+/*
+   FP 256-bit packed single-precision FP instructions retired per instruction.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_fp32_vec256b_retired(const double FP_ARITH_INST_RETIRED_256B_PACKED_SINGLE,
+                                const double INST_RETIRED_ANY) {
+
+       return (FP_ARITH_INST_RETIRED_256B_PACKED_SINGLE/
+                                    INST_RETIRED_ANY);
+}
+
+
+/*
+    FP 256-bit packed double-precision FP instructions retired per instruction.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_fp64_vec256b_retired(const double FP_ARITH_INST_RETIRED_256B_PACKED_DOUBLE,
+                                const double INST_RETIRED_ANY) {
+
+       return (FP_ARITH_INST_RETIRED_256B_PACKED_DOUBLE/
+                                    INST_RETIRED_ANY);
+}
+
+
+/*
+    FP 512-bit packed single-precision FP instructions retired per instruction.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_fp32_vec512b_retired(const double FP_ARITH_INST_RETIRED_512B_PACKED_SINGLE,
+                                const double INST_RETIRED_ANY) {
+
+       return (FP_ARITH_INST_RETIRED_512B_PACKED_SINGLE/
+                                    INST_RETIRED_ANY);
+}
+
+
+/*
+    FP 512-bit packed double-precision FP instructions retired per instruction.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_fp64_vec512b_retired(const double FP_ARITH_INST_RETIRED_512B_PACKED_DOUBLE,
+                                const double INST_RETIRED_ANY) {
+
+       return (FP_ARITH_INST_RETIRED_512B_PACKED_DOUBLE/
+                                    INST_RETIRED_ANY);
+}
+
+
+/*
+    FP instruction density (percentage).
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_fp_instructions_density(const double FP_ARITH_INST_RETIRED_SCALAR_SINGLE,
+                                   const double FP_ARITH_INST_RETIRED_SCALAR_DOUBLE,
+				   const double FP_ARITH_INST_RETIRED_128B_PACKED_SINGLE,
+				   const double FP_ARITH_INST_RETIRED_128B_PACKED_DOUBLE,
+				   const double FP_ARITH_INST_RETIRED_256B_PACKED_SINGLE,
+				   const double FP_ARITH_INST_RETIRED_256B_PACKED_DOUBLE,
+				   const double FP_ARITH_INST_RETIRED_512B_PACKED_SINGLE,
+				   const double FP_ARITH_INST_RETIRED_512B_PACKED_DOUBLE,
+				   const double INST_RETIRED_ANY) {
+
+       const double term1 = FP_ARITH_INST_RETIRED_SCALAR_SINGLE+
+                            FP_ARITH_INST_RETIRED_SCALAR_DOUBLE+
+			    FP_ARITH_INST_RETIRED_128B_PACKED_SINGLE+
+			    FP_ARITH_INST_RETIRED_128B_PACKED_DOUBLE;
+       const double term2 = FP_ARITH_INST_RETIRED_256B_PACKED_SINGLE+
+                            FP_ARITH_INST_RETIRED_256B_PACKED_DOUBLE+
+			    FP_ARITH_INST_RETIRED_512B_PACKED_SINGLE+
+			    FP_ARITH_INST_RETIRED_512B_PACKED_DOUBLE;
+       return ((term1+term2)/INST_RETIRED_ANY);
+}
+
+
+/*
+    Branch instruction density
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_branch_instr_density(const double BR_INST_RETIRED_ALL_BRANCHES,
+                                const double INST_RETIRED_ANY) {
+
+       return (100.0*BR_INST_RETIRED_ALL_BRANCHES/
+                                      INST_RETIRED_ANY);
+}
+
+
+/*
+    DRAM power (watts).
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_DRAM_power(const double FREERUN_DRAM_ENERGY_STATUS,
+                      const double TIME_INTERVAL_SECONDS) {
+
+       const double term1 =  FREERUN_DRAM_ENERGY_STATUS*15.3;
+       return (term1/1000000.0/TIME_INTERVAL_SECONDS);
+}
+
+
+/*
+    Package power (watts)
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_Package_power(const double FREERUN_PKG_ENERGY_STATUS,
+                         const double TIME_INTERVAL_SECONDS) {
+
+       const double term1 = FREERUN_PKG_ENERGY_STATUS*61.0;
+       return (term1/1000000.0/TIME_INTERVAL_SECONDS);
+}
+
+
+/*
+    Core c3 residency
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_Core_C3_residency(const double FREERUN_CORE_C3_RESIDENCY,
+                        const double TSC) {
+
+       return (100.0*FREERUN_CORE_C3_RESIDENCY/TSC);
+}
+
+
+/*
+   Core c6 residency
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_Core_C6_residency(const double FREERUN_CORE_C6_RESIDENCY,
+                             const double TSC) {
+
+       return (100.0*FREERUN_CORE_C6_RESIDENCY/TSC);
+}
+
+
+/*
+   Package c2 residency
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_Package_C2_residency(const double FREERUN_PKG_C2_RESIDENCY,
+                                const double TSC) {
+
+       return (100.0*FREERUN_PKG_C2_RESIDENCY/TSC);
+}
+
+
+/*
+   Package c3 residency
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_Package_C3_residency(const double FREERUN_PKG_C3_RESIDENCY,
+                                const double TSC) {
+
+       return (100.0*FREERUN_PKG_C3_RESIDENCY/TSC);
+}
+
+
+/*
+   Package c6 residency
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_Package_C6_residency(const double FREERUN_PKG_C6_RESIDENCY,
+                                const double TSC) {
+
+       return (100.0*FREERUN_PKG_C6_RESIDENCY/TSC);
+}
+
+
+/*
+    core SW prefetch NTA per instruction.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_sw_nta_prefetches(const double SW_PREFETCH_ACCESS_NTA,
+                             const double INST_RETIRED_ANY) {
+
+       return(SW_PREFETCH_ACCESS_NTA/INST_RETIRED_ANY);
+}
+
+
+/*
+   core  cycles core power throttled.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_Core_power_throttled(const double CORE_POWER_THROTTLE,
+                                const double CPU_CLK_UNHALTED_THREAD) {
+
+       return (100.0*CORE_POWER_THROTTLED/
+                     CPU_CLK_UNHALTED_THREAD);
+}
+
+
+/*
+     Core IPC.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_TMAM_core_ipc(const double INST_RETIRED_ANY,
+                         const double CPU_CLK_UNHALTED_THREAD_ANY,
+			 const double HW_THREAD_COUNT) {
+
+       const double term1 = CPU_CLK_UNHALTED_THREAD_ANY/
+                            HW_THREAD_COUNT;
+       return (INST_RETIRED_ANY/term1);
+}
+
+
+/*
+    TMAM Info Memory Level Parallelism
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_TMAM_mem_lvl_parallelism(const double L1D_PEND_MISS_PENDING,
+                                    const double L1D_PEND_MISS_PENDING_CYCLES_ANY,
+				    const double HW_THREAD_COUNT) {
+
+       const double term1 = L1D_PEND_MISS_PENDING_CYCLES_ANY/
+                            HW_THREAD_COUNT;
+       return (L1D_PEND_MISS_PENDING/term1);
+}
+
+
+/*
+   TMAM Info cycles both threads active
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_TMAM_SMT_activity(const double CPU_CLK_THREAD_UNHALTED_ONE_THREAD_ACTIVE,
+                             const double CPU_CLK_THREAD_UNHALTED_REF_XCLK_ANY,
+			     const double HW_THREAD_COUNT) {
+
+       const double term1 = 1.0-CPU_CLK_THREAD_UNHALTED_ONE_THREAD_ACTIVE/
+                            (CPU_CLK_THREAD_UNHALTED_REF_XCLK_ANY*0.5);
+       return ((HW_THREAD_COUNT < 2.0) ? 0.0 : 100.0*term1)
+}
+
+
+/*
+    TMAM Frontend Bound
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_frontend_bound(const double CPU_CLK_UNHALTED_THREAD,
+                          const double IDQ_UOPS_NOT_DELIVERED_CORE){
+			
+
+        const double term1 = 4.0*CPU_CLK_UNHALTED_THREAD_ANY;
+	return (100.0*IDQ_UOPS_NOT_DELIVERED_CORE/term1);
+}
+
+
+/*
+   TMAM Frontend_Latency
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_frontend_latency(const double CPU_CLK_UNHALTED_THREAD,
+                            const double IDQ_UOPS_NOT_DELIVERED_CYCLES_0_UOPS_DELIV_CORE){
+			  
+       return (100.0*IDQ_UOPS_NOT_DELIVERED_CYCLES_0_UOPS_DELIV_CORE/
+                 CPU_CLK_UNHALTED_THREAD); 
+}
+
+
+/*
+    TMAM ICache Misses.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_ICache_misses(const double ICACHE_16B_IFDATA_STALL,
+                         const double ICACHE_16B_IFDATA_STALL_c1_e1,
+			 const double CPU_CLK_UNHALTED_THREAD) {
+
+       const double term1 = ICACHE_16B_IFDATA_STALL+2.0*
+                            ICACHE_16B_IFDATA_STALL_c1_e1;
+       return (100.0*term1/CPU_CLK_UNHALTED_THREAD);
+}
+
+
+/*
+   ITLB Misses 
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_ITLB_misses(const double ITLB_MISSES_STLB_HIT,
+                       const double ITLB_MISSES_WALK_PENDING_c1,
+		       const double CPU_CLK_UNHALTED_THREAD) {
+
+       const double term1 = 9.0*ITLB_MISSES_STLB_HIT+
+                            ITLB_MISSES_WALK_PENDING_c1;
+       return (100.0*term1/CPU_CLK_UNHALTED_THREAD);
+}
+
+
+/*
+   Branch Resteers
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_branch_resteers(const double INT_MISC_CLEAR_RESTEER_CYCLES,
+                           const double BACLEARS_ANY,
+			   const double RS_EVENTS_EMPTY_CYCLES,
+			   const double RS_EVENTS_EMPTY_END,
+			   const double  ICACHE_16B_IFDATA_STALL,
+			   const double ICACHE_64B_IFTAG_STALL,
+			   const double CPU_CLK_UNHALTED_THREAD) {
+   
+       const double term1 = (RS_EVENTS_EMPTY_CYCLES-ICACHE_16B_IFDATA_STALL-
+                            ICACHE_64B_IFTAG_STALL)/RS_EVENTS_EMPTY_END;
+       return (100.0*(INT_MISC_CLEAR_RESTEER_CYCLES+term1*BACLEARS_ANY)/
+                      CPU_CLK_UNHALTED_THREAD);
+}
+
+
+/*
+    DSB switches.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_DSB_switches(const double DSB2MITE_SWITCHES_PENALTY_CYCLES,
+                        const double CPU_CLK_UNHALTED_THREAD) {
+
+       return (100.0*DSB2MITE_SWITCHES_PENALTY_CYCLES/
+                   CPU_CLK_UNHALTED_THREAD);
+}
+
+
+/*
+    MS switches.
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_MS_switches(const double IDQ_MS_SWITCHES,
+                       const double CPU_CLK_UNHALTED_THREAD) {
+
+       return (200.0*IDQ_MS_SWITCHES/CPU_CLK_UNHALTED_THREAD);
+}
+
+
+/*
+   Frontend Bandwidth (SMT is enabled)
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_smt_frontend_bw(const double CPU_CLK_UNHALTED_THREAD_ANY,
+                       const double IDQ_UOPS_NOT_DELIVERED_CORE,
+		       const double IDQ_UOPS_NOT_DELIVERED_CYCLES_0_UOPS_DELIV_CORE,
+		       const double HW_THREAD_COUNT){
+	
+
+        const double term1 = IDQ_UOPS_NOT_DELIVERED_CORE-4.0*
+	                     IDQ_UOPS_NOT_DELIVERED_CYCLES_0_UOPS_DELIV_CORE;
+        const double term2 = 4.0*(CPU_CLK_UNHALTED_THREAD_ANY/HW_THREAD_COUNT);
+        return (100.0*term1/term2);
+}
+
+
+/*
+    Frontend Bandwidth (SMT is disabled)
+*/
+__attribute__((always_inline))
+__attribute__((pure))
+__attribute__((aligned(32)))
+#pragma omp declare simd
+static inline
+double skx_no_smt_frontend_bw(const double CPU_CLK_UNHALTED_THREAD_ANY,
+                              const double IDQ_UOPS_NOT_DELIVERED_CORE,
+		              const double IDQ_UOPS_NOT_DELIVERED_CYCLES_0_UOPS_DELIV_CORE) {
+
+       const double term1 = IDQ_UOPS_NOT_DELIVERED_CORE-4.0*
+	                    IDQ_UOPS_NOT_DELIVERED_CYCLES_0_UOPS_DELIV_CORE;
+       const double term2 = 4.0*CPU_CLK_UNHALTED_THREAD_ANY;
+        return (100.0*term1/term2);    
+}
 
 
 #endif /*__GMS_SKX_HW_EVENTS_METRICS_HPP__*/
