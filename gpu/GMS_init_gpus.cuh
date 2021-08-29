afb460b59b827d4c5380ad5eff94286dfca1c945
Bernard Gingold <beniekg@gmail.com>
2021-08-29 09:52:49 +0200

    Fixed few issues.

--- gpu/GMS_init_gpus.cuh
+++ gpu/GMS_init_gpus.cuh
@@ -3,7 +3,7 @@
 #define __GMS_INIT_GPUS_CUH__
 
 
-
+#include <stdint.h>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
@@ -15,37 +15,38 @@
 //
 //	Print device capabilities to screen.
 //
-void devcaps_to_screen(const int, int * , const bool);
+void devcaps_to_screen(const int32_t, 
+                       int32_t * , const bool);
 
 
 //
 //	Print device capabilities to file.
 //
-void devcaps_to_file(const int, const char *, 
-				   int * , const bool );
+void devcaps_to_file(const int32_t, const char *, 
+		     int32_t * , const bool );
 
 
 //
 // Run integer and real arithmentic test.
 // Host code.
 //
-cudaError_t  gpu_vec_add_tests(const int);
+cudaError_t  gpu_vec_add_tests(const32_t int);
 
 // 
 // Device code.
-// simple vector addition int32_t,REAL4 and REAL8.
+// simple vector addition.
 //
 __global__ void kvec_add_int32(int32_t * __restrict,
-							const int32_t * __restrict,
-							const int32_t * __restrict);
+			       const int32_t * __restrict,
+			       const int32_t * __restrict);
 
-__global__ void kvec_add_real4(float * __restrict,
-						   const REAL4 * __restrict ,
-						   const REAL4 * __restrict);
+__global__ void kvec_add_float(float * __restrict,
+			       const float * __restrict ,
+			       const float * __restrict);
 
-__global__ void kvec_add_real8(double * __restrict,
-						    const REAL8 * __restrict,
-							const REAL8 * __restrict);
+__global__ void kvec_add_double(double * __restrict,
+				const double * __restrict,
+				const double * __restrict);
 
 
 
