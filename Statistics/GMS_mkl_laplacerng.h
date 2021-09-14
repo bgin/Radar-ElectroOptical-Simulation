daa4abd6b29a7cfbad1a3710a6443e511815492d
Bernard Gingold <beniekg@gmail.com>
2021-09-14 09:10:42 +0200

    Update GMS_mkl_laplacerng.h

--- Statistics/GMS_mkl_laplacerng.h
+++ Statistics/GMS_mkl_laplacerng.h
@@ -4,24 +4,20 @@
 
 namespace file_info {
 
-#if defined _WIN64
-    #include "../GMS_version.h"
-#elif defined __linux
-    #include "GMS_version.h"
-#endif
 
-const unsigned int gGMS_MKL_LAPLACERNG_MAJOR =  gms::common::gVersionInfo.m_VersionMajor;
 
-const unsigned int gGMS_MKL_LAPLACERNG_MINOR =  gms::common::gVersionInfo.m_VersionMinor;
+const unsigned int gGMS_MKL_LAPLACERNG_MAJOR =  1;
 
-const unsigned int gGMS_MKL_LAPLACERNG_MICRO =  gms::common::gVersionInfo.m_VersionMicro;
+const unsigned int gGMS_MKL_LAPLACERNG_MINOR = 1;
+
+const unsigned int gGMS_MKL_LAPLACERNG_MICRO =  0;
 
 const unsigned int gGMS_MKL_LAPLACERNG_FULLVER = 
 	1000U*gGMS_MKL_LAPLACERNG_MAJOR + 100U*gGMS_MKL_LAPLACERNG_MINOR + 10U*gGMS_MKL_LAPLACERNG_MICRO;
 
 const char * const pgGMS_MKL_LAPLACERNG_CREATE_DATE = "25-04-2018 09:25 +00200 (WED 25 APR 2018 GMT+2)";
 
-const char * const pgGMS_MKL_LAPLACERNG_BUILD_DATE = "00-00-0000 00:00";
+const char * const pgGMS_MKL_LAPLACERNG_BUILD_DATE = __DATE__ ":" __TIME__;
 
 const char * const pgGMS_MKL_LAPLACERNG_AUTHOR = "Programmer: Bernard Gingold, e-mail: beniekg@gmail.com";
 
@@ -33,12 +29,9 @@ const char * const pgGMS_MKL_LAPLACERNG_DESCRIPT = "C++ wrapper for Intel MKL vd
 
 
 #include <iostream>
-#if defined _WIN64
-    #include "../GMS_config.h"
-#elif defined __linux
-    #include "GMS_config.h"
-#else
-    #error Unsupported Operating System (Linux and Win64 -- only supported currently)
+
+#include "GMS_config.h"
+
 #endif
 #if (USE_MKL) == 1
 #include <mkl_vsl.h>
@@ -53,16 +46,12 @@ namespace gms {
 				//
 				//	C++ wrapper around Intel MKL vdRngLaplace procedure.
 				//
-#if defined _WIN64
-			__declspec(align(64)) struct MKLLaplaceRNG{
-#elif defined __linux
+
 			  __attribute__((align(64))) struct MKLLaplaceRNG {
-#endif
-#if defined _WIN64			    
-				    _Field_size_(m_nvalues)double * __restrict m_rvec;
-#elif defined __linux
+
+
 			                double * __restrict m_rvec;
-#endif
+
 					double  m_a;
 
 					double  m_beta;
