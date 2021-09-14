ec5b046c9ff0e8e14740d88f47cce989b5b1bc74
Bernard Gingold <beniekg@gmail.com>
2021-09-14 09:37:24 +0200

    Redeclared a loop index to be a int32_t type.

--- Statistics/GMS_svrng_wrappers.h
+++ Statistics/GMS_svrng_wrappers.h
@@ -12,7 +12,7 @@ namespace file_info {
 
      const unsigned int gGMS_SVRNG_WRAPPERS_MAJOR = 1;
      const unsigned int gGMS_SVRNG_WRAPPERS_MINOR = 0;
-     const unsigned int gGMS_SVRNG_WRAPPERS_MICRO = 0;
+     const unsigned int gGMS_SVRNG_WRAPPERS_MICRO = 1;
      const unsigned int gGMS_SVRNG_WRAPPERS_FULLVER = 1000U*gGMS_SVRNG_WRAPPERS_MAJOR+100U*gGMS_SVRNG_WRAPPERS_MINOR+
                                                       10U*gGMS_SVRNG_WRAPPERS_MICRO;
      const char * const pgGMS_SVRNG_WRAPPERS_CREATION_DATE = "29-12-2019 11:55 +00200 (SUN 29 DEC 2019 GMT+2)";
@@ -36,14 +36,14 @@ namespace gms{
 
                       // mt19937 engine float8
 		      void svrng_wrapper_mt19937_init_float8(float * __restrict __ATTR_ALIGN__(64),
-		                                             const int64_t ,
+		                                             const int32_t ,
 							     const float,
 							     const float,
 							     const int32_t,
 							     int32_t &  ) __ATTR_COLD__ __ATTR_ALIGN__(32);
 		      // mt19937 engine double4
 		      void svrng_wrapper_mt19937_init_double4(double * __restrict __ATTR_ALIGN__(64),
-		                                              const int64_t,
+		                                              const int32_t,
 							      const double,
 							      const double,
 							      const int32_t,
@@ -51,7 +51,7 @@ namespace gms{
 
 		      // mt19937 engine AVXVec8
 		      void svrng_wrapper_mt19937_init_avxvec8(AVXVec8 * __restrict __ATTR_ALIGN__(64),
-		                                              const int64_t,
+		                                              const int32_t,
 							      const float,
 							      const float,
 							      const int32_t,
@@ -59,7 +59,7 @@ namespace gms{
 
 		      // mt19937 engine AVX512c4f32
 		      void svnrg_wrapper_mt19937_init_avx512c4f32(AVX512c4f32 * __restrict __ATTR_ALIGN__(64),
-		                                                  const int64_t,
+		                                                  const int32_t,
 								  const float,
 								  const float,
 								  const float,
@@ -68,7 +68,7 @@ namespace gms{
 								  int32_t & ) __ATTR_COLD__ __ATTR_ALIGN__(32);
 		      // mt19937 engine AVXc8f32
 		      void svrng_wrapper_mt19937_init_avxc8f32(AVXc8f32 * __restrict __ATTR_ALIGN__(64),
-							       const int64_t,
+							       const int32_t,
 							       const float,
 							       const float,
 							       const float,
@@ -78,7 +78,7 @@ namespace gms{
 
 		      // mt19937 engine AVXc4f64
 		      void svrng_wrapper_mt19937_init_avxc4f64(AVXc4f64 * __restrict __ATTR_ALIGN__(64),
-							       const int64_t,
+							       const int32_t,
 							       const double,
 							       const double,
 							       const double,
