6cbae4b934c8c6ccd9593454b5d37288af9805fc
Bernard Gingold <beniekg@gmail.com>
2021-09-16 14:11:51 +0200

    Old clutter removed.

--- Statistics/GMS_mkl_uniformrng.h
+++ Statistics/GMS_mkl_uniformrng.h
@@ -1,27 +1,23 @@
 
 #ifndef __GMS_MKL_UNIFORMRNG_H__
-#define __GMS_MKL_UNIFORMRNG_H__
+#define __GMS_MKL_UNIFORMRNG_H__ 201824040842
 
 namespace file_info {
 
-#if defined _WIN64
-  #include "../GMS_version.h"
-#elif defined __linux
-  #include "GMS_version.h"
-#endif
+
   
-const unsigned int gGMS_MKL_UNIFORMRNG_MAJOR = gms::common::gVersionInfo.m_VersionMajor;
+const unsigned int gGMS_MKL_UNIFORMRNG_MAJOR = 1;
 
-const unsigned int gGMS_MKL_UNIFORMRNG_MINOR = gms::common::gVersionInfo.m_VersionMinor;
+const unsigned int gGMS_MKL_UNIFORMRNG_MINOR = 1;
 
-const unsigned int gGMS_MKL_UNIFORMRNG_MICRO = gms::common::gVersionInfo.m_VersionMicro;
+const unsigned int gGMS_MKL_UNIFORMRNG_MICRO = 0;
 
 const unsigned int gGMS_MKL_UNIFORMRNG_FULLVER = 
 	1000U*gGMS_MKL_UNIFORMRNG_MAJOR + 100U*gGMS_MKL_UNIFORMRNG_MINOR + 10U*gGMS_MKL_UNIFORMRNG_MICRO;
 
 const char * const pgGMS_MKL_UNIFORMRNG_CREATE_DATE = "24-04-2018 08:42 +00200 (TUE 24 APR 2018 GMT+2)";
 
-const char * const pgGMS_MKL_UNIFORMRNG_BUILD_DATE = "00-00-0000 00:00";
+const char * const pgGMS_MKL_UNIFORMRNG_BUILD_DATE = __DATE__ ":" __TIME__;
 
 const char * const pgGMS_MKL_UNIFORMRNG_AUTHOR = "Programmer: Bernard Gingold, e-mail: beniekg@gmail.com";
 
@@ -30,13 +26,9 @@ const char * const pgGMS_MKL_UNIFORMRNG_DESCRIPT = "C++ wrapper over MKL vdRngUn
 }
 
 #include <iostream>
-#if defined _WIN64
-    #include "../GMS_config.h"
-#elif defined __linux
-    #include "GMS_config.h"
-#else
-    #error UNsupported Operating System (Linux and Win64 are currently supported)
-#endif
+
+#include "GMS_config.h"
+
 #if (USE_MKL) == 1
     #include <mkl_vsl.h>
 #else
@@ -50,16 +42,12 @@ namespace gms {
 			//
 			//	C++ wrapper around Intel MKL vdRngUniform procedure.
 			//
-#if defined _WIN64
-			__declspec(align(64))  struct MKLUniformRNG{
-#elif defined __linux
+
                         __attribute__((align(64))) struct MKLUniformRNG {
-#endif
-#if defined _WIN64
-				    _Field_size_(m_nvalues) double * __restrict m_rvec; // vector of random values
-#elif defined __linux
+
+
                                     double * __restrict m_rvec;
-#endif
+
 				    double   m_a; // param 'a'
 
 				    double   m_b;   // param 'b'
