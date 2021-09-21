e74afb49a2fc69e305dd6be49509e26174341abd
Bernard Gingold <beniekg@gmail.com>
2021-09-21 11:06:11 +0200

    Changed name of the kernels.

--- dsp/GMS_32f_atan_32f.hpp
+++ dsp/GMS_32f_atan_32f.hpp
@@ -63,7 +63,7 @@
 	 __ATTR_HOT__
 	 __ATTR_ALIGN__(32)
 	 static inline
-	 void dsp_32f_atan_32f_u_avx(float * __restrict b,
+	 void dsp_32f_atan_32f_u_avx_looped(float * __restrict b,
 	                             float * __restrict a,
 				     const int32_t npoints) {
                DSP_32F_ATAN_32F_BLOCK
@@ -125,7 +125,7 @@
 	 __ATTR_HOT__
 	 __ATTR_ALIGN__(32)
 	 static inline
-	 void dsp_32f_atan_32f_a_avx(float * __restrict b,
+	 void dsp_32f_atan_32f_a_avx_looped(float * __restrict b,
 	                             float * __restrict a,
 				     const int32_t npoints) {
                DSP_32F_ATAN_32F_BLOCK
