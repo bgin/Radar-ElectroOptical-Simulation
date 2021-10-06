5c5e91ecd6c7ff41f7df90449514e6fdd72be123
Bernard Gingold <beniekg@gmail.com>
2021-10-06 16:03:07 +0200

    Removed Win support.

--- Math/GMS_avx512vcprocedures.h
+++ Math/GMS_avx512vcprocedures.h
@@ -1,26 +1,22 @@
 
 #ifndef __GMS_AVX512VCPROCEDURES_H__
-#define __GMS_AVX512VCPROCEDURES_H__
+#define __GMS_AVX512VCPROCEDURES_H__ 121020191812
 
 namespace file_info {
-#if defined _WIN64
-    #include "../GMS_version.h"
-#elif defined __linux
-    #include "GMS_version.h"
-#endif
 
-	const unsigned int gGMS_AVX512PROCEDURES_MAJOR = gms::common::gVersionInfo.m_VersionMajor;
 
-	const unsigned int gGMS_AVX512PROCEDURES_MINOR = gms::common::gVersionInfo.m_VersionMinor;
+	const unsigned int gGMS_AVX512PROCEDURES_MAJOR = 1;
 
-	const unsigned int gGMS_AVX512PROCEDURES_MICRO = gms::common::gVersionInfo.m_VersionMicro;
+	const unsigned int gGMS_AVX512PROCEDURES_MINOR = 0;
+
+	const unsigned int gGMS_AVX512PROCEDURES_MICRO = 1;
 
 	const unsigned int gGMS_AVX512PROCEDURES_FULLVER = 
 		1000U*gGMS_AVX512COMPLEX_MAJOR+100U*gGMS_AVX512COMPLEX_MINOR+10U*gGMS_AVX512COMPLEX_MICRO;
 
 	const char * const pgGMS_AVX512PROCEDURES_CREATE_DATE = "12-10-2019 18:12 +00200 (SAT  12 OCT  2019 GMT+2)";
 
-	const char * const pgGMS_AVX512PROCEDURES_BUILD_DATE = "00-00-0000 00:00";
+	const char * const pgGMS_AVX512PROCEDURES_BUILD_DATE = __DATE__":"__TIME__;
 
 	const char * const pgGMS_AVX512PROCEDURES_AUTHOR = "Programmer: Bernard Gingold, contact: beniekg@gmail.com";
 
@@ -91,9 +87,7 @@ class AVX512VComplex1D;
 				   const double * __restrict,
 				   const bool) noexcept(true);
 
-	  //
-	  //	*********** Procedures which compute equality/inequality of complex series implemented in LAM_avxvcprocedures.cpp ************
-	  //
+	 
 	}
 }
 
