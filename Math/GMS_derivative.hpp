56fc03ee8dc678cece7c6917f94022d1277c43ab
Bernard Gingold <beniekg@gmail.com>
2021-10-11 15:13:45 +0200

    Update GMS_derivative.hpp

--- Math/GMS_derivative.hpp
+++ Math/GMS_derivative.hpp
@@ -80,7 +80,7 @@ namespace  gms {
 			}
 
 
-			__ATTR_ALWAYS_INLINE__
+		        __ATTR_ALWAYS_INLINE__
                         __ATTR_HOT__
                         __ATTR_ALIGN__(32)
 	                static inline
