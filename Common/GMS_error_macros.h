d01fa6db0abb73392ede0a64c83bb69b09ed6903
Bernard Gingold <beniekg@gmail.com>
2021-10-11 09:39:56 +0200

    Update GMS_error_macros.h

--- Common/GMS_error_macros.h
+++ Common/GMS_error_macros.h
@@ -1,78 +1,21 @@
 
 #ifndef __GMS_ERROR_MACROS_H__
-#define __GMS_ERROR_MACROS_H__
+#define __GMS_ERROR_MACROS_H__ 280920191202
 
-#if !defined (GMS_ERROR_MACROS_MAJOR)
-#define GMS_ERROR_MACROS_MAJOR 1
-#endif
 
-#if !defined (GMS_ERROR_MACROS_MINOR)
-#define GMS_ERROR_MACROS_MINOR 0
-#endif
 
-#if !defined (GMS_ERROR_MACROS_MICRO)
-#define GMS_ERROR_MACROS_MICRO 0
-#endif
 
-#if !defined (GMS_ERROR_MACROS_FULLVER)
-#define GMS_ERROR_MACROS_FULLVER 1000
-#endif
-
-#if !defined (GMS_ERROR_MACROS_CREATE_DATE)
-#define GMS_ERROR_MACROS_CREATE_DATE "28-09-2019 12:02 +00200 (SAT 28 SEP 2019 GMT+2)"
-#endif
-//
-//  Set this value after successful build date/time
-//
-#if !defined (GMS_ERROR_MACROS_BUILD_DATE)
-#define GMS_ERROR_MACROS_BUILD_DATE " "
-#endif
-
-#if !defined (GMS_ERROR_MACROS_AUTHOR)
-#define GMS_ERROR_MACROS_AUTHOR "Programmer: Bernard Gingold, e-mail: beniekg@gmail.com"
-#endif
-
-#if !defined (GMS_ERROR_MACROS_DESCRIPT)
-#define GMS_ERROR_MACROS_DESCRIPT "Parametrized macros for displaying error messages."
-#endif
 
 
 
 #include <iostream>
 #include <iomanip>
-#if defined _WIN64
-    #include "../LibAtmosModelCPP/System/StackWalker.h"
-#endif
 
-#if defined _WIN64
-  #if !defined (DUMP_CALLSTACK_ON_ERROR)
-      #define DUMP_CALLSTACK_ON_ERROR  \
-	       StackWalker sw{};   \
-	       sw.ShowCallstack();
-  #endif
-#endif
 
-#if defined _WIN64
-   #if !defined (PRINT_ERROR_INFO)
-       #define PRINT_ERROR_INFO(msg) \
-	       std::cerr << "[" << __DATE__ << ":" << __TIME__ << "]" << (msg) << "\n"; \
-	       std::cerr << "at " << __FILE__ << ":" << __LINE__ << "(" << std::hex << "0x" << __FUNCTIONW__ << ")" << "\n";\
-	       std::cerr << " Dumping callstack: \n"; \
-	       StackWalker sw{};                      \
-	       sw.ShowCallstack();
-   #endif
-#endif
 
-#if defined _WIN64
-   #if !defined (PRINT_ERROR_VALUE)
-       #define PRINT_ERROR_VALUE(msg,value) \
-	       std::cerr << "[" << __DATE__ << ":" << __TIME__ << "]" << (msg) << (value) << "\n"; \
-	       std::cerr << "at " << __FILE__ << ":" << __LINE__ << "(" << std::hex << "0x" << __FUNCTIONW__ << ")" << "\n"; \
-	       std::cerr << " Dumping callstack: \n"; \
-	       StackWalker sw{};                      \
-	       sw.ShowCallstack();
-    #endif
-#endif
+
+
+
 
 #if !defined (ABORT_ON_ERROR)
 #define ABORT_ON_ERROR(msg,err) \
