640d44e21a30988577182d7be1a83aa863817c9e
Bernard Gingold <beniekg@gmail.com>
2022-01-22 13:27:26 +0100

    ODE diva (added threadprivate)

---
+++ ODE-Solvers/diva.h
@@ -0,0 +1,16 @@
+void diva(double[],double[],double[],long[],long,
+          void(*)(double[],double[],double[],long[]),
+          void(*)(double[],double[],double[],long[]),long,long,long,long,long[]);
+void divaa(double[],double[],double[],long[],
+          void(*)(double[],double[],double[],long[]),
+          void(*)(double[],double[],double[],long[]));
+void divabu(double[],long[]);
+void divaco(long[],double[]);
+void divacr(double[],double[],long[],double[],long[]);
+void divahc(void);
+void divain(double[],double[],double[],long[]);
+void divaop(long[],double[]);
+void divapr(double[],double[],double[],long[]);
+void dmess(long[],CHAR_INT,long[],double[]);
+void mess(long[],CHAR_INT,long[]);
+void optchk(long[],long[],STRING);
