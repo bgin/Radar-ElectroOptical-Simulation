0b4a68bf6ea5fc11043bb9b2beac516edce62927
Bernard Gingold <beniekg@gmail.com>
2022-02-13 17:41:56 +0100

    Added new function.

--- gpu/kernels/GMS_thermal_noise.cuh
+++ gpu/kernels/GMS_thermal_noise.cuh
@@ -30,6 +30,32 @@ void therm_noise_range_cuda(       const float Frdr,
 				   const float * __restrict d_La,
 				   float * __restrict d_Rm,
 				   const int nth);
+
+
+extern "C"
+void tropo_range_loss_cuda(        const float Frdr,
+                                   const float Kth,
+                                   const float rho,
+                                   const float tf
+                                   const float tr,
+				   const float th,
+				   const float * __restrict d_Pt,
+				   const float * __restrict d_Rmj,
+				   const float gamm,
+				   const float * __restrict d_w,
+				   const float * __restrict d_h,
+				   const float Ln,
+				   const float * __restrict d_Ts,
+				   const float sig,
+				   const float F,
+				   const float Fp,
+				   const float Flens,
+				   const float Dx,
+				   const float * __restrict d_Lt,
+				   const float * __restrict d_La,
+				   float * __restrict d_Rm,
+				   float * __restrict d_La1,
+				   const int nth);
 				  
 
 
