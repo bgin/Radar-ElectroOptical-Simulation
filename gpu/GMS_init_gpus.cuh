153cc69433354f6e44e9d1705eaacf047696d63b
Bernard Gingold <beniekg@gmail.com>
2022-02-14 17:04:27 +0100

    Added new functions.
    WIP

--- gpu/GMS_init_gpus.cuh
+++ gpu/GMS_init_gpus.cuh
@@ -4,8 +4,8 @@
 
 
 #include <stdint.h>
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
 
 
 //
@@ -24,6 +24,68 @@ void devcaps_to_screen(const int32_t,
 //
 void devcaps_to_file(const int32_t, const char *, 
 		     int32_t * , const bool );
+
+//
+//   Taken and adapted slightly from the cuda-samples   
+//
+/* Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+ // Defines for GPU Architecture types (using the SM version to determine
+  // the # of cores per SM
+int32_t convertSMVer2Cores(const int32_t,
+                           const int32_t);
+
+  // Defines for GPU Architecture types (using the SM version to determine
+  // the GPU Arch name)
+const char * convertSMVer2ArchName(const int32_t,
+                                   const int32_t);
+
+
+#ifdef __CUDA_RUNTIME_H__
+int32_t gpuDeviceInit(const int32_t);
+
+// The device with highest Gflops throughput
+int32_t gpuGetMaxGflopsDevId();
+
+// General check for CUDA GPU SM capabilities
+bool checkCudaCapabilities(const int32_t,
+                           const int32_t);
+
+// Finds the integrated GPU compute-capable
+int32_t findIntegratedGPU();
+
+// Finds the best CUDA device
+int32_t findCudaDevice();
+#endif
+
+
+
+
 
 
 //
