1a8e619936ecda0c4cf944f2653eec1b7f444a91
Bernard Gingold <beniekg@gmail.com>
2022-02-16 18:54:33 +0100

    Added more wrappers.
    WIP

--- gpu/GMS_cuBLAS_L1_wrappers.cuh
+++ gpu/GMS_cuBLAS_L1_wrappers.cuh
@@ -18,7 +18,7 @@ cuBLAS_Isamax_iface(const float * __restrict,
                     const int32_t,
                     const int32_t,
                     int32_t *     __restrict,
-                    cublasStatus_t * __restrict,
+                    cudaError_t * __restrict,
                     int32_t * __restrict,
                     uint64_t * __restrict) 
                                        __ATTR_HOT__
@@ -29,7 +29,7 @@ cuBLAS_Icamax_iface(const cuComplex * __restrict,
                     const int32_t,
                     const int32_t,
                     int32_t *  __restrict,
-                    cublasStatus_t * __restrict,
+                    cudaError_t * __restrict,
                     int32_t * __restrict,
                     uint64_t * __restrict)  
                                        __ATTR_HOT__
@@ -37,6 +37,74 @@ cuBLAS_Icamax_iface(const cuComplex * __restrict,
 
 
 
+void
+cuBLAS_Isamin_iface(const float * __restrict,
+                    const int32_t,
+                    const int32_t,
+                    int32_t *     __restrict,
+                    cudaError_t * __restrict,
+                    int32_t * __restrict,
+                    uint64_t * __restrict) 
+                                       __ATTR_HOT__
+                                       __ATTR_ALIGN__(32);
+
+
+void
+cuBLAS_Icamin_iface(const cuComplex * __restrict,
+                    const int32_t,
+                    const int32_t,
+                    int32_t *  __restrict,
+                    cudaError_t * __restrict,
+                    int32_t * __restrict,
+                    uint64_t * __restrict)  
+                                       __ATTR_HOT__
+                                       __ATTR_ALIGN__(32);
+
+
+void
+cuBLAS_Sasum_iface(const float * __restrict,
+                   const int32_t,
+                   const int32_t,
+                   float * __restrict,
+                   cudaError_t * __restrict,
+                   int32_t * __restrict,
+                   uint64_t * __restrict) 
+                                        __ATTR_HOT__
+                                       __ATTR_ALIGN__(32);
+
+
+
+void
+cuBLAS_Scasum_iface(const cuComplex * __restrict,
+                   const int32_t,
+                   const int32_t,
+                   cuComplex * __restrict,
+                   cudaError_t * __restrict,
+                   int32_t * __restrict,
+                   uint64_t * __restrict) 
+                                        __ATTR_HOT__
+                                       __ATTR_ALIGN__(32);
+
+
+void
+cuBLAS_Sgemm_iface(cublasOperation_t,
+                   cublasOperation_t,
+                   const int32_t,
+                   const int32_t,
+                   const int32_t,
+                   const float,
+                   const float * __restrict,
+                   const int32_t,
+                   const float * __restrict,
+                   const int32_t,
+                   float,
+                   float * __restrict,
+                   const int32_t,
+                   cudaError_t * __restrict,
+                   int32_t * __restrict,
+                   uint64_t * __restrict) 
+                                       __ATTR_HOT__
+                                       __ATTR_ALIGN__(32);
 
 
 
