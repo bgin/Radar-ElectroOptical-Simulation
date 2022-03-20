5d7a6899e685cdf4a78ffac0a0558af01734c535
Bernard Gingold <beniekg@gmail.com>
2022-03-20 17:18:17 +0100

    Add files via upload
    wip

--- gpu/kernels/GMS_platform_error_kernels.cuh
+++ gpu/kernels/GMS_platform_error_kernels.cuh
@@ -18,7 +18,53 @@ void platform_orient_err_cuda(const float * __restrict__ ,
                               const uint32_t);
 
 
+extern "C"
+void platform_pos_err_cuda(const float * __restrict__,
+                           const float * __restrict__,
+                           const float * __restrict__,
+                           const float * __restrict__,
+                           const float * __restrict__,
+                           const float * __restrict__,
+                           float * __restrict__ , //min, azimuth error
+                           float * __restrict__ ,  //min, elevation error
+                           float * __restrict__,    //m,   range error
+                           const uint32_t,
+                           const uint32_t,
+                           const uint32_t);
 
+
+extern "C"
+void elev_mpath_err_cuda(   const float * __restrict__,
+                            const float * __restrict__,
+                            const float * __restrict__,
+                            const float,
+                            const float,
+                            const float,
+                            const float * __restrict__,
+                            const float * __restrict__,
+                            const float * __restrict__,
+                            float * __restrict__, // deg, elevation multipath error
+                            const uint32_t,
+                            const uint32_t,
+                            const uint32_t);
+
+
+extern "C"
+void elev_refract_err_cuda(     const float * __restrict__,
+                                const float * __restrict__,
+                                const float * __restrict__,
+                                const float,
+                                const float,
+                                const float,
+                                const float * __restrict__,
+                                const float * __restrict__,
+                                const float * __restrict__, 
+                                const float,
+                                const int32_t,
+                                float * __restrict__, //deg, rms error of elevation measurement
+                                const uint32_t,
+                                const uint32_t,
+                                const uint32_t);
 
 
 
