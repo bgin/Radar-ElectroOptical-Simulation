9973c3ae5465dfc6755fcdeadf3f4a97142db242
Bernard Gingold <beniekg@gmail.com>
2022-04-17 16:41:54 +0200

    minor changes.

--- Common/GMS_indices.h
+++ Common/GMS_indices.h
@@ -7,18 +7,19 @@
 */
 
 // To be used with CUDA
-#ifndef INDEX2
+/*
+     When used in this for loop configuration -- traversing is column-order
+     for(i ...
+         for(j ....
+     When used in this for loop configuration -- traversing is row-order
+     for(j ...
+        for(i ....
+*/
 #define INDEX2(isize,i,j) i + isize*j
-#endif
-#ifndef INDEX3
 #define INDEX3(isize,jsize,i,j,k) i + isize*(j + jsize*k)
-#endif
-#ifndef INDEX4
 #define INDEX4(isize,jsize,ksize,i,j,k,x) i + isize*(j + jsize*(k + ksize*x))
-#endif
-#ifndef INDEX5
 #define INDEX5(isize,jsize,ksize,xsize,i,j,k,x,y) i + isize*(j + jsize*(k + ksize*(x + xsize*y)))
-#endif
+
 
 /*#define I2D(i,j,dim0)                              i+j*dim0
 #define I3D(i,j,dim0,k,dim1)                       i+j*dim0+k*dim0*dim1
@@ -28,24 +29,32 @@
 */
 
 //More optimized version
-#define I2D(i,j,dim0)                                        i+j*dim0
-#define I3D(i,j,dim0,k,dim01)                                i+j*dim0+k*dim01
-#define I4D(i,j,dim0,k,dim01,l,dim012)                       i+j*dim0+k*dim01+l*dim012
-#define I5D(i,j,dim0,k,dim01,l,dim012,m,dim0123)             i+j*dim0+k*dim01+l*dim012+m*dim0123
-#define I6D(i,j,dim0,k,dim01,l,dim012,m,dim0123,n,dim01234)  i+j*dim0+k*dim01+l*dim012+m*dim0123+n*dim01234
+/*
+     When used in this for loop configuration -- traversing is column-order
+     for(i ...
+         for(j ....
+     When used in this for loop configuration -- traversing is row-order
+     for(j ...
+        for(i ....
+*/
+#define Index2D(i,j,dim0)                                        i+j*dim0
+#define Index3D(i,j,dim0,k,dim01)                                i+j*dim0+k*dim01
+#define Index4D(i,j,dim0,k,dim01,l,dim012)                       i+j*dim0+k*dim01+l*dim012
+#define Index5D(i,j,dim0,k,dim01,l,dim012,m,dim0123)             i+j*dim0+k*dim01+l*dim012+m*dim0123
+#define Index6D(i,j,dim0,k,dim01,l,dim012,m,dim0123,n,dim01234)  i+j*dim0+k*dim01+l*dim012+m*dim0123+n*dim01234
 
 /*
 	@Warning:
 				Macro parameters named idim,jdim,kdim,ldim
 				must be present in the calling scope.
 */
-//#define Idx2D(i,j)       ((i) * jdim + (j))
+#define Idx2D(i,j)       ((i) * jdim + (j))
 
-//#define Idx3D(i,j,k)     ((i) * jdim + ((j) * kdim + (k)))
+#define Idx3D(i,j,k)     ((i) * jdim + ((j) * kdim + (k)))
 
-//#define Idx4D(i,j,k,l)   ((i) + idim * ((j) + jdim * ((k) + kdim * (l))))
+#define Idx4D(i,j,k,l)   ((i) + idim * ((j) + jdim * ((k) + kdim * (l))))
 
-//#define Idx5D(i,j,k,l,m) ((i) + idim * ((j) + jdim * (k) + kdim * (l) + ldim * (m)))
+#define Idx5D(i,j,k,l,m) ((i) + idim * ((j) + jdim * (k) + kdim * (l) + ldim * (m)))
 
 /*
 	@Warning:
@@ -53,11 +62,11 @@
 				present in the calling scope.
 */
 
-//#define I2D(i,j)    ((i) * ny + (j))
+#define I2D(i,j)    ((i) * ny + (j))
 
-//#define I3D(i,j,k)  ((i) * ny + ((j) * nz + (k)))
+#define I3D(i,j,k)  ((i) * ny + ((j) * nz + (k)))
 
-//#define I4D(i,j,k,l)  ((i) + nx * ((j) + ny * ((k) + nz * ((l)))))
+#define I4D(i,j,k,l)  ((i) + nx * ((j) + ny * ((k) + nz * ((l)))))
 
 /*
 	In this case dimension parameters 
@@ -65,9 +74,9 @@
 				
 */
 
-#define Ix2D(i,ny,j) ((i) * (ny) + (j))
+#define Ix2D(i,ny,j) i * ny + j
 
-#define Ix3D(i,ny,j,nz,k) ((i) * (ny) + ((j) * (nz) + (k)))
+#define Ix3D(i,ny,j,nz,k) i * ny + j * nz + k
 
 // Different indexing scheme.
 
@@ -77,17 +86,17 @@
 				Macro parameters named idim,jdim,kdim,ldim
 				must be present in the calling scope.
 */
-//#define IS2D(i,idim,j)   i * jdim + j
+#define IS2D(i,idim,j)   i * jdim + j
 
-//#define IS3D(i,idim,j,jdim,k) i + idim * (j + jdim * k)
+#define IS3D(i,idim,j,jdim,k) i + idim * (j + jdim * k)
 
-//#define IS4D(i,idim,j,jdim,k,kdim,l) i + idim * (j + jdim * (k + kdim * l))
+#define IS4D(i,idim,j,jdim,k,kdim,l) i + idim * (j + jdim * (k + kdim * l))
 
-//#define IS5D(i,idim,j,jdim,k,kdim,l,ldim,m) i + idim * (j + jdim * (k + kdim + (l + ldim * m)))
+#define IS5D(i,idim,j,jdim,k,kdim,l,ldim,m) i + idim * (j + jdim * (k + kdim + (l + ldim * m)))
 
 // WRF indexing scheme
 // m_jme and m_kme must be present in the calling scope.
-//#define Dim3(j,k,i) ((j) + (m_jme) * ((k) + (m_kme) * (i)))
+#define Dim3(j,k,i) ((j) + (m_jme) * ((k) + (m_kme) * (i)))
 
 
 
