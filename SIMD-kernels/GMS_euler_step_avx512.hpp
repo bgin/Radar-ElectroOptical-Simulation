45221fbad445db1e3c362339fb343491af065709
Bernard Gingold <beniekg@gmail.com>
2022-06-01 15:03:04 +0200

    Euler step vectorized (AVX512).

---
+++ SIMD-kernels/GMS_euler_step_avx512.hpp
@@ -0,0 +1,129 @@
+
+
+#ifndef __GMS_EULER_STEP_AVX512_HPP__
+#define __GMS_EULER_STEP_AVX512_HPP__ 010620221448
+
+
+/*
+   Adapted from: http://www.mymathlib.com/diffeq/runge-kutta/improved_euler_method.html
+   Manually vectorized by Bernard Gingold, beniekg@gmail.com
+*/
+
+namespace file_version {
+
+    const unsigned int GMS_EULER_STEP_AVX512_MAJOR = 1U;
+    const unsigned int GMS_EULER_STEP_AVX512_MINOR = 0U;
+    const unsigned int GMS_EULER_STEP_AVX512_MICRO = 0U;
+    const unsigned int GMS_EULER_STEP_AVX512_FULLVER =
+      1000U*GMS_EULER_STEP_AVX512_MAJOR+
+      100U*GMS_EULER_STEP_AVX512_MINOR+
+      10U*GMS_EULER_STEP_AVX512_MICRO;
+    const char * const GMS_EULER_STEP_AVX512_CREATION_DATE = "01-06-2022 14:48 PM +00200 (WED 01 JUN 2022 GMT+2)";
+    const char * const GMS_EULER_STEPAVX512_BUILD_DATE    = __DATE__ ":" __TIME__;
+    const char * const GMS_EULER_STEP_AVX512_AUTHOR        = "Programmer: Bernard Gingold, contact: beniekg@gmail.com";
+    const char * const GMS_EULER_STEP_AVX512_DESCRIPTION   = "Vectorized (AVX512) Runge-Kutta order 4 step."
+
+}
+
+
+#include <immintrin.h>
+#include "GMS_config.h"
+
+namespace  gms {
+
+          namespace math {
+
+////////////////////////////////////////////////////////////////////////////////
+//  double Improved_Euler_Method( double (*f)(double, double), double y0,     //
+//                               double x0, double h, int number_of_steps );  //
+//                                                                            //
+//  Description:                                                              //
+//     This routine uses the improved Euler method to approximate the solution//
+//     at x = x0 + h * number_of_steps of the initial value problem y'=f(x,y) //
+//     y(x0) = y0.                                                            //
+//                                                                            //
+//  Arguments:                                                                //
+//     double *f                                                              //
+//            Pointer to the function which returns the slope at (x,y) of the //
+//            integral curve of the differential equation y' = f(x,y) which   //
+//            passes through the point (x0,y0).                               //
+//     double y0                                                              //
+//            The initial value of y at x = x0.                               //
+//     double x0                                                              //
+//            The initial value of x.                                         //
+//     double h                                                               //
+//            The step size.                                                  //
+//     int    number_of_steps                                                 //
+//            The number of steps. Must be a nonnegative integer.             //
+//                                                                            //
+//  Return Values:                                                            //
+//     The solution of the initial value problem y' = f(x,y), y(x0) = y0 at   //
+//     x = x0 + number_of_steps * h.                                          //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+
+                        __ATTR_ALWAYS_INLINE__
+                        __ATTR_HOT__
+                        __ATTR_ALIGN__(32)
+			__ATTR_VECTORCALL__
+	                static inline
+			__m512d
+			euler_step_zmm8r8(__m512d(*f)(__m512d,
+			                              __m512d),
+					  __m512d y0,
+					  __m512d x0,
+					  const __m512d h,
+					  const int32_t n) {
+
+                           const __m512d _0_5 = _mm512_set1_pd(0.5);
+			   const __m512d h2   = _mm512_mul_pd(_0_5,h);
+			   __m512d k1;
+			   while(--n >= 0) {
+                                 k1 = _mm512_mul_pd(h,f(x0,y0));
+				 y0 = _mm512_add_pd(y0,_mm512_mul_pd(h,
+				                                 f(_mm512_add_pd(x0,h2),
+								   _mm512_add_pd(y0,k1))));
+				 x0 = _mm512_add_pd(x0,h);
+			   }
+			   return (y0);
+		      }
+
+
+		      
+	               __ATTR_ALWAYS_INLINE__
+                        __ATTR_HOT__
+                        __ATTR_ALIGN__(32)
+			__ATTR_VECTORCALL__
+	                static inline
+			__m512
+			euler_step_zmm16r4(__m512(*f)(__m512,
+			                              __m512),
+					  __m512 y0,
+					  __m512 x0,
+					  const __m512 h,
+					  const int32_t n) {
+
+                           const __m512 _0_5 = _mm512_set1_ps(0.5f);
+			   const __m512 h2   = _mm512_mul_ps(_0_5,h);
+			   __m512 k1;
+			   while(--n >= 0) {
+                                 k1 = _mm512_mul_ps(h,f(x0,y0));
+				 y0 = _mm512_add_ps(y0,_mm512_mul_ps(h,
+				                                 f(_mm512_add_ps(x0,h2),
+								   _mm512_add_ps(y0,k1))));
+				 x0 = _mm512_add_ps(x0,h);
+			   }
+			   return (y0);
+		      }
+
+
+    } // math
+
+} //gms
+
+
+
+
+
+#endif /*__GMS_EULER_STEP_AVX512_HPP__*/
