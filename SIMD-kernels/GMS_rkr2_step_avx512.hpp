dead612d189b9189bcc385fcbfad72c61bb996b2
Bernard Gingold <beniekg@gmail.com>
2022-06-02 09:21:34 +0200

    Ralston order 2 step (AVX512).

---
+++ SIMD-kernels/GMS_rkr2_step_avx512.hpp
@@ -0,0 +1,137 @@
+
+
+#ifndef __GMS_RKR2_STEP_AVX512_HPP__
+#define __GMS_RKR2_STEP_AVX512_HPP__ 020620220847
+
+
+/*
+     Adapted from: http://www.mymathlib.com/diffeq/runge-kutta/runge_kutta_ralston_2.html
+     Manually vectorized by Bernard Gingold, beniekg@gmail.com
+*/
+
+namespace file_version {
+
+    const unsigned int GMS_RKR2_STEP_AVX512_MAJOR = 1U;
+    const unsigned int GMS_RKR2_STEP_AVX512_MINOR = 0U;
+    const unsigned int GMS_RKR2_STEP_AVX512_MICRO = 0U;
+    const unsigned int GMS_RKR2_STEP_AVX512_FULLVER =
+      1000U*GMS_RKR2_STEP_AVX512_MAJOR+
+      100U*GMS_RKR2_STEP_AVX512_MINOR+
+      10U*GMS_RKR2_STEP_AVX512_MICRO;
+    const char * const GMS_RKR2_STEP_AVX512_CREATION_DATE = "02-06-2022 08:47 PM +00200 (THR 02 JUN 2022 GMT+2)";
+    const char * const GMS_RKR2_STEPAVX512_BUILD_DATE    = __DATE__ ":" __TIME__;
+    const char * const GMS_RKR2_STEP_AVX512_AUTHOR        = "Programmer: Bernard Gingold, contact: beniekg@gmail.com";
+    const char * const GMS_RKR2_STEP_AVX512_DESCRIPTION   = "Vectorized (AVX512) Heuns step."
+
+}
+
+
+#include <immintrin.h>
+#include <cstdint>
+#include "GMS_config.h"
+
+
+namespace  gms {
+
+
+        namespace math {
+
+////////////////////////////////////////////////////////////////////////////////
+//  double Runge_Kutta_Ralston_2_Method( double (*f)(double, double),         //
+//                    double y0, double x0, double h, int number_of_steps );  //
+//                                                                            //
+//  Description:                                                              //
+//     This routine uses the second order Ralston method to approximate the   //
+//     solution at x = x0 + h * number_of_steps of the initial value problem  //
+//     y'=f(x,y), y(x0) = y0.                                                 //
+//                                                                            //
+//  Arguments:                                                                //
+//     double *f                                                              //
+//            Pointer to the function which returns the slope at (x,y) of the //
+//            integral curve of the differential equation y' = f(x,y) which   //
+//            passes through the point (x0,y0).                               //
+//     double y0                                                              //
+//            The initial value of y at x = x0.                               //
+//     double x0                                                              //
+//            The initial value of x.                                         //
+//     double h                                                               //
+//            The step size.                                                  //
+//     int    number_of_steps                                                 //
+//            The number of steps. Must be a nonnegative integer.             //
+//                                                                            //
+//  Return Values:                                                            //
+//     The solution of the initial value problem y' = f(x,y), y(x0) = y0 at   //
+//     x = x0 + number_of_steps * h.                                          //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+
+                        __ATTR_ALWAYS_INLINE__
+                        __ATTR_HOT__
+                        __ATTR_ALIGN__(32)
+			__ATTR_VECTORCALL__
+	                static inline
+			__m512d
+			rkr2_step_zmm8r8(__m512d(*f)(__m512d,
+			                             __m512d),
+					 __m512d y0,
+					 __m512d x0,
+					 const __m512d h,
+					 const int32_t n) {
+
+                           const __m512d _2_3 = _mm512_set1_pd(0.6666666666666666666667);
+			   const __m512d _0_25= _mm512_set1_pd(0.25);
+			   const __m512d _3   = _mm512_set1_pd(3.0);
+			   const __m512d h3   = _mm512_mul_pd(_2_3,h);
+			   const __m512d h4   = _mm512_mul_pd(h,_0_25);
+			   __m512d k1;
+			   while(--n >= 0) {
+                                 k1 = f(x0,y0);
+				 y0 = _mm512_add_pd(y0,_mm512_mul_pd(h4,
+				                                 _mm512_fmadd_pd(_3,f(_mm512_add_pd(x0,h3),
+								                      _mm512_fmadd_pd(h3,k1,y0)),k1)));
+				 x0 = _mm512_add_pd(x0,h);
+			   }
+			   return (y0);
+		     }
+
+
+		        __ATTR_ALWAYS_INLINE__
+                        __ATTR_HOT__
+                        __ATTR_ALIGN__(32)
+			__ATTR_VECTORCALL__
+	                static inline
+			__m512
+			rkr2_step_zmm16r4(__m512(*f)(__m512,
+			                             __m512),
+					 __m512 y0,
+					 __m512 x0,
+					 const __m512 h,
+					 const int32_t n) {
+
+                           const __m512 _2_3 = _mm512_set1_ps(0.6666666666666666666667f);
+			   const __m512 _0_25= _mm512_set1_ps(0.25f);
+			   const __m512 _3   = _mm512_set1_ps(3.0f);
+			   const __m512 h3   = _mm512_mul_ps(_2_3,h);
+			   const __m512 h4   = _mm512_mul_ps(h,_0_25);
+			   __m512 k1;
+			   while(--n >= 0) {
+                                 k1 = f(x0,y0);
+				 y0 = _mm512_add_ps(y0,_mm512_mul_ps(h4,
+				                                 _mm512_fmadd_ps(_3,f(_mm512_add_ps(x0,h3),
+								                      _mm512_fmadd_ps(h3,k1,y0)),k1)));
+				 x0 = _mm512_add_ps(x0,h);
+			   }
+			   return (y0);
+		     }
+
+     } //math
+
+
+
+} //gms
+
+
+
+
+#endif /*__GMS_RKR2_STEP_AVX512_HPP__*/
