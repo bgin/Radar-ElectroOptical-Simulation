9ff8f5c70487cce8639d25d0cabe3f2cc4b9dbba
Bernard Gingold <beniekg@gmail.com>
2022-06-07 12:16:11 +0200

    Added Richardson extrapolation (avx512).

--- SIMD-kernels/GMS_rkg_step_avx512.hpp
+++ SIMD-kernels/GMS_rkg_step_avx512.hpp
@@ -177,6 +177,137 @@ namespace gms {
 			 }
 			 return (y0);
 		     }
+////////////////////////////////////////////////////////////////////////////////
+//  double Runge_Kutta_Gill_Richardson( double (*f)(double, double),          //
+//                       double y0, double x0, double h, int number_of_steps, //
+//                                                   int richardson_columns)  //
+//                                                                            //
+//  Description:                                                              //
+//     This routine uses the 4th order Runge-Kutta method described above     //
+//     together with Richardson extrapolation to approximate the solution at  //
+//     x = x0 + h * number_of_steps of the initial value problem y'=f(x,y),   //
+//     y(x0) = y0.                                                            //
+//                                                                            //
+//  Arguments:                                                                //
+//     double *f                                                              //
+//            Pointer to the function which returns the slope at (x,y) of the //
+//            integral curve of the differential equation y' = f(x,y) which   //
+//            passes through the point (x0,y0).                               //
+//     double y0                                                              //
+//            The initial value of y at x = x0.                               //
+//     double x0                                                              //
+//            The initial value of x.                                         //
+//     double h                                                               //
+//            The step size.                                                  //
+//     int    number_of_steps                                                 //
+//            The number of steps. Must be nonnegative.                       //
+//     int    richardson_columns                                              //
+//            The maximum number of columns to use in the Richardson          //
+//            extrapolation to the limit.                                     //
+//                                                                            //
+//  Return Values:                                                            //
+//     The solution of the initial value problem y' = f(x,y), y(x0) = y0 at   //
+//     x = x0 + number_of_steps * h.                                          //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+#include <algorithm>
+
+
+                        __ATTR_ALWAYS_INLINE__
+                        __ATTR_HOT__
+                        __ATTR_ALIGN__(32)
+			__ATTR_VECTORCALL__
+	                static inline
+			__m512d
+			rkg_richardson_zmm8r8(__m512d(*f)(__m512d,
+			                                     __m512d),
+						 __m512d y0,
+						 __m512d x0,
+						 const __m512d h,
+						 const int32_t n,
+						 int32_t n_cols) {
+
+			   __ATTR_ALIGN__(64) const __m512d richardson[] = {_mm512_set1_pd(1.0/15.0),
+			                                                    _mm512_set1_pd(1.0/31.0),
+								            _mm512_set1_pd(1.0/63.0),
+								            _mm512_set1_pd(1.0/127.0),
+								            _mm512_set1_pd(1.0/255.0),
+									    _mm512_set1_pd(1.0/511.0)};
+			   constexpr int32_t MAX_COLS = 7;
+			   __ATTR_ALIGN__(64) __m512d dt[MAX_COLS];
+			   const __m512d _1_2 = _mm512_set1_pd(0.5);
+			   __m512d integral,delta,h_used;
+			   int32_t n_subints;
+			   n_cols = std::max(1,std::min(MAX_COLS,n_cols));
+			   while(--n >= 0) {
+                                 h_used = h;
+				 n_subints = 1;
+				 #pragma loop_count min(1),max(6),avg(3)
+				 for(int32_t j = 0; j < n_cols; ++j) {
+                                     integral = rkg_step_zmm8r8(f,y0,x0,h_used,n_subints);
+				     for(int32_t k = 0; k < j; ++k) {
+                                         delta = _mm512_sub_pd(integral,dt[k]);
+					 dt[k] = integral;
+					 integral = _mm512_fmadd_pd(richardson[k],delta,integral);
+				     }
+				     dt[j] = integral;
+				     h_used = _mm512_mul_pd(h_used,_1_2);
+				     n_subints += n_subints;
+				 }
+				 y0 = integral;
+				 x0 = _mm512_add_pd(x0,h);
+			   }
+			   return (y0);
+		    }
+
+
+		        __ATTR_ALWAYS_INLINE__
+                        __ATTR_HOT__
+                        __ATTR_ALIGN__(32)
+			__ATTR_VECTORCALL__
+	                static inline
+			__m512
+			rkg_richardson_zmm16r4(__m512(*f)(__m512,
+			                                     __m512),
+						 __m512 y0,
+						 __m512 x0,
+						 const __m512 h,
+						 const int32_t n,
+						 int32_t n_cols) {
+
+			   __ATTR_ALIGN__(64) const __m512 richardson[] = { _mm512_set1_ps(1.0f/15.0f),
+			                                                    _mm512_set1_ps(1.0f/31.0f),
+								            _mm512_set1_ps(1.0f/63.0f),
+								            _mm512_set1_ps(1.0f/127.0f),
+								            _mm512_set1_ps(1.0f/255.0f),
+									    _mm512_set1_ps(1.0f/511.0f)};
+			   constexpr int32_t MAX_COLS = 7;
+			   __ATTR_ALIGN__(64) __m512 dt[MAX_COLS];
+			   const __m512 _1_2 = _mm512_set1_ps(0.5f);
+			   __m512 integral,delta,h_used;
+			   int32_t n_subints;
+			   n_cols = std::max(1,std::min(MAX_COLS,n_cols));
+			   while(--n >= 0) {
+                                 h_used = h;
+				 n_subints = 1;
+				 #pragma loop_count min(1),max(6),avg(3)
+				 for(int32_t j = 0; j < n_cols; ++j) {
+                                     integral = rkg_step_zmm16r4(f,y0,x0,h_used,n_subints);
+				     for(int32_t k = 0; k < j; ++k) {
+                                         delta = _mm512_sub_ps(integral,dt[k]);
+					 dt[k] = integral;
+					 integral = _mm512_fmadd_ps(richardson[k],delta,integral);
+				     }
+				     dt[j] = integral;
+				     h_used = _mm512_mul_ps(h_used,_1_2);
+				     n_subints += n_subints;
+				 }
+				 y0 = integral;
+				 x0 = _mm512_add_ps(x0,h);
+			   }
+			   return (y0);
+		    }
+
 
 
    }
