96a1bfbdc0b032c342cf7026b858d1b511ca0bf1
Bernard Gingold <beniekg@gmail.com>
2022-06-18 10:32:44 +0200

    Dryden PSD vectorized (avx512).

--- SIMD-kernels/GMS_dryden_psd_avx512.hpp
+++ SIMD-kernels/GMS_dryden_psd_avx512.hpp
@@ -63,20 +63,20 @@ namespace gms {
                         __ATTR_ALIGN__(32)
 			__ATTR_VECTORCALL__
 	                static inline
-			__m512d gust_psd_Ug_zmm8r8(const __m512d sigma,
-			                           const __m512d L,
+			__m512d gust_psd_Ug_zmm8r8(const __m512d sigmau,
+			                           const __m512d Lu,
 						   const __m512d omega) {
 
 			     __m512d PhiUg;
                              const __m512d invpi    = _mm512_set1_pd(0.318309886183790671537767526745);
 			     const __m512d _2       = _mm512_set1_pd(2.0);
-			     const __m512d t0       = _mm512_mul_pd(L,omega);
+			     const __m512d t0       = _mm512_mul_pd(Lu,omega);
 			     const __m512d Lomega2  = _mm512_mul_pd(t0,t0);
 			     const __m512d _1       = _mm512_set1_pd(1.0);
-			     const __m512d sigma2   = _mm512_mul_pd(sigma,sigma);
-			     const __m512d L_ov_pi  = _mm512_mul_pd(_mm512_mul_pd(_2,L),invpi);
+			     const __m512d sigmau2  = _mm512_mul_pd(sigmau,sigmau);
+			     const __m512d L_ov_pi  = _mm512_mul_pd(_mm512_mul_pd(_2,Lu),invpi);
 			     const __m512d i_L_omega= _mm512_div_pd(_1,_mm512_add_pd(_1,Lomega));
-			     PhiUg                  = _mm512_mul_pd(sigma2,_mm512_mul_pd(L_ov_pi,i_L_omega));
+			     PhiUg                  = _mm512_mul_pd(sigmau2,_mm512_mul_pd(L_ov_pi,i_L_omega));
 			     return (PhiUg);
 		       }
 
@@ -87,22 +87,107 @@ namespace gms {
                         __ATTR_ALIGN__(32)
 			__ATTR_VECTORCALL__
 	                static inline
-			__m512 gust_psd_Ug_zmm16r4(const __m512 sigma,
-			                           const __m512 L,
+			__m512 gust_psd_Ug_zmm16r4(const __m512 sigmau,
+			                           const __m512 Lu,
 						   const __m512 omega) {
 
 			     __m512 PhiUg;
                              const __m512 invpi    = _mm512_set1_ps(0.318309886183790671537767526745f);
 			     const __m512 _2       = _mm512_set1_ps(2.0f);
-			     const __m512 t0       = _mm512_mul_ps(L,omega);
+			     const __m512 t0       = _mm512_mul_ps(Lu,omega);
 			     const __m512 Lomega2  = _mm512_mul_ps(t0,t0);
 			     const __m512 _1       = _mm512_set1_ps(1.0f);
-			     const __m512 sigma2   = _mm512_mul_ps(sigma,sigma);
-			     const __m512 L_ov_pi  = _mm512_mul_ps(_mm512_mul_ps(_2,L),invpi);
+			     const __m512 sigmau2   = _mm512_mul_ps(sigmau,sigmau);
+			     const __m512 L_ov_pi  = _mm512_mul_ps(_mm512_mul_ps(_2,Lu),invpi);
 			     const __m512 i_L_omega= _mm512_div_ps(_1,_mm512_add_ps(_1,Lomega));
-			     PhiUg                  = _mm512_mul_ps(sigma2,_mm512_mul_ps(L_ov_pi,i_L_omega));
+			     PhiUg                  = _mm512_mul_ps(sigmau2,_mm512_mul_ps(L_ov_pi,i_L_omega));
 			     return (PhiUg);
 		       }
+
+
+		        __ATTR_ALWAYS_INLINE__
+                        __ATTR_HOT__
+                        __ATTR_ALIGN__(32)
+			__ATTR_VECTORCALL__
+	                static inline
+			__m512d gust_psd_Vg_zmm8r8(const __m512d sigmav,
+			                           const __m512d Lv,
+						   const __m512d omega) {
+
+                           __m512d PhiVg;
+			   const __m512d invpi    = _mm512_set1_pd(0.318309886183790671537767526745);
+			   const __m512d _2       = _mm512_set1_pd(2.0);
+			   const __m512d _12      = _mm512_set1_pd(12.0);
+			   const __m512d _4       = _mm512_set1_pd(4.0);
+			   const __m512d sigmav2  = _mm512_mul_pd(sigmav,sigmav);
+			   const __m512d Lv_o_pi  = _mm512_mul_pd(_mm512_mul_pd(_2,Lv),invpi);
+			   const __m512d t0       = _mm512_mul_pd(sigmav2,Lv_o_pi);
+			   const __m512d Lvom     = _mm512_mul_pd(Lv,omega);
+			   const __m512d Lvom2    = _mm512_mul_pd(Lvom,Lvom);
+			   const __m512d num      = _mm512_fmadd_pd(_12,Lvom2,_1);
+			   const __m512d denom    = _mm512_fmadd_pd(_4,Lvom2,_1);
+			   const __m512d denom2   = _mm512_mul_pd(denom,denom);
+			   const __m512d ratio    = _mm512_div_pd(num,denom2);
+			   PhiVg                  = _mm512_mul_pd(t0,ratio);
+			   return (PhiVg);
+		       }
+
+
+		        __ATTR_ALWAYS_INLINE__
+                        __ATTR_HOT__
+                        __ATTR_ALIGN__(32)
+			__ATTR_VECTORCALL__
+	                static inline
+			__m512 gust_psd_Vg_zmm16r4(const __m512 sigmav,
+			                           const __m512 Lv,
+						   const __m512 omega) {
+
+                           __m512 PhiVg;
+			   const __m512 invpi    = _mm512_set1_ps(0.318309886183790671537767526745f);
+			   const __m512 _2       = _mm512_set1_ps(2.0f);
+			   const __m512 _12      = _mm512_set1_ps(12.0f);
+			   const __m512 _4       = _mm512_set1_ps(4.0f);
+			   const __m512 sigmav2  = _mm512_mul_ps(sigmav,sigmav);
+			   const __m512 Lv_o_pi  = _mm512_mul_ps(_mm512_mul_ps(_2,Lv),invpi);
+			   const __m512 t0       = _mm512_mul_ps(sigmav2,Lv_o_pi);
+			   const __m512 Lvom     = _mm512_mul_ps(Lv,omega);
+			   const __m512 Lvom2    = _mm512_mul_ps(Lvom,Lvom);
+			   const __m512 num      = _mm512_fmadd_ps(_12,Lvom2,_1);
+			   const __m512 denom    = _mm512_fmadd_ps(_4,Lvom2,_1);
+			   const __m512 denom2   = _mm512_mul_ps(denom,denom);
+			   const __m512 ratio    = _mm512_div_ps(num,denom2);
+			   PhiVg                 = _mm512_mul_ps(t0,ratio);
+			   return (PhiVg);
+		       }
+
+
+		        __ATTR_ALWAYS_INLINE__
+                        __ATTR_HOT__
+                        __ATTR_ALIGN__(32)
+			__ATTR_VECTORCALL__
+	                static inline
+			__m512d gust_psd_Wg_zmm8r8(const __m512d sigmaw,
+			                           const __m512d Lw,
+						   const __m512d omega) {
+						   
+                           __m512d PhiWg = gust_psd_Vg_zmm8r8(sigmaw,Lw,omega);
+			   return (PhiWg);
+			}
+
+
+			__ATTR_ALWAYS_INLINE__
+                        __ATTR_HOT__
+                        __ATTR_ALIGN__(32)
+			__ATTR_VECTORCALL__
+	                static inline
+			__m512 gust_psd_Wg_zmm16r4(const __m512 sigmaw,
+			                           const __m512 Lw,
+						   const __m512 omega) {
+						   
+                           __m512 PhiWg = gust_psd_Vg_zmm16r4(sigmaw,Lw,omega);
+			   return (PhiWg);
+			}
+
 
 
 			
