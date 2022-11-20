f11d0780bcf9de13aeefb938af4de29abeafa9e4
Bernard Gingold <beniekg@gmail.com>
2022-11-20 13:24:56 +0100

    Added more data types.
    WIP.

--- Radiolocation/GMS_antenna_types.h
+++ Radiolocation/GMS_antenna_types.h
@@ -148,6 +148,122 @@ namespace gms {
                       PAD_TO(0,12)
 #endif
               } H_r8_t;
+
+
+              typedef struct __ATTR_ALIGN__(32) JE_c4_t {
+                      // Complex magnetic current
+                      std::complex<float> * __restrict je_x;
+                      std::complex<float> * __restrict je_y;
+                      std::complex<float> * __restrict je_z;
+                      int32_t                          npts;
+#if (USE_STRUCT_PADDING) == 1
+                      PAD_TO(0,8)
+#endif
+              } JE_c4_t;
+
+
+              typedef struct __ATTR_ALIGN__(32) JM_c4_t {
+                      // Complex electric current
+                      std::complex<float> * __restrict jm_x;
+                      std::complex<float> * __restrict jm_y;
+                      std::complex<float> * __restrict jm_z;
+                      int32_t                          npts;
+#if (USE_STRUCT_PADDING) == 1
+                      PAD_TO(0,8)
+#endif
+              } JM_c4_t;
+
+
+              typedef struct __ATTR_ALIGN__(32) JE_c8_t {
+                      // Complex magnetic current
+                      std::complex<double> * __restrict je_x;
+                      std::complex<double> * __restrict je_y;
+                      std::complex<double> * __restrict je_z;
+                      int32_t                          npts;
+#if (USE_STRUCT_PADDING) == 1
+                      PAD_TO(0,8)
+#endif
+              } JE_c8_t;
+
+
+              typedef struct __ATTR_ALIGN__(32) JM_c8_t {
+                      // Complex electric current
+                      std::complex<double> * __restrict jm_x;
+                      std::complex<double> * __restrict jm_y;
+                      std::complex<double> * __restrict jm_z;
+                      int32_t                          npts;
+#if (USE_STRUCT_PADDING) == 1
+                      PAD_TO(0,8)
+#endif
+              } JM_c8_t;
+
+
+              typedef struct __ATTR_ALIGN__(64) JE_r4_t {
+                     // ! Complex Electric Current  decomposed into real and imaginary parts 
+                     // ! To be used mainly by the integrators.
+                      float * __restrict je_xr;
+                      float * __restrict je_xi;
+                      float * __restrict je_yr;
+                      float * __restrict je_yi;
+                      float * __restrict je_zr;
+                      float * __restrict je_zi;
+                      int32_t            npts;
+#if (USE_STRUCT_PADDING) == 1
+                      PAD_TO(0,12)
+#endif 
+              } JE_r4_t;
+
+
+              typedef struct __ATTR_ALIGN__(64) JM_r4_t {
+                     // ! Complex Magnetic Current  decomposed into real and imaginary parts 
+                     // ! To be used mainly by the integrators.
+                      float * __restrict jm_xr;
+                      float * __restrict jm_xi;
+                      float * __restrict jm_yr;
+                      float * __restrict jm_yi;
+                      float * __restrict jm_zr;
+                      float * __restrict jm_zi;
+                      int32_t            npts;
+#if (USE_STRUCT_PADDING) == 1
+                      PAD_TO(0,12)
+#endif 
+              } JM_r4_t;
+
+
+              typedef struct __ATTR_ALIGN__(64) JE_r8_t {
+                     // ! Complex Electric Current  decomposed into real and imaginary parts 
+                     // ! To be used mainly by the integrators.
+                      double * __restrict je_xr;
+                      double * __restrict je_xi;
+                      double * __restrict je_yr;
+                      double * __restrict je_yi;
+                      double * __restrict je_zr;
+                      double * __restrict je_zi;
+                      int32_t            npts;
+#if (USE_STRUCT_PADDING) == 1
+                      PAD_TO(0,12)
+#endif 
+              } JE_r8_t;
+
+
+              typedef struct __ATTR_ALIGN__(64) JM_r8_t {
+                     // ! Complex Magnetic Current  decomposed into real and imaginary parts 
+                     // ! To be used mainly by the integrators.
+                      double * __restrict jm_xr;
+                      double * __restrict jm_xi;
+                      double * __restrict jm_yr;
+                      double * __restrict jm_yi;
+                      double * __restrict jm_zr;
+                      double * __restrict jm_zi;
+                      int32_t            npts;
+#if (USE_STRUCT_PADDING) == 1
+                      PAD_TO(0,12)
+#endif 
+              } JM_r8_t;
+
+
+
+              
                
        }
 
