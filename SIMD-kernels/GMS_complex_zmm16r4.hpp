e3637ea10597499efa2b61cbf952aeddd5a14000
Bernard Gingold <beniekg@gmail.com>
2022-12-25 10:13:19 +0100

    short complex vector kernels (avx512-sp)
    WIP.

---
+++ SIMD-kernels/GMS_complex_zmm16r4.hpp
@@ -0,0 +1,114 @@
+
+#ifndef __GMS_COMPLEX_ZMM16R4_HPP__
+#define __GMS_COMPLEX_ZMM16R4_HPP__ 251220220954
+
+
+/*MIT License
+Copyright (c) 2020 Bernard Gingold
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+namespace file_version {
+
+    const unsigned int GMS_COMPLEX_ZMM16R4_MAJOR = 1U;
+    const unsigned int GMS_COMPLEX_ZMM16R4_MINOR = 0U;
+    const unsigned int GMS_COMPLEX_ZMM16R4_MICRO = 0U;
+    const unsigned int GMS_COMPLEX_ZMM16R4_FULLVER =
+      1000U*GMS_COMPLEX_ZMM16R4_MAJOR+
+      100U*GMS_COMPLEX_ZMM16R4_MINOR+
+      10U*GMS_COMPLEX_ZMM16R4_MICRO;
+    const char * const GMS_COMPLEX_ZMM16R4_CREATION_DATE = "25-12-2022 09:54 AM +00200 (SUN 25 DEC 2022 GMT+2)";
+    const char * const GMS_COMPLEX_ZMM16R4_BUILD_DATE    = __DATE__ ":" __TIME__;
+    const char * const GMS_COMPLEX_ZMM16R4_AUTHOR        = "Programmer: Bernard Gingold, contact: beniekg@gmail.com";
+    const char * const GMS_COMPLEX_ZMM16R4_DESCRIPTION   = "AVX512 optimized complex number implementation."
+
+}
+
+#include <cstdint>
+#include <immintrin.h>
+#include "GMS_config.h"
+
+
+namespace  gms {
+
+
+       namespace math {
+
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void cadd_zmm16r4_u(const float * __restrict xre,
+                                       const float * __restrict xim,
+                                       const float * __restrict yre,
+                                       const float * __restrict yim,
+                                       float *       __restrict zre,
+                                       float *       __restrict zim) {
+
+                        register __m512 zmm0,zmm1,zmm2,zmm3;
+                        zmm0  = _mm512_loadu_ps(&xre[0]);
+                        zmm1  = _mm512_loadu_ps(&yre[0]);
+                        _mm512_storeu_ps(&zre[0], _mm512_add_ps(zmm0,zmm1));
+                        zmm2  = _mm512_loadu_ps(&xim[0]);
+                        zmm3  = _mm512_loadu_ps(&yim[0]);
+                        _mm512_storeu_ps(&zim[0], _mm512_add_ps(zmm2,zmm3));
+                 }
+
+
+                  __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void cadd_zmm16r4_a(const float * __restrict __ATTR_ALIGN__(64) xre,
+                                       const float * __restrict __ATTR_ALIGN__(64) xim,
+                                       const float * __restrict __ATTR_ALIGN__(64) yre,
+                                       const float * __restrict __ATTR_ALIGN__(64) yim,
+                                       float *       __restrict __ATTR_ALIGN__(64) zre,
+                                       float *       __restrict __ATTR_ALIGN__(64) zim) {
+
+                        register __m512 zmm0,zmm1,zmm2,zmm3;
+                        zmm0  = _mm512_load_ps(&xre[0]);
+                        zmm1  = _mm512_load_ps(&yre[0]);
+                        _mm512_store_ps(&zre[0], _mm512_add_ps(zmm0,zmm1));
+                        zmm2  = _mm512_load_ps(&xim[0]);
+                        zmm3  = _mm512_load_ps(&yim[0]);
+                        _mm512_store_ps(&zim[0], _mm512_add_ps(zmm2,zmm3));
+                 }
+
+
+      } // math
+
+
+} // gms
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /*__GMS_COMPLEX_ZMM16R4_HPP__*/
