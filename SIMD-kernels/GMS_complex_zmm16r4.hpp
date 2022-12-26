b0f8cbd5923adeeca4e7c643aec9e4e7eac90faa
Bernard Gingold <beniekg@gmail.com>
2022-12-26 16:27:20 +0100

    minor fix added.

--- SIMD-kernels/GMS_complex_zmm16r4.hpp
+++ SIMD-kernels/GMS_complex_zmm16r4.hpp
@@ -237,7 +237,7 @@ namespace  gms {
 	           __ATTR_ALIGN__(32)
                    __ATTR_VECTORCALL__
 	           static inline
-                   void cadd_zmm16r4(const __m512 xre,
+                   void csub_zmm16r4(const __m512 xre,
                                      const __m512 xim,
                                      const __m512 s,
                                      __m512 &     zre,
@@ -253,7 +253,7 @@ namespace  gms {
 	           __ATTR_ALIGN__(32)
                    __ATTR_VECTORCALL__
 	           static inline
-                   void cadd_zmm16r4_uip(const float * __restrict xre,
+                   void csub_zmm16r4_uip(const float * __restrict xre,
                                          const float * __restrict xim,
                                          float *       __restrict zre,
                                          float *       __restrict zim) {
@@ -272,7 +272,7 @@ namespace  gms {
 	           __ATTR_ALIGN__(32)
                    __ATTR_VECTORCALL__
 	           static inline
-                   void cadd_zmm16r4_aip(const float * __restrict __ATTR_ALIGN__(64) xre,
+                   void csub_zmm16r4_aip(const float * __restrict __ATTR_ALIGN__(64) xre,
                                          const float * __restrict __ATTR_ALIGN__(64) xim,
                                          float *       __restrict __ATTR_ALIGN__(64) zre,
                                          float *       __restrict __ATTR_ALIGN__(64) zim) {
@@ -284,6 +284,61 @@ namespace  gms {
                         _mm512_store_ps(&zre[0], _mm512_sub_ps(zmm2,zmm0));
                         _mm512_store_ps(&zim[0], _mm512_sub_ps(zmm3,zmm1));
               }
+
+
+               ////////////////////////////////////////////////////////////////////////
+
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void cmul_zmm16r4_u(const float * __restrict xre,
+                                       const float * __restrict xim,
+                                       const float * __restrict yre,
+                                       const float * __restrict yim,
+                                       float *       __restrict zre,
+                                       float *       __restrict zim) {
+
+                           register __m512 zmm0,zmm1,zmm2,zmm3,zmm4,zmm5;
+                           zmm0  = _mm512_loadu_ps(&xre[i+0]);
+                           zmm1  = _mm512_loadu_ps(&yre[i+0]);
+                           zmm2  = _mm512_loadu_ps(&xim[i+0]);
+                           zmm3  = _mm512_loadu_ps(&yim[i+0]);
+                           zmm4  = _mm512_sub_ps(_mm512_mul_ps(zmm0,zmm1),
+                                                                        _mm512_mul_ps(zmm2,zmm3));
+                           _mm512_storeu_ps(&zre[i+0], zmm4);
+                           zmm5  = _mm512_mul_ps(_mm512_mul_ps(zmm2,zmm1),
+                                                                        _mm512_mul_ps(zmm0,zmm3));
+                           _mm512_storeu_ps(&zim[i+0], zmm5);
+               }
+
+
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void cmul_zmm16r4_a(const float * __restrict __ATTR_ALIGN__(64) xre,
+                                       const float * __restrict __ATTR_ALIGN__(64) xim,
+                                       const float * __restrict __ATTR_ALIGN__(64) yre,
+                                       const float * __restrict __ATTR_ALIGN__(64) yim,
+                                       float *       __restrict __ATTR_ALIGN__(64) zre,
+                                       float *       __restrict __ATTR_ALIGN__(64) zim) {
+
+                           register __m512 zmm0,zmm1,zmm2,zmm3,zmm4,zmm5;
+                           zmm0  = _mm512_load_ps(&xre[i+0]);
+                           zmm1  = _mm512_load_ps(&yre[i+0]);
+                           zmm2  = _mm512_load_ps(&xim[i+0]);
+                           zmm3  = _mm512_load_ps(&yim[i+0]);
+                           zmm4  = _mm512_sub_ps(_mm512_mul_ps(zmm0,zmm1),
+                                                                        _mm512_mul_ps(zmm2,zmm3));
+                           _mm512_store_ps(&zre[i+0], zmm4);
+                           zmm5  = _mm512_mul_ps(_mm512_mul_ps(zmm2,zmm1),
+                                                                        _mm512_mul_ps(zmm0,zmm3));
+                           _mm512_store_ps(&zim[i+0], zmm5);
+               }
+
 
 
 
