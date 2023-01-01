5e3449f534958eb70106ab706752f0bd10c3588f
Bernard Gingold <beniekg@gmail.com>
2023-01-01 13:23:33 +0100

    cnorm-prod short kernels.

--- SIMD-kernels/GMS_complex_zmm16r4.hpp
+++ SIMD-kernels/GMS_complex_zmm16r4.hpp
@@ -1387,6 +1387,91 @@ namespace  gms {
               }
 
 
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void cnorm_prod_zmm16r4_u(const float * __restrict xre,
+                                             const float * __restrict xim,
+                                             const float * __restrict yre,
+                                             const float * __restrict yim,
+                                             float * __restrict zre,
+                                             float * __restrict zim) {
+
+                        register __m512 zmm0,zmm1,zmm2,zmm3;
+                        register __m512 rep,imp;
+                        zmm0 = _mm512_loadu_ps(&xre[0]);
+                        zmm1 = _mm512_loadu_ps(&yre[0]);
+                        zmm2 = _mm512_loadu_ps(&xim[0]);
+                        zmm3 = _mm512_loadu_ps(&yim[0]);
+                        rep  = _mm512_fmsub_ps(zmm0,zmm1,
+                                               _mm512_mul_ps(zmm2,zmm3));
+                        imp  = _mm512_fmadd_pd(zmm2,zmm1,
+                                               _mm512_mul_ps(zmm0,zmm3));
+                        zmm0 = _mm512_mul_ps(rep,rep);
+                        zmm1 = _mm512_mul_ps(imp,imp);
+                        zmm2 = _mm512_sqrt_ps(_mm512_add_ps(zmm0,zmm1));
+                        _mm512_storeu_ps(&zre[0], _mm512_div_ps(rep,zmm2));
+                        _mm512_storeu_ps(&zim[0], _mm512_div_ps(imp,zmm2));
+              }
+
+
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void cnorm_prod_zmm16r4_a(const float * __restrict __ATTR_ALIGN__(64) xre,
+                                             const float * __restrict __ATTR_ALIGN__(64) xim,
+                                             const float * __restrict __ATTR_ALIGN__(64) yre,
+                                             const float * __restrict __ATTR_ALIGN__(64) yim,
+                                             float * __restrict __ATTR_ALIGN__(64) zre,
+                                             float * __restrict __ATTR_ALIGN__(64) zim) {
+
+                        register __m512 zmm0,zmm1,zmm2,zmm3;
+                        register __m512 rep,imp;
+                        zmm0 = _mm512_load_ps(&xre[0]);
+                        zmm1 = _mm512_load_ps(&yre[0]);
+                        zmm2 = _mm512_load_ps(&xim[0]);
+                        zmm3 = _mm512_load_ps(&yim[0]);
+                        rep  = _mm512_fmsub_ps(zmm0,zmm1,
+                                               _mm512_mul_ps(zmm2,zmm3));
+                        imp  = _mm512_fmadd_pd(zmm2,zmm1,
+                                               _mm512_mul_ps(zmm0,zmm3));
+                        zmm0 = _mm512_mul_ps(rep,rep);
+                        zmm1 = _mm512_mul_ps(imp,imp);
+                        zmm2 = _mm512_sqrt_ps(_mm512_add_ps(zmm0,zmm1));
+                        _mm512_store_ps(&zre[0], _mm512_div_ps(rep,zmm2));
+                        _mm512_store_ps(&zim[0], _mm512_div_ps(imp,zmm2));
+              }
+
+
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void cnorm_prod_zmm16r4(  const __m512  xre,
+                                             const __m512  xim,
+                                             const __m512  yre,
+                                             const __m512  yim,
+                                             __m512 * __restrict zre,
+                                             __m512 * __restrict zim) {
+
+                        register __m512 rep,imp,zmm0,zmm1,zmm2;
+                        rep  = _mm512_fmsub_ps(xre,yre,
+                                               _mm512_mul_ps(xim,yim));
+                        imp  = _mm512_fmadd_pd(xim,yre,
+                                               _mm512_mul_ps(xre,yim));
+                        zmm0 = _mm512_mul_ps(rep,rep);
+                        zmm1 = _mm512_mul_ps(imp,imp);
+                        zmm2 = _mm512_sqrt_ps(_mm512_add_ps(zmm0,zmm1));
+                        *zre = _mm512_div_ps(rep,zmm2);
+                        *zim = _mm512_div_ps(imp,zmm2);
+             }
+
+
 
                 
 
