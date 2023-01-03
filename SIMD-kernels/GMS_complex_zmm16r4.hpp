6f0baca4a15f215da9ab70554d25015fceee2c8e
Bernard Gingold <beniekg@gmail.com>
2023-01-03 12:57:23 +0100

    cmagnitude short kernels.

--- SIMD-kernels/GMS_complex_zmm16r4.hpp
+++ SIMD-kernels/GMS_complex_zmm16r4.hpp
@@ -1979,6 +1979,68 @@ namespace  gms {
              }
 
 
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void cmagnitude_zmm16r4_u( const float * __restrict xre,
+                                              const float * __restrict xim,
+                                              const float * __restrict yre,
+                                              const float * __restrict yim,
+                                              float * __restrict mre) {
+
+                        register __m512 zmm0,zmm1,zmm2,zmm3;
+                        register __m512 cvmag;
+                        zmm0 = _mm512_loadu_ps(&xre[0]);
+                        zmm1 = _mm512_loadu_ps(&yre[0]);
+                        zmm2 = _mm512_loadu_ps(&xim[0]);
+                        zmm3 = _mm512_loadu_ps(&yim[0]);
+                        cvmag= _mm512_sqrt_ps(_mm512_fmadd_ps(zmm0,zmm1,
+                                                          _mm512_mul_ps(zmm2,zmm3)));
+                        _mm512_storeu_ps(&mre[0], cvmag);
+             }
+
+
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void cmagnitude_zmm16r4_a( const float * __restrict __ATTR_ALIGN__(64) xre,
+                                              const float * __restrict __ATTR_ALIGN__(64) xim,
+                                              const float * __restrict __ATTR_ALIGN__(64) yre,
+                                              const float * __restrict __ATTR_ALIGN__(64) yim,
+                                              float * __restrict __ATTR_ALIGN__(64) mre) {
+
+                        register __m512 zmm0,zmm1,zmm2,zmm3;
+                        register __m512 cvmag;
+                        zmm0 = _mm512_load_ps(&xre[0]);
+                        zmm1 = _mm512_load_ps(&yre[0]);
+                        zmm2 = _mm512_load_ps(&xim[0]);
+                        zmm3 = _mm512_load_ps(&yim[0]);
+                        cvmag= _mm512_sqrt_ps(_mm512_fmadd_ps(zmm0,zmm1,
+                                                          _mm512_mul_ps(zmm2,zmm3)));
+                        _mm512_store_ps(&mre[0], cvmag);
+             }
+
+
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void cmagnitude_zmm16r4(   const __m512 xre,
+                                              const __m512 xim,
+                                              const __m512 yre,
+                                              const __m512 yim,
+                                              __m512 * __restrict  mre) {
+
+                        register __m512 cvmag;
+                        cvmag= _mm512_sqrt_ps(_mm512_fmadd_ps(xre,yre,
+                                                          _mm512_mul_ps(xim,yim)));
+                        *mre = cvmag;
+             }
 
 
 
