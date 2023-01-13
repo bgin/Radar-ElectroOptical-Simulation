6af6bf31303e4c811958435b349b3f21c90b1e72
Bernard Gingold <beniekg@gmail.com>
2023-01-13 16:36:26 +0100

    S12_f3226_zmm16r4_u unaligned kernel.
    WIP.

--- Radiolocation/GMS_rcs_sphere_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_sphere_zmm16r4.hpp
@@ -1603,7 +1603,7 @@ namespace gms {
                        Complex scattering amplitude represented as a scattering function -- formula 3.2-26
                  */
 
-                  __ATTR_ALWAYS_INLINE__
+                   __ATTR_ALWAYS_INLINE__
 	           __ATTR_HOT__
 	           __ATTR_ALIGN__(32)
                    __ATTR_VECTORCALL__
@@ -1634,6 +1634,80 @@ namespace gms {
                         t3                         = _mm512_fmadd_ps(c4,k0a3,_1);
                         *S12i                      = _mm512_mul_ps(t2,t3);
                }
+
+
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void S12_f3226_zmm16r4_a(const float * __restrict __ATTR_ALIGN__(64) pk0a,
+                                            float * __restrict __ATTR_ALIGN__(64) S12r,
+                                            float * __restrict __ATTR_ALIGN__(64) S12i) {
+
+                        
+                        __m512 k0a2,k0a3,k0a4,k0a6;
+                        __m512 t0,t1,t2,t3;
+                        register __m512       k0a  = _mm512_load_ps(&pk0a[0]);
+                        const register __m512 nhlf = _mm512_set1_ps(-0.5f);
+                        k0a2                       = _mm512_mul_ps(k0a,k0a);
+                        const register __m512 _1   = _mm512_set1_ps(1.0f);
+                        k0a3                       = _mm512_mul_ps(k0a2,k0a);
+                        const register __m512 c0   = _mm512_set1_ps(113.0f/90.0f);
+                        k0a4                       = _mm512_mul_ps(k0a2,k0a2);
+                        const register __m512 c1   = _mm512_set1_ps(1783.0f/2100.0f);
+                        k0a6                       = _mm512_mul_ps(k0a3,k0a2);
+                        const register __m512 c2   = _mm512_set1_ps(670057.0f/396900.0f);
+                        t0                         = _mm512_mul_ps(nhlf,k0a3)
+                        const register __m512 c3   = _mm512_set1_ps(0.833333333333333333333333333333f);
+                        const register __m512 c4   = _mm512_set1_ps(0.24f);
+                        t1                         = _mm512_fmsub_ps(_mm512_add_ps(_1,c0),k0a2,
+                                                                 _mm512_fmsub_ps(c1,k0a4,
+                                                                             _mm512_mul_ps(c2,k0a6)));
+                        _mm512_store_ps(&S12r[0],  _mm512_mul_ps(t0,t1));
+                        t2                         = _mm512_mul_ps(Ini,_mm512_mul_ps(c3,k0a6));
+                        t3                         = _mm512_fmadd_ps(c4,k0a3,_1);
+                        _mm512_store_ps(&S12i[0],  _mm512_mul_ps(t2,t3));
+               }
+
+
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void S12_f3226_zmm16r4_u(const float * __restrict  pk0a,
+                                            float * __restrict  S12r,
+                                            float * __restrict  S12i) {
+
+                        
+                        __m512 k0a2,k0a3,k0a4,k0a6;
+                        __m512 t0,t1,t2,t3;
+                        register __m512       k0a  = _mm512_loadu_ps(&pk0a[0]);
+                        const register __m512 nhlf = _mm512_set1_ps(-0.5f);
+                        k0a2                       = _mm512_mul_ps(k0a,k0a);
+                        const register __m512 _1   = _mm512_set1_ps(1.0f);
+                        k0a3                       = _mm512_mul_ps(k0a2,k0a);
+                        const register __m512 c0   = _mm512_set1_ps(113.0f/90.0f);
+                        k0a4                       = _mm512_mul_ps(k0a2,k0a2);
+                        const register __m512 c1   = _mm512_set1_ps(1783.0f/2100.0f);
+                        k0a6                       = _mm512_mul_ps(k0a3,k0a2);
+                        const register __m512 c2   = _mm512_set1_ps(670057.0f/396900.0f);
+                        t0                         = _mm512_mul_ps(nhlf,k0a3)
+                        const register __m512 c3   = _mm512_set1_ps(0.833333333333333333333333333333f);
+                        const register __m512 c4   = _mm512_set1_ps(0.24f);
+                        t1                         = _mm512_fmsub_ps(_mm512_add_ps(_1,c0),k0a2,
+                                                                 _mm512_fmsub_ps(c1,k0a4,
+                                                                             _mm512_mul_ps(c2,k0a6)));
+                        _mm512_storeu_ps(&S12r[0],  _mm512_mul_ps(t0,t1));
+                        t2                         = _mm512_mul_ps(Ini,_mm512_mul_ps(c3,k0a6));
+                        t3                         = _mm512_fmadd_ps(c4,k0a3,_1);
+                        _mm512_storeu_ps(&S12i[0],  _mm512_mul_ps(t2,t3));
+               }
+
+
+
+                 
 
 
      } // radiolocation
