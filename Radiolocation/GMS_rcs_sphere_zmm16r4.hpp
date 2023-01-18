ddb8c701d355986ddda648664dcfa753a1d343ad
Bernard Gingold <beniekg@gmail.com>
2023-01-18 11:08:50 +0100

    rcs_f3310_zmm16r4 kernel.

--- Radiolocation/GMS_rcs_sphere_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_sphere_zmm16r4.hpp
@@ -3037,6 +3037,40 @@ namespace gms {
                         _mm512_storeu_ps(&S2i[0] ,_mm512_fmadd_ps(t1i,cost,div1i));
                }
 
+                   /*
+                         E-plane and H-plane RCS.
+                     */
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   __m512 rcs_f3310_zmm16r4(const __m512 tht,
+                                            const __m512 a,
+                                            const __m512 ka04,
+                                            const __m512 mm1r,
+                                            const __m512 mm1i) {
+
+                          const register __m512 _1 = _mm512_set1_ps(1.0f);
+                          const register __m512 aa = _mm512_mul_ps(a,a);
+                          const register __m512 _2 = _mm512_set1_ps(2.0f); 
+                          const register __m512 cost = xcosf(tht);
+                          const register __m512 _4pi = _mm512_set1_ps(12.566370614359172953850573533118f);
+                          register __m512 cos2t,mms1r,mms1i,mma2r,mma2i;
+                          register __m512 divr,divi,rcs,frac,cabs;
+                          cos2t   = _mm512_mul_ps(cost,cost);
+                          mms1r   = _mm512_sub_ps(mm1r,_1);
+                          mma2r   = _mm512_add_ps(mm1r,_2);
+                          mms1i   = _mm512_sub_ps(mm1i,_1);
+                          mma2i   = _mm512_add_ps(mm1i,_2);
+                          cdiv_zmm16r4(mms1r,mms1i,mma2r,mma2i,&divr,&divi);
+                          frac    = _mm512_mul_ps(_4pi,aa);
+                          cabs    = cabs_zmm16r4(divr,divi);
+                          rcs     = _mm512_mul_ps(_mm512_mul_ps(frac,cabs),
+                                                  _mm512_mul_ps(cos2t,ka04));
+                          return (rcs); 
+                 }
+
 
 
 
