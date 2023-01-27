b253719079d8b550d089bd749a72be7707dbd8e2
Bernard Gingold <beniekg@gmail.com>
2023-01-27 19:01:27 +0100

    rcs_f4140_zmm16r4_u unaligned kernel.
    WIP.

--- Radiolocation/GMS_rcs_cylinder_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_cylinder_zmm16r4.hpp
@@ -2400,6 +2400,28 @@ namespace gms {
                   }
 
 
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   __m512 rcs_f4140_zmm16r4_u(const float * __restrict  pk0a,
+                                              const float * __restrict  palpha) {
+
+                          register __m512 k0a   = _mm512_loadu_ps(&pk0a[0]);
+                          register __m512 alpha = _mm512_loadu_ps(&palpha[0]);
+                          register __m512 sinc,k0alp,k0as,t0;
+                          register __m512 rcs;
+                          const __m512 _4       = _mm512_set1_ps(4.0f);
+                          k0alp = _mm512_mul_ps(k0a,alpha);
+                          t0    = xsinf(k0alp);
+                          sinc  = _mm512_div_ps(t0,k0alp); 
+                          k0as  = _mm512_mul_ps(_4,_mm512_mul_ps(k0a,k0a)); 
+                          rcs   = _mm512_mul_ps(k0as,_mm512_mul_ps(sinc,sinc));
+                          return (rcs);        
+                  }
+
+
 
 
 
