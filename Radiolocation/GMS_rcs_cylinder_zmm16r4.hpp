964b3616dc047e5ed9fd64b9185a1fae85c1c049
Bernard Gingold <beniekg@gmail.com>
2023-01-30 12:36:07 +0100

    rcs_f4151_zmm16r4 kernel.
    WIP.

--- Radiolocation/GMS_rcs_cylinder_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_cylinder_zmm16r4.hpp
@@ -3426,6 +3426,118 @@ namespace gms {
                    }
 
 
+                    /*
+                         Forward scattering width (k0a<<1, k1a<<1), phi = pi
+                         Formula 4.1-51
+                     */
+
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   __m512 rcs_f4151_zmm16r4(const __m512 a,
+                                            const __m512 k0a,
+                                            const __m512 eps1,
+                                            const __m512 eps0,
+                                            const __m512 mu1,
+                                            const __m512 mu0) {
+
+                          register __m512 t0,t1,k0a3,epst,mut,sqr,t2,diff;
+                          register __m512 rcs;
+                          const __m512 pi4 = _mm512_set1_ps(0.78539816339744830961566084582f);
+                          t0               = _mm512_mul_ps(pi4,_mm512_mul_ps(PI,a));
+                          const __m512 _1  = _mm512_set1_ps(1.0f);
+                          k0a3             = _mm512_mul_ps(k0a,_mm512_mul_ps(k0a,k0a));
+                          const __m512 _2  = _mm512_set1_ps(2.0f);
+                          epst             = _mm512_sub_ps(_mm512_div_ps(eps1,eps0),_1);
+                          t1               = _mm512_sub_ps(mu1,mu0);
+                          t2               = _mm512_add_ps(mu1,mu0);
+                          mut              = _mm512_mul_ps(_2,_mm512_div_ps(t1,t2));
+                          diff             = _mm512_add_ps(epst,mut);
+                          sqr              = _mm512_mul_ps(diff,diff);
+                          rcs              = _mm512_mul_ps(t0,_mm512_mul_ps(k0a3,sqr));
+                          return (rcs);
+
+                   }
+
+
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   __m512 rcs_f4149_zmm16r4_a(const float * __restrict __ATTR_ALIGN__(64) pa,
+                                              const float * __restrict __ATTR_ALIGN__(64) pk0a,
+                                              const float * __restrict __ATTR_ALIGN__(64) peps1,
+                                              const float * __restrict __ATTR_ALIGN__(64) peps0,
+                                              const float * __restrict __ATTR_ALIGN__(64) pmu1,
+                                              const float * __restrict __ATTR_ALIGN__(64) pmu0) {
+
+                          register __m512 a   = _mm512_load_ps(&pa[0]);
+                          register __m512 k0a = _mm512_load_ps(&pk0a3[0]);
+                          register __m512 eps1= _mm512_load_ps(&peps1[0]);
+                          register __m512 eps0= _mm512_load_ps(&peps0[0]);
+                          register __m512 mu1 = _mm512_load_ps(&pmu1[0]);
+                          register __m512 mu0 = _mm512_load_ps(&pmu0[0]);
+                          register __m512 t0,t1,k0a3,epst,mut,sqr,t2,diff;
+                          register __m512 rcs;
+                          const __m512 pi4 = _mm512_set1_ps(0.78539816339744830961566084582f);
+                          t0               = _mm512_mul_ps(pi4,_mm512_mul_ps(PI,a));
+                          const __m512 _1  = _mm512_set1_ps(1.0f);
+                          k0a3             = _mm512_mul_ps(k0a,_mm512_mul_ps(k0a,k0a));
+                          const __m512 _2  = _mm512_set1_ps(2.0f);
+                          epst             = _mm512_sub_ps(_mm512_div_ps(eps1,eps0),_1);
+                          t1               = _mm512_sub_ps(mu1,mu0);
+                          t2               = _mm512_add_ps(mu1,mu0);
+                          mut              = _mm512_mul_ps(_2,_mm512_div_ps(t1,t2));
+                          diff             = _mm512_sub_ps(epst,mut);
+                          sqr              = _mm512_mul_ps(diff,diff);
+                          rcs              = _mm512_mul_ps(t0,_mm512_mul_ps(k0a3,sqr));
+                          return (rcs);
+
+                   }
+
+
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   __m512 rcs_f4149_zmm16r4_u(const float * __restrict  pa,
+                                              const float * __restrict  pk0a,
+                                              const float * __restrict  peps1,
+                                              const float * __restrict  peps0,
+                                              const float * __restrict  pmu1,
+                                              const float * __restrict  pmu0) {
+
+                          register __m512 a   = _mm512_loadu_ps(&pa[0]);
+                          register __m512 k0a = _mm512_loadu_ps(&pk0a3[0]);
+                          register __m512 eps1= _mm512_loadu_ps(&peps1[0]);
+                          register __m512 eps0= _mm512_loadu_ps(&peps0[0]);
+                          register __m512 mu1 = _mm512_loadu_ps(&pmu1[0]);
+                          register __m512 mu0 = _mm512_loadu_ps(&pmu0[0]);
+                          register __m512 t0,t1,k0a3,epst,mut,sqr,t2,diff;
+                          register __m512 rcs;
+                          const __m512 pi4 = _mm512_set1_ps(0.78539816339744830961566084582f);
+                          t0               = _mm512_mul_ps(pi4,_mm512_mul_ps(PI,a));
+                          const __m512 _1  = _mm512_set1_ps(1.0f);
+                          k0a3             = _mm512_mul_ps(k0a,_mm512_mul_ps(k0a,k0a));
+                          const __m512 _2  = _mm512_set1_ps(2.0f);
+                          epst             = _mm512_sub_ps(_mm512_div_ps(eps1,eps0),_1);
+                          t1               = _mm512_sub_ps(mu1,mu0);
+                          t2               = _mm512_add_ps(mu1,mu0);
+                          mut              = _mm512_mul_ps(_2,_mm512_div_ps(t1,t2));
+                          diff             = _mm512_sub_ps(epst,mut);
+                          sqr              = _mm512_mul_ps(diff,diff);
+                          rcs              = _mm512_mul_ps(t0,_mm512_mul_ps(k0a3,sqr));
+                          return (rcs);
+
+                   }
+
+                  
+
+
 
                     
 
