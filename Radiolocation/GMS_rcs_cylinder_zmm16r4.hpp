1110ae3e9efe93bf5e02a530c870d2b359a59aba
Bernard Gingold <beniekg@gmail.com>
2023-02-02 19:00:36 +0100

    Rint_f4169_zmm16r4_u unaligned kernel.

--- Radiolocation/GMS_rcs_cylinder_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_cylinder_zmm16r4.hpp
@@ -4713,7 +4713,7 @@ namespace gms {
 	           __ATTR_ALIGN__(32)
                    __ATTR_VECTORCALL__
 	           static inline
-                   void Rint_f4169_zmm16r4( const float * __restrict __ATTR_ALIGN__(64) pmur,
+                   void Rint_f4169_zmm16r4_a( const float * __restrict __ATTR_ALIGN__(64) pmur,
                                              const float * __restrict __ATTR_ALIGN__(64) pmui,
                                              const float * __restrict __ATTR_ALIGN__(64) pepsr,
                                              const float * __restrict __ATTR_ALIGN__(64) pepsi,
@@ -4729,6 +4729,30 @@ namespace gms {
                         Rext_f4164_zmm16r4(mur,mui,epsr,epsi,&t0r,&t0i);
                         _mm512_store_ps(&Rintr[0] ,_mm512_mul_ps(n1,t0r));
                         _mm512_store_ps(&Rinti[0] ,_mm512_mul_ps(n1,t0i));
+                 }
+
+
+                    __ATTR_ALWAYS_INLINE__
+                   __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void Rint_f4169_zmm16r4_u( const float * __restrict pmur,
+                                              const float * __restrict  pmui,
+                                              const float * __restrict  pepsr,
+                                              const float * __restrict  pepsi,
+                                              float * __restrict  Rintr,
+                                              float * __restrict  Rinti) {
+                        
+                        register __m512 mur  = _mm512_loadu_ps(&pmur[0]);
+                        register __m512 mui  = _mm512_loadu_ps(&pmui[0]);
+                        register __m512 epsr = _mm512_loadu_ps(&pepsr[0]);
+                        register __m512 epsi = _mm512_loadu_ps(&pepsi[0]);
+                        register __m512 t0r,t0i;
+                        const __m512 n1 = _mm512_mul_ps(-1.0f);
+                        Rext_f4164_zmm16r4(mur,mui,epsr,epsi,&t0r,&t0i);
+                        _mm512_storeu_ps(&Rintr[0] ,_mm512_mul_ps(n1,t0r));
+                        _mm512_storeu_ps(&Rinti[0] ,_mm512_mul_ps(n1,t0i));
                  }
 
 
