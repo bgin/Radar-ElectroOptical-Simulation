de7dec5838838d390b59586b6daedc8bcdfa4063
Bernard Gingold <beniekg@gmail.com>
2023-02-21 17:22:28 +0100

    Added a comment.

--- Radiolocation/GMS_rcs_cylinder_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_cylinder_zmm16r4.hpp
@@ -12873,6 +12873,48 @@ namespace gms {
                 }
 
 
+                   __ATTR_ALWAYS_INLINE__
+                   __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   __m512 rcs_f4356_zmm16r4_a(const float * __restrict __ATTR_ALIGN__(64) pk0a,
+                                            const float * __restrict __ATTR_ALIGN__(64) ph) {
+
+                          register __m512  k0a  = _mm512_load_ps(&pk0a[0]);
+                          register __m512  h    = _mm512_load_ps(&ph[0]);
+                          const __m512 _4 = _mm512_set1_ps(4.0f);
+                          register __m512 rcs,h2;
+                          h2 = _mm512_mul_ps(h,h);
+                          rcs = _mm512_mul_ps(_4,_mm512_mul_ps(k0a,h2));
+                          return (rcs); 
+                }
+
+
+                  __ATTR_ALWAYS_INLINE__
+                   __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   __m512 rcs_f4356_zmm16r4_u(const float * __restrict  pk0a,
+                                            const float * __restrict  ph) {
+
+                          register __m512  k0a  = _mm512_loadu_ps(&pk0a[0]);
+                          register __m512  h    = _mm512_loadu_ps(&ph[0]);
+                          const __m512 _4 = _mm512_set1_ps(4.0f);
+                          register __m512 rcs,h2;
+                          h2 = _mm512_mul_ps(h,h);
+                          rcs = _mm512_mul_ps(_4,_mm512_mul_ps(k0a,h2));
+                          return (rcs); 
+                }
+
+
+                  /*
+                       Elliptical cylinders.
+                   */
+
+
+
                   
 
 
