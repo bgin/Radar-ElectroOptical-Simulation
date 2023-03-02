e8bfa9110f35310ba864f8a9548810c3235e7e56
Bernard Gingold <beniekg@gmail.com>
2023-03-02 17:12:14 +0100

    rc_zmm16r4_u kernel.

--- Radiolocation/GMS_rcs_common_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_common_zmm16r4.hpp
@@ -242,6 +242,58 @@ c
                 }
 
 
+                    __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   __m512 rc_zmm16r4_u(const float * __restrict  px,
+                                       const float * __restrict  py,
+                                       const float * __restrict  perrtot) {
+
+                          register __m512 x       = _mm512_loadu_ps(&px[0]);
+                          register __m512 y       = _mm512_loadu_ps(&py[0]);
+                          register __m512 errtot  = _mm512_loadu_ps(&perrtot[0]);
+                          const register __m512 c1 = _mm512_set1_ps(0.142857142857142857142857142857f);
+                          const register __m512 c2 = _mm512_set1_ps(0.409090909090909090909090909091f);
+                          const register __m512 _1 = _mm512_set1_ps(1.0f);
+                          const register __m512 thr= _mm512_set1_ps(0.333333333333333333333333333333f);
+                          const register __m512 c3 = _mm512_set1_ps(0.375f);
+                          const register __m512 _2 = _mm512_set1_ps(2.0f);
+                          const register __m512 qtr= _mm512_set1_ps(0.25f);
+                          const register __m512 c4 = _mm512_set1_ps(0.3f);
+                          register __m512 rc,xn,yn,mu,s,sn,lamda;
+                          xn = x;
+                          yn = y;
+                          
+                          while(true) {
+
+                                mu = _mm512_mul_ps(_mm512_add_ps(xn,
+                                                          _mm512_add_ps(yn,yn)),thr);
+                                sn = _mm512_div_ps(_mm512_add_ps(yn,mu),
+                                                   _mm512_sub_ps(mu,_2));
+                                if(_mm512_cmp_mask_ps(
+                                             _mm512_abs_ps(sn),errtot,_CMP_LT_OQ)) {
+                                   register __m512 t0 = _mm512_fmadd_ps(sn,c2,c3);
+                                   register __m512 t1 = _mm512_fmadd_ps(t0,sn,c1);
+                                   register __m512 t2 = _mm512_fmadd_ps(t1,sn,c4);
+                                   s                  = _mm512_mul_ps(_mm512_mul_ps(sn,sn),t2);
+                                   rc                 = _mm512_div_ps(_mm512_add_ps(_1,s),
+                                                                      _mm512_sqrt_ps(mu));
+                                   return (rc);
+                                }
+                                register __m512 sxn = _mm512_sqrt_ps(xn);
+                                register __m512 syn = _mm512_sqrt_ps(yn);
+                                lamda =  _mm512_fmadd_ps(_mm512_mul_ps(_2,sxn),syn,yn);
+                                xn    = _mm512_mul_ps(_mm512_add_ps(xn,lamda),qtr);
+                                yn    = _mm512_mul_ps(_mm512_add_ps(ym,lamda),qtr);
+                         }
+                }
+
+
+                       
+
+
                  /*
 
 c*********************************************************************72
