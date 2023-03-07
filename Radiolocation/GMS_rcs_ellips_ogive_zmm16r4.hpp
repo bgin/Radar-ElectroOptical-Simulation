b0e751ab0d58927ee7be3cb390cb7f495b2dc68d
Bernard Gingold <beniekg@gmail.com>
2023-03-07 16:23:16 +0100

    HSth_f5186_zmm16r4_u kernel.

--- Radiolocation/GMS_rcs_ellips_ogive_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_ellips_ogive_zmm16r4.hpp
@@ -2402,6 +2402,30 @@ namespace gms {
                }
 
 
+                  __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void HSth_f5186_zmm16r4_u(const float * __restrict pk0,
+                                          const float * __restrict  pr,
+                                          const float * __restrict  pepsr,
+                                          const float * __restrict  pepsi,
+                                          const float * __restrict  pmur,
+                                          const float * __restrict  pmui,
+                                          const float * __restrict  pa,
+                                          const float * __restrict  pc,
+                                          const float * __restrict  ptht1,
+                                          const float * __restrict  ptht2,
+                                          const float * __restrict  pphi2,
+                                          float * __restrict  HSr,
+                                          float * __restrict  HSi) {
+
+                        ESph_f5186_zmm16r4_u(pk0,pr,pepsr,pepsi,pmur,pmui,
+                                             pa,pc,ptht1,ptht2,pphi2,HSr,HSi);
+               }
+
+
 
               
 
