e899dd983897f342a7a7c34cfd462e43fcc39951
Bernard Gingold <beniekg@gmail.com>
2023-03-14 09:49:28 +0100

    ESph_f6216_zmm16r4_u kernel.

--- Radiolocation/GMS_rcs_cone_wedge_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_cone_wedge_zmm16r4.hpp
@@ -264,7 +264,7 @@ namespace  gms {
 	           __ATTR_ALIGN__(32)
                    __ATTR_VECTORCALL__
 	           static inline
-                   void ESph_f6216_zmm16r4(const float * __restrict __ATTR_ALIGN__(64) pk0,
+                   void ESph_f6216_zmm16r4_a(const float * __restrict __ATTR_ALIGN__(64) pk0,
                                            const float * __restrict __ATTR_ALIGN__(64) pr,
                                            const float * __restrict __ATTR_ALIGN__(64) palp,
                                            const float * __restrict __ATTR_ALIGN__(64) ptht,
@@ -297,6 +297,47 @@ namespace  gms {
                         alphs= _mm512_mul_ps(_mm512_mul_ps(alph,alph),rat);
                         _mm512_store_ps(&ESr[0] ,_mm512_mul_ps(t0r,alphs));
                         _mm512_store_ps(&ESi[0] ,_mm512_mul_ps(t0i,alphs));
+                 }
+
+
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void ESph_f6216_zmm16r4_u(const float * __restrict  pk0,
+                                           const float * __restrict  pr,
+                                           const float * __restrict  palp,
+                                           const float * __restrict  ptht,
+                                           float * __restrict  ESr,
+                                           float * __restrict  ESi) {
+  
+                        register __m512 k0  = _mm512_loadu_ps(&pk0[0]);
+                        register __m512 r   = _mm512_loadu_ps(&pr[0]);
+                        register __m512 alp = _mm512_loadu_ps(&palp[0]);
+                        register __m512 tht = _mm512_loadu_ps(&ptht[0]);
+                        const __m512 hlf = _mm512_set1_ps(0.5f);
+                        const __m512 _3  = _mm512_set1_ps(3.0f);
+                        const __m512 _4  = _mm512_set1_ps(4.0f);
+                        register __m512 k0r,inv,alph,alphs,ctht,t0r,t0i;
+                        register __m512 ctht2,num,den,ear,eai,cer,cei,rat;
+                        k0r  = _mm512_mul_ps(k0,r);
+                        ctht = xcosf(tht);
+                        alph = _mm512_mul_ps(alp,hlf);
+                        ctht2= _mm512_mul_ps(ctht,ctht);
+                        ear  = _mm512_setzero_ps();
+                        ctht3= _mm512_mul_ps(ctht2,ctht);
+                        eai  = k0r;
+                        num  = _mm512_add_ps(_3,ctht2);
+                        inv  = _mm512_rcp14_ps(k0r);
+                        cexp_zmm16r4(ear,eai,&cer,&cei);
+                        den  = _mm512_mul_ps(_4,ctht3);
+                        t0r  = _mm512_mul_ps(cer,inv);
+                        rat  = _mm512_div_ps(num,den);
+                        t0i  = _mm512_mul_ps(cei,inv);
+                        alphs= _mm512_mul_ps(_mm512_mul_ps(alph,alph),rat);
+                        _mm512_storeu_ps(&ESr[0] ,_mm512_mul_ps(t0r,alphs));
+                        _mm512_storeu_ps(&ESi[0] ,_mm512_mul_ps(t0i,alphs));
                  }
 
           }
