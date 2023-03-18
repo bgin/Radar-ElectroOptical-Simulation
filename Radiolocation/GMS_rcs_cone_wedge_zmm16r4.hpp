d19cce190ecef84f9e1666f7456da56d2c6f69c7
Bernard Gingold <beniekg@gmail.com>
2023-03-18 09:29:58 +0100

    rcs_f6230_zmm16r4 kernel.

--- Radiolocation/GMS_rcs_cone_wedge_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_cone_wedge_zmm16r4.hpp
@@ -1811,7 +1811,28 @@ namespace  gms {
                       */
 
 
-                    
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   __m512 rcs_f6230_zmm16r4(const __m512 b,
+                                            const __m512 k0) {
+
+                          const __m512 pi = _mm512_set1_ps(3.14159265358979323846264338328f);
+                          const __m512 _4 = _mm512_set1_ps(4.0f);
+                          register __m512 b4,k02,k0b2,b2;
+                          register __m512 trm1,trm1,rcs,x0;
+                          b2  = _mm512_mul_ps(b,b);
+                          k02 = _mm512_mul_ps(k0,k0);
+                          b4  = _mm512_mul_ps(b2,b2);
+                          k0b2= _mm512_mul_ps(k02,k02);
+                          trm2= _mm512_mul_ps(k0b2,_mm512_mul_ps(pi,b2));
+                          x0  = _mm512_mul_ps(_4,k02);
+                          trm1= _mm512_div_ps(_mm512_mul_ps(pi,b4),x0);
+                          rcs = _mm512_add_ps(trm1,trm2);
+                          return (rcs);
+                }
 
 
 
