5398e413820be449b848503381b0b94a5c224151
Bernard Gingold <beniekg@gmail.com>
2023-04-15 15:49:45 +0200

    Va_f656_zmm16r4_a kernel.

--- Radiolocation/GMS_rcs_cone_wedge_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_cone_wedge_zmm16r4.hpp
@@ -6427,6 +6427,46 @@ namespace  gms {
                 }
 
 
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void Va_f656_zmm16r4_a(const float * __restrict __ATTR_ALIGN__(64) pk0,
+                                          const float * __restrict __ATTR_ALIGN__(64) pr,
+                                          const float * __restrict __ATTR_ALIGN__(64) ppsi,
+                                          const float * __restrict __ATTR_ALIGN__(64) parg2, //2pinN
+                                          float * __restrict __ATTR_ALIGN__(64) Var,
+                                          float * __restrict __ATTR_ALIGN__(64) Vai) {
+
+                       register __m512 psi = _mm512_load_ps(&ppsi[0]);
+                       register __m512 arg2= _mm512_load_ps(&parg2[0]);
+                       const __m512 npi    = _mm512_set1_ps(-3.14159265358979323846264338328f);
+                       const __m512 pi     = _mm512_set1_ps(3.14159265358979323846264338328f);
+                       register __m512 arg1= _mm512_add_ps(psi,arg2);
+                       const __m512 n1     = _mm512_set1_ps(-1.0f);
+                       const __mmask16 m1  = _mm512_cmp_ps_mask(npi,arg1,_CMP_LT_OQ);
+                       const __mmask16 m2  = _mm512_cmp_ps_mask(arg1,pi,_CMP_LT_OQ);
+                       if(m1 && m2) {
+                           register __m512 k0 = _mm512_load_ps(&pk0[0]);
+                           register __m512 r  = _mm512_load_ps(&pr[0]);
+                           register __m512 ear,eai,cer,cei,k0r;
+                           register __m512 arg1,carg;
+                           k0r = _mm512_mul_ps(k0,r);
+                           ear = _mm512_setzero_ps();
+                           carg= xcosf(arg1);
+                           eai = _mm512_mul_ps(k0r,carg);
+                           cexp_zmm16r4(ear,eai,&cer,&cei);
+                           _mm512_store_ps(&Var[0] ,cer);
+                           _mm512_store_ps(&Vai[0] ,cei);
+                       }
+                       else {  
+                           _mm512_store_ps(&Var[0] ,_mm512_setzero_ps());
+                           _mm512_store_ps(&Vai[0] ,_mm512_setzero_ps());
+                       }
+                }
+
+
 
 
 
