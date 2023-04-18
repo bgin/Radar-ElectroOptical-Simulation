cd9896ff4662bdee3547e93ff958b766b5193eaa
Bernard Gingold <beniekg@gmail.com>
2023-04-18 16:24:13 +0200

    zi_f716_zmm16r4_u kernel.

--- Radiolocation/GMS_rcs_planar_surf_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_planar_surf_zmm16r4.hpp
@@ -89,7 +89,7 @@ namespace  gms {
 	           __ATTR_ALIGN__(32)
                    __ATTR_VECTORCALL__
 	           static inline
-                   void zi_f716_zmm16r4(const float * __restrict __ATTR_ALIGN__(64) ptht,
+                   void zi_f716_zmm16r4_a(const float * __restrict __ATTR_ALIGN__(64) ptht,
                                         const float * __restrict __ATTR_ALIGN__(64) pmur,
                                         const float * __restrict __ATTR_ALIGN__(64) pmui,
                                         const float * __restrict __ATTR_ALIGN__(64) pepsr,
@@ -110,6 +110,35 @@ namespace  gms {
                         csqrt_zmm16r4(divr,divi,&wrkc,&csqr,&csqi);
                         _mm512_store_ps(&zr[0] ,_mm512_mul_ps(invc,csqr));
                         _mm512_store_ps(&zi[0] ,_mm512_mul_ps(invc,csqi));
+                }
+
+
+                    __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void zi_f716_zmm16r4_u(const float * __restrict  ptht,
+                                        const float * __restrict  pmur,
+                                        const float * __restrict  pmui,
+                                        const float * __restrict  pepsr,
+                                        const float * __restrict  pepsi,
+                                        float * __restrict  zr,
+                                        float * __restrict   zi) {
+
+                        register __m512 tht  = _mm512_loadu_ps(&ptht[0]);
+                        register __m512 mur  = _mm512_loadu_ps(&pmur[0]);
+                        register __m512 mui  = _mm512_loadu_ps(&pmui[0]);
+                        register __m512 epsr = _mm512_loadu_ps(&pepsr[0]);
+                        register __m512 epsi = _mm512_loadu_ps(&pepsi[0]);
+                        register __m512 cost,invc,divr,divi,csqr,csqi,wrkc;
+                        cost = xcosf(tht);
+                        wrkc = _mm512_setzero_ps();
+                        cdiv_zmm16r4(mur,mui,epsr,epsi,&divr,&divi);
+                        invc = _mm512_rcp14_ps(cost);
+                        csqrt_zmm16r4(divr,divi,&wrkc,&csqr,&csqi);
+                        _mm512_storeu_ps(&zr[0] ,_mm512_mul_ps(invc,csqr));
+                        _mm512_storeu_ps(&zi[0] ,_mm512_mul_ps(invc,csqi));
                 }
 
 
