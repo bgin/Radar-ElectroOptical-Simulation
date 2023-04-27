2d713b3b733b2ceb673231e0143dc488277e0f91
Bernard Gingold <beniekg@gmail.com>
2023-04-27 08:46:41 +0200

    rcs_f743_zmm16r4_a kernel.

--- Radiolocation/GMS_rcs_planar_surf_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_planar_surf_zmm16r4.hpp
@@ -1742,6 +1742,35 @@ namespace  gms {
                 }
 
 
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline 
+                   __m512 rcs_f743_zmm16r4_a(const float * __restrict __ATTR_ALIGN__(64) pk0,
+                                             const float * __restrict __ATTR_ALIGN__(64) pa,
+                                             const float * __restrict __ATTR_ALIGN__(64) ptht) {
+
+                          register __m512 k0  = _mm512_load_ps(&pk0[0]);
+                          register __m512 a   = _mm512_load_ps(&pa[0]);
+                          register __m512 tht = _mm512_load_ps(&ptht[0]);
+                          const __m512 pis = _mm512_set1_ps(9.869604401089358618834490999876f); 
+                          const __m512 1o64= _mm512_set1_ps(0.015625f);
+                          register __m512 k0a,k0a2,cost,cos2t,fac;
+                          register __m512 rcs,x0,x1,num,x2;
+                          k0a  = _mm512_mul_ps(k0,a);
+                          cost = xcosf(tht);
+                          k0a2 = _mm512_add_ps(k0a,k0a);
+                          fac  = _mm512_div_ps(pis,k0);
+                          x2   = _mm512_mul_ps(cos2t,cos2t);
+                          x0   = _mm512_mul_ps(k0a2,k0a2);
+                          x1   = _mm512_mul_ps(x0,x0);
+                          num  = _mm512_mul_ps(_mm512_mul_ps(x1,x2),1o64);
+                          rcs  = _mm512_mul_ps(fac,num);
+                          return (rcs);
+                }
+
+
 
       } // radiolocation
 
