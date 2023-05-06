8456f737501222c417a0b83da3ac039231a8be00
Bernard Gingold <beniekg@gmail.com>
2023-05-06 19:20:21 +0200

    Added a kernel description.

--- Radiolocation/GMS_rcs_planar_surf_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_planar_surf_zmm16r4.hpp
@@ -5078,6 +5078,37 @@ namespace  gms {
                                                                 _mm512_mul_ps(k04,a6));
                          return (rcs);                         
                  }
+                 
+                 
+                  __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline   
+                   __m512 rcs_f755_zmm16r4_u(const float * __restrict  pk0,
+                                             const float * __restrict pa) {
+                                         
+                         register __m512 k0  = _mm512_loadu_ps(&pk0[0]);
+                         register __m512 a   = _mm512_loadu_ps(&pa[0]);
+                               
+                         const __m512 C2263536968418066997601902412409 = 
+                                          _mm512_set1_ps(2.263536968418066997601902412409f);
+                         register __m512 rcs,k02,k04,a2,a6;
+                         k02 = _mm512_mul_ps(k0,k0);
+                         a2  = _mm512_mul_ps(a,a);
+                         k04 = _mm512_mul_ps(k02,k02);
+                         a6  = _mm512_mul_ps(a2,_mm512_mul_ps(a2,a2));
+                         rcs = _mm512_mul_ps( C2263536968418066997601902412409,
+                                                                _mm512_mul_ps(k04,a6));
+                         return (rcs);                         
+                 }
+                 
+                 
+                 /*
+                       Wedge-on incidence.
+                       Formula: 7.5-6
+                 */
+                 
 
 
 
