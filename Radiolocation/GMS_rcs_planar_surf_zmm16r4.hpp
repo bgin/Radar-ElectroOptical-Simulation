c1afc1f0876804356c9a7879fd1816645e4e625b
Bernard Gingold <beniekg@gmail.com>
2023-05-06 11:52:35 +0200

    Eph_f752_zmm16r4 kernel.

--- Radiolocation/GMS_rcs_planar_surf_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_planar_surf_zmm16r4.hpp
@@ -4705,6 +4705,54 @@ namespace  gms {
                         _mm512_storeu_ps(&Esi[0] ,_mm512_mul_ps(x1,cei));
                  }
                  
+                 
+                 /*
+                             Flat plates approximations.
+                             Low-frequency approximations for k0a<<1,
+                             plane wave unit amplitude xz-plane propagating
+                             at angle 'phi'.
+                             Parallel polarization.
+                             Theta component.
+                             Formula 7.5-2
+                        */
+                        
+                        
+                    __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline   
+                   void Eph_f752_zmm16r4(const __m512 tht,
+                                         const __m512 phi2,
+                                         const __m512 k0,
+                                         const __m512 r,
+                                         const __m512 a,
+                                         __m512 * __restrict Esr,
+                                         __m512 * __restrict Esi) {
+                        
+                        const __m512 C0424413181578387562050356702327 = 
+                                              _mm512_set1_ps(0.424413181578387562050356702327f);
+                        register __m512 ear,eai,cer,cei,k0r,k02,a3;
+                        register __m512 cost,sinp,x0,x1,inr;
+                        k0r  = _mm512_mul_ps(k0,r);
+                        sinp = xsinf(phi2);
+                        ear  = _mm512_setzero_ps();
+                        eai  = k0r;
+                        cost = xcosf(tht);
+                        k02  = _mm512_mul_ps(k0,k0);
+                        inr  = _mm512_rcp14_ps(r);
+                        cexp_zmm16r4(ear,eai,&cer,&cei);
+                        a3   = _mm512_mul_ps(a,_mm512_mul_ps(a,a));
+                        k02  = gms::math::negate_zmm16r4(k02);
+                        x0   = _mm512_mul_ps(C0424413181578387562050356702327,
+                                                         _mm512_mul_ps(sinp,cost));
+                        x1   = _mm512_mul_ps(k02,a3);
+                        cer  = _mm512_mul_ps(x0,cer);
+                        *Esr = _mm512_mul_ps(x1,cer);
+                        cei  = _mm512_mul_ps(x0,cei);
+                        *Esi = _mm512_mul_ps(x1,cei);       
+                 }
+                 
 
                     
 
