d920c334c70d0b71983c8ee56576ef0fa5e7f2e7
Bernard Gingold <beniekg@gmail.com>
2023-05-11 12:29:06 +0200

    coef_D12_f8121_zmm16r4_a kernel.

--- Radiolocation/GMS_rcs_complex_objects_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_complex_objects_zmm16r4.hpp
@@ -67,7 +67,7 @@ namespace  gms {
 	           __ATTR_ALIGN__(32)
                    __ATTR_VECTORCALL__
 	           static inline
-                   void coef_D_f8121_zmm16r4(const __m512 gam,
+                   void coef_D12_f8121_zmm16r4(const __m512 gam,
                                              const __m512 phi,
                                              const __m512 k0,
                                              __m512 * __restrict D1r,
@@ -110,7 +110,61 @@ namespace  gms {
                         *D2r = _mm512_mul_ps(ear,x1);
                         *D2i = _mm512_mul_ps(eai,x1);
                 }
-                   
+                
+                
+                
+                    __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void coef_D12_f8121_zmm16r4_a(const float * __restrict __ATTR_ALIGN__(64) pgam,
+                                               const float * __restrict __ATTR_ALIGN__(64) pphi,
+                                               const float * __restrict __ATTR_ALIGN__(64) k0,
+                                               float * __restrict __ATTR_ALIGN__(64) D1r,
+                                               float * __restrict __ATTR_ALIGN__(64) D1i,
+                                               float * __restrict __ATTR_ALIGN__(64) D2r,
+                                               float * __restrict __ATTR_ALIGN__(64) D2i) {
+                                   
+                        register __m512 gam = _mm512_load_ps(&pgam[0]);
+                        register __m512 phi = _mm512_load_ps(&pphi[0]);  
+                        register __m512 k0  = _mm512_load_ps(&pk0[0]);       
+                        const __m512 C078539816339744830961566084582  = 
+                                                     _mm512_set1_ps(0.78539816339744830961566084582f);
+                        const __m512 C6283185307179586476925286766559 = 
+                                                     _mm512_set1_ps(6.283185307179586476925286766559f);
+                        const __m512 C314159265358979323846264338328  = 
+                                                     _mm512_set1_ps(3.14159265358979323846264338328f);
+                        const __m512 C10                              = _mm512_set1_ps(1.0f);
+                        register __m512 invn,x0,x1,ear,eai,cer,cei,phi2,sqr,sinp,cos1,cos2,trm1,trm2;
+                        phi2 = _mm512_add_ps(phi,phi);
+                        x0   = _mm512_div_ps(gam,C314159265358979323846264338328); 
+                        ear  = _mm512_setzero_ps();
+                        invn = _mm512_rcp14_ps(x0);
+                        eai  = C078539816339744830961566084582;
+                        x1   = _mm512_mul_ps(C314159265358979323846264338328,invn);
+                        sqr  = _mm512_sqrt_ps(_mm512_mul_ps(k0,
+                                                C6283185307179586476925286766559));
+                        sinp = xsinf(x1);
+                        cexp_zmm16r4(ear,eai,&cer,&cei);
+                        cos1 = xcosf(x1);
+                        x0   = _mm512_mul_ps(invn,phi2);
+                        cos2 = xcosf(x0);
+                        cer  = _mm512_mul_ps(cer,sinp);
+                        trm1 = _mm512_rcp14_ps(_mm512_sub_ps(cos1),C10);
+                        cei  = _mm512_mul_ps(cei,sinp);
+                        trm2 = _mm512_rcp14_ps(_mm512_sub_ps(cos1,cos2));
+                        sqr  = _mm512_mul_ps(invn,sqr);
+                        ear  = _mm512_mul_ps(cer,sqr);
+                        eai  = _mm512_mul_ps(cei,sqr);
+                        x0   = _mm512_sub_ps(trm1,trm2);
+                        _mm512_store_ps(&D1r[0] ,_mm512_mul_ps(ear,x0));
+                        _mm512_store_ps(&D1i[0] ,_mm512_mul_ps(eai,x0));
+                        x1   = _mm512_add_ps(trm1,trm2);
+                        _mm512_store_ps(&D2r[0] ,_mm512_mul_ps(ear,x1));
+                        _mm512_store_ps(&D2i[0] ,_mm512_mul_ps(eai,x1));
+                }
+                
          
      } // radiolocation
 
