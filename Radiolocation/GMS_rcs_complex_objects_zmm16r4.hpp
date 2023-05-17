8a4e556d863180426071a7c4ddf5d57a0e782e62
Bernard Gingold <beniekg@gmail.com>
2023-05-17 14:11:01 +0200

    Added a kernel description.

--- Radiolocation/GMS_rcs_complex_objects_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_complex_objects_zmm16r4.hpp
@@ -3295,6 +3295,16 @@ namespace  gms {
 	         }
 	         
 	         
+	            /*
+	             Model 9B4 (Peake's Model)
+	             Model resembling many natural grass-like structures like
+	             forests,grass,wheat fields, etc.
+	             Bistatic RCS (vv) polarized per unit surface area.
+	             Formula 9.1-36
+	       
+	       */
+	         
+	         
 	         
 	       
 	       
