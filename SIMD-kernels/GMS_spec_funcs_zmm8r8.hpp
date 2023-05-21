94a0c31df5b873d87ea6462c5ca3df46f7b3fbb0
Bernard Gingold <beniekg@gmail.com>
2023-05-21 14:16:41 +0200

    besk1_zmm8r8_a kernel.

--- SIMD-kernels/GMS_spec_funcs_zmm8r8.hpp
+++ SIMD-kernels/GMS_spec_funcs_zmm8r8.hpp
@@ -812,6 +812,37 @@ namespace gms {
 !
 !    Output, real ( kind = 8 ) BESK1, the value of the function.        
 */  
+
+
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+	           __m512d besk1_zmm8r8(const __m512d x) {
+	           
+	                   register __m512d result;
+	                   int32_t jint;
+	                   jint = 1;
+	                   result = calck1_zmm8r8(x,jint);
+	                   return (result);
+	          }   
+	          
+	          
+	           __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+	           __m512d besk1_zmm8r8_a(const double * __restrict __ATTR_ALIGN__(64) px) {
+	           
+	                   register __m512d x = _mm512_load_pd(&px[0]);
+	                   register __m512d result;
+	                   int32_t jint;
+	                   jint = 1;
+	                   result = calck1_zmm8r8(x,jint);
+	                   return (result);
+	          }   
 	                   
 	         
         
