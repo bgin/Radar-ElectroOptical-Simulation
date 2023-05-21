1d8db23db3d9bb15a845a4743462fb33b5c065a5
Bernard Gingold <beniekg@gmail.com>
2023-05-21 16:31:42 +0200

    besy1_zmm8r8_a kernels.

--- SIMD-kernels/GMS_spec_funcs_zmm8r8.hpp
+++ SIMD-kernels/GMS_spec_funcs_zmm8r8.hpp
@@ -987,7 +987,23 @@ namespace gms {
 	                   jint = 1;
 	                   result = caljy1_zmm8r8(x,jint);
 	                   return (result);
-	          }     
+	          }   
+	          
+	          
+	           __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+	           __m512d besy1_zmm8r8_a(const double * __restrict __ATTR_ALIGN__(64) px) {
+	           
+	                   register __m512d x = _mm512_load_pd(&px[0]);
+	                   register __m512d result;
+	                   int32_t jint;
+	                   jint = 1;
+	                   result = caljy1_zmm8r8(x,jint);
+	                   return (result);
+	          }    
 	                   
 	         
         
