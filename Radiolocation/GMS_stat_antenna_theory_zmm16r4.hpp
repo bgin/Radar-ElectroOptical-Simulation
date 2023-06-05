405b934af7ded5bdf3d820e2b7ceec9430f93808
Bernard Gingold <beniekg@gmail.com>
2023-06-05 09:34:53 +0200

    f11_zmm16r4_cspint_ne_a kernel.

--- Radiolocation/GMS_stat_antenna_theory_zmm16r4.hpp
+++ Radiolocation/GMS_stat_antenna_theory_zmm16r4.hpp
@@ -49,6 +49,7 @@ namespace file_version {
 #include "GMS_simd_utils.hpp"
 #include "GMS_cspint_quad.hpp"
 #include "GMS_avint_quad.hpp"
+#include "GMS_cephes.h"
 
 namespace  gms {
 
@@ -63,8 +64,9 @@ namespace  gms {
                     
                /*
                    Work (input) arrays for integrator 'cspint'
+                   Multi-threaded i.e. (two-threaded)
                */
-               __ATTR_ALIGN__(64) struct CSPINT_DATA {
+               __ATTR_ALIGN__(64) struct CSPINT_DATA_2T {
                
                        float * __restrict  Ya1; 
                        float * __restrict  Ya2; 
@@ -78,9 +80,24 @@ namespace  gms {
                        float * __restrict  WRKb;  
                };
                
+                /*
+                   Work (input) arrays for integrator 'cspint'
+                   Single-threaded.
+               */
+                __ATTR_ALIGN__(64) struct CSPINT_DATA_1T {
+               
+                       float * __restrict  Y1; 
+                       float * __restrict  Y2; 
+                       float * __restrict  Y3; 
+                       float * __restrict  E;  
+                       float * __restrict  WRK; 
+                       
+               };
+               
+               
                    /*
                         Formula 1.1, p. 14
-                        Integrator cspint single data vector.
+                        Integrator cspint single data vector ZMM.
                    */
                    __ATTR_ALWAYS_INLINE__
 	           __ATTR_HOT__
@@ -115,10 +132,10 @@ namespace  gms {
                          vtht= _mm512_set1_ps(tht);
                          k   = _mm512_mul_ps(C314159265358979323846264338328,
                                                           _mm512_set1_ps(gam));
-                         stht= xsinf(vtht);
+                         stht= xsinf(tht);
                          ear = _mm512_setzero_ps();
                          eai = _mm512_fmadd_pd(stht,
-                                           _mm12_mul_pd(k,z),phiz);
+                                           _mm512_mul_pd(k,z),phiz);
                          cexp_zmm16r4(ear,eai,&cer,&cei);
                          sumr= 0.0f;
                          cer = _mm512_mul_ps(Az,cer);
@@ -166,10 +183,10 @@ namespace  gms {
                          vtht= _mm512_set1_ps(tht);
                          k   = _mm512_mul_ps(C314159265358979323846264338328,
                                                           _mm512_set1_ps(gam));
-                         stht= xsinf(vtht);
+                         stht= xsinf(tht);
                          ear = _mm512_setzero_ps();
                          eai = _mm512_fmadd_pd(stht,
-                                           _mm12_mul_pd(k,z),phiz);
+                                           _mm512_mul_pd(k,z),phiz);
                          cexp_zmm16r4(ear,eai,&cer,&cei);
                          sumr= 0.0f;
                          cer = _mm512_mul_ps(Az,cer);
@@ -182,7 +199,86 @@ namespace  gms {
                          fth = {sumr,sumi};
                          return (fth);
                  }
-      
+                 
+                 
+                   /*
+                        Formula 1.1, p. 14
+                        Integrator cspint n-elements data arrays.
+                        Single-threaded.
+                   */
+                   
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   std::complex<float>
+                   f11_zmm16r4_cspint_ne_a( const float * __restrict __ATTR_ALIGN__(64) pAz,
+                                            const float * __restrict __ATTR_ALIGN__(64) pphiz,
+                                            const float * __restrict __ATTR_ALIGN__(64) pz,
+                                            float * __restrict __ATTR_ALIGN__(64) intr,
+                                            float * __restrict __ATTR_ALIGN__(64) inti,
+                                            CSPINT_DATA_1T & csd,
+                                            const float tht,
+                                            const float gam,
+                                            const int32_t L2,
+                                            const int32_t NTAB) {
+                                            
+                        if(__builtin_expect(NTAB==16,0)) {
+                           std::complex<float> fth = {0.0f,0.0f};
+                           fth = f11_zmm16r4_cspint_16_a(pAz,pphiz,pz,tht,gam,L2);
+                           return (fth);
+                        }     
+                        
+                        constexpr float C314159265358979323846264338328 = 
+                                                        3.14159265358979323846264338328f;
+                        register __m512 vtht,stht,vk;
+                        register __m512 ear,cer,cei;
+                        register std::complex<float> fth;
+                        register float sumr,sumi; 
+                        register float sint,k;
+                        int32_t i; 
+                        vtht = _mm512_set1_ps(tht);
+                        sint = cephes_sinf(tht);
+                        vk   = _mm512_mul_ps(C314159265358979323846264338328,
+                                                          _mm512_set1_ps(gam));
+                        ear  = _mm512_setzero_ps();
+                        k    = C314159265358979323846264338328*gam;
+                        stht = xsinf(vtht);
+                        for(i = 0; i != ROUND_TO_SIXTEEN(NTAB,15); i += 16) {
+                             _mm_prefetch((const char*)&pAz[i],_MM_HINT_T0);
+                             _mm_prefetch((const char*)&pphiz[i],_MM_HINT_T0);
+                             _mm_prefetch((const char*)&pz[i],_MM_HINT_T0);
+                             register __m512 t0 = _mm512_load_ps(&pz[i]);
+                             register __m512 t1 = _mm512_load_ps(&pphiz[i]);
+                             register __m512 eai= _mm512_fmadd_ps(stht,
+                                                            _mm512_mul_ps(vk,t0),t1);
+                             register __m512 t2 = _mm512_load_ps(&pAz[i]);
+                             cexp_zmm16r4(ear,eai,&cer,&cei);
+                             cer = _mm512_mul_ps(cer,t2);
+                             _mm512_store_ps(&intr[i],cer);
+                             cei = _mm512_mul_ps(cei,t2);
+                             _mm512_store_ps(&inti[i],cei);
+                        }   
+                        sumr = 0.0f;
+                        sumi = 0.0f;
+                        for(; i != NTAB; ++i) {
+                             register float t0 = pAz[i];
+                             register float t1 = pphiz[i];
+                             register float t2 = pz[i];
+                             register float zz = k*t0;
+                             register float eai= sint*zz+t1;
+                             register std::complex<float> c = std::exp({0.0f,eai});
+                             intr[i] = c.real()*t0;
+                             inti[i] = c.imag()*t0;
+                        } 
+                        cspint(NTAB,&pz[0],&intr[0],-L2,L2,&csd.Y1[0],
+                               &csd.Y2[0],&csd.Y3[0],&csd.E[0],&csd.WRK[0],sumr); 
+                        cspint(NTAB,&pz[0],&inti[0],-L2,L2, &csd.Y1[0],
+                               &csd.Y2[0],&csd.Y3[0],&csd.E[0],&csd.WRK[0],sumi);  
+                        fth = {sumr,sumi};
+                        return (fth);            
+                }
       
       } // radiolocation
 
