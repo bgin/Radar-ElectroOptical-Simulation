22223ba911d78680368265ae36afe5f4fe6a30a6
Bernard Gingold <beniekg@gmail.com>
2023-06-11 15:23:38 +0200

    scrossv_zmm16r4_a kernel.

--- SIMD-kernels/GMS_vecmath_zmm16r4.hpp
+++ SIMD-kernels/GMS_vecmath_zmm16r4.hpp
@@ -229,6 +229,36 @@ namespace gms {
 	                *vcz = _mm512_fmsub_ps(v1x,v2y,
 	                                   _mm512_mul_ps(v1y,v2x));
 	         }
+	         
+	         
+	           __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+	           void scrossv_zmm16r4_a(const float * __restrict __ATTR_ALIGN__(64) pv1x,
+	                                  const float * __restrict __ATTR_ALIGN__(64) pv1y,
+	                                  const float * __restrict __ATTR_ALIGN__(64) pv1z,
+	                                  const float * __restrict __ATTR_ALIGN__(64) pv2x,
+	                                  const float * __restrict __ATTR_ALIGN__(64) pv2y,
+	                                  const float * __restrict __ATTR_ALIGN__(64) pv2z,
+	                                  float * __restrict __ATTR_ALIGN__(64) vcx,
+	                                  float * __restrict __ATTR_ALIGN__(64) vcy,
+	                                  float * __restrict __ATTR_ALIGN__(64) vcz) {
+	                      
+	                 register __m512 v1x = _mm512_load_ps(&pv1x[0]);
+	                 register __m512 v1y = _mm512_load_ps(&pv1y[0]);
+	                 register __m512 v1z = _mm512_load_ps(&pv1z[0]);
+	                 register __m512 v2x = _mm512_load_ps(&pv2x[0]);
+	                 register __m512 v2y = _mm512_load_ps(&pv2y[0]);
+	                 register __m512 v2z = _mm512_load_ps(&pv2z[0]);          
+	                *vcx = _mm512_fmsub_ps(v1y,v2z,
+	                                   _mm512_mul_ps(v1x,v2y));
+	                *vcy = _mm512_fmsub_ps(v1z,v2x,
+	                                   _mm512_mul_ps(v1x,v2z));
+	                *vcz = _mm512_fmsub_ps(v1x,v2y,
+	                                   _mm512_mul_ps(v1y,v2x));
+	         }
 	        
                 
                 
