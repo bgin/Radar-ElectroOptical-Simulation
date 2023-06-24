a117319d36d005de72dc2c7530e41017f790d6bc
Bernard Gingold <beniekg@gmail.com>
2023-06-24 10:46:53 +0200

    rcs_f419_zmm16r4_unroll6x kernel.

--- Radiolocation/GMS_rcs_cylinder_zmm16r4.hpp
+++ Radiolocation/GMS_rcs_cylinder_zmm16r4.hpp
@@ -288,6 +288,77 @@ namespace gms {
                            
                          }            
               }
+              
+              
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void rcs_f419_zmm16r4_unroll6x(const __m512 * __restrict __ATTR_ALIGN__(64) pa,
+                                                   const __m512 * __restrict __ATTR_ALIGN__(64) pk0a,
+                                                   __m512 * __restrict __ATTR_ALIGN__(64) prcs,
+                                                   const int32_t n,
+                                                   int32_t & PF_DIST) {
+                                                   
+                         if(__builtin_expect(n<=0,0)) {return;}
+                         if(__builtin_expect(PF_DIST<=0)) PF_DIST = 6;
+                         register __m512 a,k0a,rcs;
+                         int32_t j,m,m1;
+                         
+                         m = n%6;
+                         if(m!=0) {
+                            for(j = 0; j != m; ++j) {
+                                a   = pa[j];
+                                k0a = pk0a[j];
+                                rcs = rcs_f419_zmm16r4(a,k0a);
+                                prcs[j] = rcs;
+                            }
+                            if(n<6) {return;}
+                         }                    
+                         
+                         m1 = m+1;
+                         for(j = m1; j != n; j += 6) {[
+#if (__RCS_CYLINDER_PF_CACHE_HINT__) == 1
+                              _mm_prefetch((char*)&pa[j+PF_DIST],_MM_HINT_T0);
+                              _mm_prefetch((char*)&pk0a[j+PF_DIST],_MM_HINT_T0);
+#elif (__RCS_CYLINDER_PF_CACHE_HINT__) == 2
+                              _mm_prefetch((char*)&pa[j+PF_DIST],_MM_HINT_T1);
+                              _mm_prefetch((char*)&pk0a[j+PF_DIST],_MM_HINT_T1);
+#elif  (__RCS_CYLINDER_PF_CACHE_HINT__) == 3
+                              _mm_prefetch((char*)&pa[j+PF_DIST],_MM_HINT_T2);
+                              _mm_prefetch((char*)&pk0a[j+PF_DIST],_MM_HINT_T2);
+#elif  (__RCS_CYLINDER_PF_CACHE_HINT__) == 4
+                              _mm_prefetch((char*)&pa[j+PF_DIST],_MM_HINT_NTA);
+                              _mm_prefetch((char*)&pk0a[j+PF_DIST],_MM_HINT_NTA);
+#endif             
+                              a   = pa[j+0];
+                              k0a = pk0a[j+0];
+                              rcs = rcs_f419_zmm16r4(a,k0a);
+                              prcs[j+0] = rcs;  
+                              a   = pa[j+1];
+                              k0a = pk0a[j+1];
+                              rcs = rcs_f419_zmm16r4(a,k0a);
+                              prcs[j+1] = rcs;
+                              a   = pa[j+2];
+                              k0a = pk0a[j+2];
+                              rcs = rcs_f419_zmm16r4(a,k0a);
+                              prcs[j+2] = rcs; 
+                              a   = pa[j+3];
+                              k0a = pk0a[j+3];
+                              rcs = rcs_f419_zmm16r4(a,k0a);
+                              prcs[j+3] = rcs;   
+                              a   = pa[j+4];
+                              k0a = pk0a[j+4];
+                              rcs = rcs_f419_zmm16r4(a,k0a);
+                              prcs[j+4] = rcs;
+                              a   = pa[j+5];
+                              k0a = pk0a[j+5];
+                              rcs = rcs_f419_zmm16r4(a,k0a);
+                              prcs[j+5] = rcs; 
+                                                       
+                         }            
+              }
 
                    __ATTR_ALWAYS_INLINE__
 	           __ATTR_HOT__
