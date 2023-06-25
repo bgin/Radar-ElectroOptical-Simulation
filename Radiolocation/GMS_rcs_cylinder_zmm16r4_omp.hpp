fdf4aec51fdf9864bb6380eadcc3deadff174768
Bernard Gingold <beniekg@gmail.com>
2023-06-25 18:20:18 +0200

    rcs_f4124_zmm16r4_unroll6x_omp kernel.

--- Radiolocation/GMS_rcs_cylinder_zmm16r4_omp.hpp
+++ Radiolocation/GMS_rcs_cylinder_zmm16r4_omp.hpp
@@ -1213,6 +1213,89 @@ namespace gms {
               }
               
               
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void rcs_f4124_zmm16r4_unroll6x_omp(const __m512 * __restrict __ATTR_ALIGN__(64) pa,
+                                                       const __m512 * __restrict __ATTR_ALIGN__(64) pk0a,
+                                                       __m512 * __restrict __ATTR_ALIGN__(64) prcs,
+                                                       const int32_t n,
+                                                       int32_t & PF_DIST) {
+                                                   
+                         if(__builtin_expect(n<=0,0)) {return;}
+                         if(__builtin_expect(PF_DIST<=0)) PF_DIST = 6;
+                         __m512 a1,a2,a3,a4,a5,a6;
+                         __m512 k0a1,k0a2,k0a3,k0a4,k0a5,k0a6;
+                         __m512 rcs1,rcs2,rcs3,rcs4,rcs5,rcs6;
+                         int32_t j,m,m1;
+                         
+                         m = n%6;
+                         if(m!=0) {
+                            for(j = 0; j != m; ++j) {
+                                a   = pa[j];
+                                k0a = pk0a[j];
+                                rcs = rcs_f4124_zmm16r4(a,k0a);
+                                prcs[j] = rcs;
+                            }
+                            if(n<6) {return;}
+                         }                    
+                         
+                         m1 = m+1;
+#pragma omp parallel for schedule(runtime) default(none)                         \
+        firstprivate(m1,PF_DIST) private(j,a1,a2,a3,a4,a5,a6)                    \
+                                 private(k0a1,k0a2,k0a3,k0a4,k0a5,k0a6)          \
+                                 private(rcs1,rcs2,rcs3,rcs4,rcs5,rcs6)          \
+                                 shared(n,pa,pk0a,prcs)
+                         for(j = m1; j != n; j += 6) {
+#if (__RCS_CYLINDER_PF_CACHE_HINT__) == 1
+                              _mm_prefetch((char*)&pa[j+PF_DIST],_MM_HINT_T0);
+                              _mm_prefetch((char*)&pk0a[j+PF_DIST],_MM_HINT_T0);
+#elif (__RCS_CYLINDER_PF_CACHE_HINT__) == 2
+                              _mm_prefetch((char*)&pa[j+PF_DIST],_MM_HINT_T1);
+                              _mm_prefetch((char*)&pk0a[j+PF_DIST],_MM_HINT_T1);
+#elif  (__RCS_CYLINDER_PF_CACHE_HINT__) == 3
+                              _mm_prefetch((char*)&pa[j+PF_DIST],_MM_HINT_T2);
+                              _mm_prefetch((char*)&pk0a[j+PF_DIST],_MM_HINT_T2);
+#elif  (__RCS_CYLINDER_PF_CACHE_HINT__) == 4
+                              _mm_prefetch((char*)&pa[j+PF_DIST],_MM_HINT_NTA);
+                              _mm_prefetch((char*)&pk0a[j+PF_DIST],_MM_HINT_NTA);
+#endif             
+                              a1   = pa[j+0];
+                              k0a1 = pk0a[j+0];
+                              rcs = rcs_f4124_zmm16r4(a1,k0a1);
+                              prcs[j+0] = rcs1;  
+                              a2   = pa[j+1];
+                              k0a2 = pk0a[j+1];
+                              rcs2 = rcs_f4124_zmm16r4(a2,k0a2);
+                              prcs[j+1] = rcs2;
+                              a3   = pa[j+2];
+                              k0a3 = pk0a[j+2];
+                              rcs3 = rcs_f4124_zmm16r4(a3,k0a3);
+                              prcs[j+2] = rcs3; 
+                              a4   = pa[j+3];
+                              k0a4 = pk0a[j+3];
+                              rcs4 = rcs_f4124_zmm16r4(a4,k0a4);
+                              prcs[j+3] = rcs4;   
+                              a5   = pa[j+4];
+                              k0a55 = pk0a[j+4];
+                              rcs5 = rcs_f4124_zmm16r4(a5,k0a5);
+                              prcs[j+4] = rcs5;
+                              a6   = pa[j+5];
+                              k0a6 = pk0a[j+5];
+                              rcs6 = rcs_f4124_zmm16r4(a6,k0a6);
+                              prcs[j+5] = rcs6; 
+                         
+                           
+                         }            
+              }
+              
+              
+              
+              
+              
+              
 	       
 	       
 	       
