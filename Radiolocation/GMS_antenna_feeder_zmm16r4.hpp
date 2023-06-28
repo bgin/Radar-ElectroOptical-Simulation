e03bdeb559671fc799ff7a1840d8cc69e677fcd5
Bernard Gingold <beniekg@gmail.com>
2023-06-28 14:56:15 +0200

    hve_f213_zmm16r4_wedint_a kernel.

--- Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
+++ Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
@@ -2299,6 +2299,83 @@ namespace gms {
                }
                
                
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+	           void hve_f213_zmm16r4_wedint_a( const float * __restrict __ATTR_ALIGN__(64)  pxre,
+	                                          const float * __restrict __ATTR_ALIGN__(64)  pxim,
+	                                          const float * __restrict __ATTR_ALIGN__(64)  pyre,
+	                                          const float * __restrict __ATTR_ALIGN__(64)  pyim,
+	                                          const float * __restrict __ATTR_ALIGN__(64)  pzre,
+	                                          const float * __restrict __ATTR_ALIGN__(64)  pzim,
+	                                          const float arg[5],
+	                                          std::complex<float> & hx,                        
+                                                  std::complex<float> & hy,
+                                                  std::complex<float> & hz) {
+                                              
+                                           
+                        constexpr float C12566370614359172953850573533118 = 
+                                              12.566370614359172953850573533118f; //4*pi 
+                        constexpr int32_t ntab = 16;   
+                        __ATTR_ALIGN__(64) float intxr[16];
+                        __ATTR_ALIGN__(64) float intxi[16];
+                        __ATTR_ALIGN__(64) float intyr[16];
+                        __ATTR_ALIGN__(64) float intyi[16];
+                        __ATTR_ALIGN__(64) float intzr[16];
+                        __ATTR_ALIGN__(64) float intzi[16];
+                        register __m512 vk,vr,ii,ir,invr,cer,cei,eai;
+                        register __m512 xr,xi,yr,yi,zr,zi;
+                        register float k,r,omg,eps,h;
+                        register float sxr,sxi,syr,syi,szr,szi,frac;
+                        xr = _mm512_load_ps(&pxre[0]);
+                        xi = _mm512_load_ps(&pxim[0]);
+                        yr = _mm512_load_ps(&pyre[0]);
+                        yi = _mm512_load_ps(&pyim[0]);
+                        zr = _mm512_load_ps(&pzre[0]);
+                        zi = _mm512_load_ps(&pzim[0]);
+                        k = arg[0];
+                        r = arg[1];
+                        vk   = _mm512_set1_ps(k);
+                        vr   = _mm512_set1_ps(r);
+                        ir   = _mm512_setzero_ps();
+                        invr = _mm512_rcp14_ps(vr);
+                        ii   = _mm512_set1_ps(-1.0f);
+                        omg   = arg[2];
+                        eps   = arg[3];
+                        eai  = _mm512_mul_ps(ii,_mm512_mul_ps(vk,vr));
+                        h    = arg[4];
+                        cexp_zmm16r4(ir,eai,&cer,&cei);
+                        cer  = _mm512_mul_ps(cer,invr);
+                        cei  = _mm512_mul_ps(cei,invr);
+                        _mm512_store_ps(&intxr[0],_mm512_mul_ps(xr,cer));
+                        _mm512_store_ps(&intyr[0],_mm512_mul_ps(yr,cer));
+                        _mm512_store_ps(&intzr[0],_mm512_mul_ps(zr,cer));
+                        _mm512_store_ps(&intxi[0],_mm512_mul_ps(xi,cei));
+                        _mm512_store_ps(&intyi[0],_mm512_mul_ps(yi,cei));
+                        _mm512_store_ps(&intzi[0],_mm512_mul_ps(zi,cei));
+                        sxr = 0.0f;
+                        sxi = sxr;
+                        syi = sxr;
+                        syr = sxr;
+                        szr = sxr;
+                        szi = sxr;
+                        float tmp = C12566370614359172953850573533118*omg*eps;
+                        frac = 1.0f/tmp;
+                        wedint(ntab,h,&intxr[0],sxr);
+                        wedint(ntab,h,&intxi[0],sxi);
+                        wedint(ntab,h,&intyr[0],syr);
+                        wedint(ntab,h,&intyi[0],syi);
+                        wedint(ntab,h,&intzr[0],szr);
+                        wedint(ntab,h,&intzi[0],szi);
+                        hx = {sxr*frac,sxi*frac};
+                        hy = {syr*frac,syi*frac};
+                        hz = {szr*frac,szi*frac};                 
+               }
+               
+               
+               
                
         } // radiolocation
 
