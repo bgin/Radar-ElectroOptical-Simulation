51ef39ad358dda26c3ed24a7176e196373fba45f
Bernard Gingold <beniekg@gmail.com>
2023-07-01 16:32:46 +0200

    hvem_f2135_zmm16r4_hiordq_u6x kernel.
    Added a complex mul.

--- Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
+++ Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
@@ -2262,12 +2262,12 @@ namespace gms {
 	           __ATTR_ALIGN__(32)
                    __ATTR_VECTORCALL__
 	           static inline
-	           void hvem_f2135_zmm16r4_hiordq_u6x_a(const float * __restrict __ATTR_ALIGN__(64)  pxre,
-	                                          const float * __restrict __ATTR_ALIGN__(64)  pxim,
-	                                          const float * __restrict __ATTR_ALIGN__(64)  pyre,
-	                                          const float * __restrict __ATTR_ALIGN__(64)  pyim,
-	                                          const float * __restrict __ATTR_ALIGN__(64)  pzre,
-	                                          const float * __restrict __ATTR_ALIGN__(64)  pzim,
+	           void hvem_f2135_zmm16r4_hiordq_u6x(const float * __restrict  pxre,
+	                                          const float * __restrict  pxim,
+	                                          const float * __restrict  pyre,
+	                                          const float * __restrict  pyim,
+	                                          const float * __restrict  pzre,
+	                                          const float * __restrict  pzim,
 	                                          float * __restrict __ATTR_ALIGN__(64) work, // size of work is 2*(n-1)
 	                                          fwork_t fw,
 	                                          const float arg[7],
@@ -2275,7 +2275,8 @@ namespace gms {
                                                   std::complex<float> & hy,
                                                   std::complex<float> & hz,
                                                   const int32_t n,
-                                                  const int32_t PF_DIST) { 
+                                                  const int32_t PF_DIST,
+                                                  const bool aligned) { 
                                                   
                         constexpr float C12566370614359172953850573533118 = 
                                               12.566370614359172953850573533118f; //4*pi 
@@ -2302,8 +2303,13 @@ namespace gms {
                         eps   = arg[6];
                         cer   = _mm512_mul_ps(cer,invr);
                         cei   = _mm512_mul_ps(cei,invr); 
-                        f2135_integrand_zmm16r4_u6x_a(pxre,pxim,pyre,pyim,pzre,pzim,
-                                                     fw,cer,cei,tmp2,n,PF_DIST);
+                        if(aligned) {
+                           f2135_integrand_zmm16r4_u6x_a(pxre,pxim,pyre,pyim,pzre,pzim,
+                                                        fw,cer,cei,tmp2,n,PF_DIST);
+                        } else {
+                           f2135_integrand_zmm16r4_u6x_u(pxre,pxim,pyre,pyim,pzre,pzim,
+                                                        fw,cer,cei,tmp2,n,PF_DIST); 
+                        }
                         sxr = 0.0f;
                         sxi = sxr;
                         syi = sxr;
