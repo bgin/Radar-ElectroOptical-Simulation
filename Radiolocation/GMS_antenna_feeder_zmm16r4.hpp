5e655ec3308a1ba092f409b3b087cd20859017ba
Bernard Gingold <beniekg@gmail.com>
2023-07-01 15:02:58 +0200

    hvemm_f2135_zmm16r4_cubint_u6x kernel.
    Added a complex mul.

--- Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
+++ Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
@@ -1904,23 +1904,24 @@ namespace gms {
 	           __ATTR_ALIGN__(32)
                    __ATTR_VECTORCALL__
 	           static inline
-	           void hvemm_f2135_zmm16r4_cubint_u6x_a(const float * __restrict __ATTR_ALIGN__(64)  pxre,
-	                                          const float * __restrict __ATTR_ALIGN__(64)  pxim,
-	                                          const float * __restrict __ATTR_ALIGN__(64)  pyre,
-	                                          const float * __restrict __ATTR_ALIGN__(64)  pyim,
-	                                          const float * __restrict __ATTR_ALIGN__(64)  pzre,
-	                                          const float * __restrict __ATTR_ALIGN__(64)  pzim,
-	                                          float * __restrict __ATTR_ALIGN__(64)  pxd,
-	                                          float * __restrict __ATTR_ALIGN__(64)  pyd,
-	                                          float * __restrict __ATTR_ALIGN__(64)  pzd,
-	                                          fwork_t fw,
-	                                          const float arg[10],
-	                                          std::complex<float> & hx,                        
-                                                  std::complex<float> & hy,
-                                                  std::complex<float> & hz,
-                                                  float err[6],
-                                                  const int32_t n,
-                                                  int32_t & PF_DIST) { 
+	           void hvemm_f2135_zmm16r4_cubint_u6x(const float * __restrict  pxre,
+	                                               const float * __restrict  pxim,
+	                                               const float * __restrict  pyre,
+	                                               const float * __restrict  pyim,
+	                                               const float * __restrict  pzre,
+	                                               const float * __restrict  pzim,
+	                                               float * __restrict  pxd,
+	                                               float * __restrict  pyd,
+	                                               float * __restrict  pzd,
+	                                               fwork_t fw,
+	                                               const float arg[10],
+	                                               std::complex<float> & hx,                        
+                                                       std::complex<float> & hy,
+                                                       std::complex<float> & hz,
+                                                       float err[6],
+                                                       const int32_t n,
+                                                       int32_t & PF_DIST,
+                                                       const bool aligned) { 
                                                   
                         
                          constexpr float C12566370614359172953850573533118 = 
@@ -1954,8 +1955,13 @@ namespace gms {
                         cei  = _mm512_mul_ps(cei,invr);
                         tmp2 = tmp1*inv;
                         eps  = arg[9];  
-                        f2135_integrand_zmm16r4_u6x_a(pxre,pxim,pyre,pyim,pzre,pzim,
+                        if(aligned) {
+                           f2135_integrand_zmm16r4_u6x_a(pxre,pxim,pyre,pyim,pzre,pzim,
                                                      fw,cer,cei,tmp2,n,PF_DIST);
+                        } else {
+                           f2135_integrand_zmm16r4_u6x_u(pxre,pxim,pyre,pyim,pzre,pzim,
+                                                     fw,cer,cei,tmp2,n,PF_DIST);
+                        }
                         sxr = 0.0f;
                         sxi = sxr;
                         syi = sxr;
