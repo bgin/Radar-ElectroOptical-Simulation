a112e6a49fd7a7467ae38bf8a96013aaffa6bcf6
Bernard Gingold <beniekg@gmail.com>
2023-07-04 14:38:00 +0200

    cmean_quot_zmm16r4 kernel.

--- SIMD-kernels/GMS_complex_zmm16r4.hpp
+++ SIMD-kernels/GMS_complex_zmm16r4.hpp
@@ -2528,6 +2528,36 @@ namespace  gms {
                         sim   = _mm512_reduce_add_ps(iquot);
                         *mim  = sre*inv16;
               }  
+              
+              
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+                   void cmean_quot_zmm16r4(  const zmm16c4_t x,
+                                             const zmm16c4_t y,
+                                             float * __restrict mre,
+                                             float * __restrict mim) {
+
+                        register __m512 rep,imp,den,rquot,iquot;
+                        constexpr float inv16 = 0.0625f;
+                        float sre,sim;
+                        sre  = 0.0f;
+                        rep  = _mm512_fmsub_ps(x.re,y.re,
+                                               _mm512_mul_ps(x.im,y.im));
+                        imp  = _mm512_fmadd_ps(xim,yre,
+                                               _mm512_mul_ps(x.re,y.im));
+                        sim  = 0.0f;
+                        den  = _mm512_fmadd_ps(y.re,y.re,
+                                               _mm512_mul_ps(y.im,y.im));
+                        rquot = _mm512_div_ps(rep,den);
+                        sre   = _mm512_reduce_add_ps(rquot);
+                        *mre  = sre*inv16;
+                        iquot = _mm512_div_ps(imp,den);
+                        sim   = _mm512_reduce_add_ps(iquot);
+                        *mim  = sre*inv16;
+              }  
 
 
                    __ATTR_ALWAYS_INLINE__
