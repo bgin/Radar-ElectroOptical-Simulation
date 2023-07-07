a3790876318f3afa20736c7df80c2447e79f880c
Bernard Gingold <beniekg@gmail.com>
2023-07-07 18:10:31 +0200

    Nem_f2235_zmm16r4_simpne_u6x kernel.

--- Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
+++ Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
@@ -5906,7 +5906,7 @@ namespace gms {
 	           __ATTR_ALIGN__(32)
                    __ATTR_VECTORCALL__
 	           static inline
-	           void Nem_f2235_zmm16r4_simpne(const float * __restrict __ATTR_ALIGN__(64) pjxr,
+	           void Nem_f2235_zmm16r4_simpne_a(const float * __restrict __ATTR_ALIGN__(64) pjxr,
 	                                         const float * __restrict __ATTR_ALIGN__(64) pjxi,
 	                                         const float * __restrict __ATTR_ALIGN__(64) pjyr,
 	                                         const float * __restrict __ATTR_ALIGN__(64) pjyi,
@@ -5985,6 +5985,60 @@ namespace gms {
 	      }
 	     
 	     
+	           __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+	           void Nem_f2235_zmm16r4_simpne_u6x(const float * __restrict  pjxr,
+	                                             const float * __restrict  pjxi,
+	                                             const float * __restrict  pjyr,
+	                                             const float * __restrict  pjyi,
+	                                             const float * __restrict  pjzr,
+	                                             const float * __restrict  pjzi,
+	                                             const float * __restrict  prho,
+	                                             const float * __restrict  pcst,
+	                                             float * __restrict  pxd,
+	                                             float * __restrict  pyd,
+	                                             float * __restrict  pzd,
+	                                             fwork_t fw,
+	                                             const float k,
+	                                             std::complex<float> & Nx,
+	                                             std::complex<float> & Ny,
+	                                             std::complex<float> & Nz,
+	                                             const int32_t n,
+	                                             const int32_t PF_DIST,
+	                                             const bool aligned) {
+	                                         
+	             
+                        float sxr,sxi,syr,syi,szr,szi;  
+                        if(aligned) {
+                            f2235_integrand_zmm16r4_u6x_a(pjxr,pjxi,pjyr,pjyi,
+                                                         pjzr,pjzi,prho,pcst,
+                                                         fw,k,n,PF_DIST);
+                        }
+                        else {
+                            f2235_integrand_zmm16r4_u6x_u(pjxr,pjxi,pjyr,pjyi,
+                                                         pjzr,pjzi,prho,pcst,
+                                                         fw,k,n,PF_DIST);
+                        }  
+                        sxr = 0.0f;
+                        sxi = sxr;
+                        syi = sxr;
+                        syr = sxr;
+                        szr = sxr;
+                        szi = sxr;
+                        simpne(n,&pxd[0],&fw.pxr[0],sxr);
+                        simpne(n,&pxd[0],&fw.pxi[0],sxi);
+                        simpne(n,&pyd[0],&fw.pyr[0],syr);
+                        simpne(n,&pyd[0],&fw.pyi[0],syi);
+                        simpme(n,&pyd[0],&fw.pzr[0],szr);
+                        simpne(n,&pyd[0],&fw.pzi[0],szi);  
+                        
+                        Nx = {sxr,sxi};
+                        Ny = {syr,syi};
+                        Nz = {szr,szi};                          
+	      }
 	     
                  
                
