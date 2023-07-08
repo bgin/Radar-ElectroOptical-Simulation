be98e3f7f254cd4e13c7f747912a72e6c12df206
Bernard Gingold <beniekg@gmail.com>
2023-07-08 16:53:11 +0200

    spher_unitv_zmm16r4 kernel
    Corrected implementation.

--- Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
+++ Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
@@ -76,16 +76,13 @@ namespace gms {
                    }
                    
                    
-                     struct __ATTR_ALIGN__(64) dwork_t {
-                   
-                          double * __restrict pxr;
-                          double * __restrict pxi;
-                          double * __restrict pyr;
-                          double * __restrict pyi;
-                          double * __restrict pzr;
-                          double * __restrict pzi;
-                          PAD_TO(0,16)
-                   }
+                  // Spherical unit vector.
+                  struct __ATTR_ALIGN__(64) er16_t {
+                  
+                         __m512 x;
+                         __m512 y;
+                         __m512 z;
+                  }
           
           
            
@@ -102,102 +99,34 @@ namespace gms {
 	                                   const __m512 ez,
 	                                   const __m512 tht,
 	                                   const __m512 phi,
-	                                   __m512 * __restrict er,
-	                                   __m512 * __restrict eth,
-	                                   __m512 * __restrict eph) {
+	                                   er16_t & er,
+	                                   er16_t & eth,
+	                                   er16_t & eph) {
 	               
 	               using namespace gms::math;                   
 	               register __m512 stht,sphi,cphi,ctht;
 	               register __m512 t0,t1;
-	               stht = xsinf(tht);
-	               sphi = xsinf(phi);
-	               t0   = _mm512_mul_ps(sphi,ey);
-	               cphi = xcosf(phi);
-	               t1   = _mm512_mul_ps(cphi,ex);
-	               ctht = xcosf(tht);
-	               *er  = _mm512_fmadd_ps(stht,t1,
-	                                 _mm512_fmadd_ps(stht,t0,
-	                                             _mm512_mul_ps(ctht,ez)));    
-	               *eth = _mm512_fmadd_ps(ctht,t1,
-	                                 _mm512_fmsub_ps(ctht,t0,
-	                                             _mm512_mul_ps(stht,ez)));  
-	               *eph = _mm512_fmadd_ps(negate_zmm16r4(sphi),ex,
-	                                             _mm512_mul_ps(cphi,ey));                           
+	               stht  = xsinf(tht);
+	               ctht  = xcosf(tht);
+	               cphi  = xcosf(phi);
+	               t0    = _mm512_mul_ps(ctht,ex);
+	               sphi  = xsinf(phi);
+	               t1    = _mm512_mul_ps(sphi,ey);   
+	               er.x  = _mm512_mul_ps(stht,t0);
+	               er.y  = _mm512_mul_ps(stht,t1);
+	               er.z  = _mm512_mul_ps(ctht,ez); 
+	               eth.x = _mm512_mul_ps(ctht,t0);
+	               eth.y = _mm512_mul_ps(ctht,t1);
+	               eth.z = negate_zmm16r4(
+	                            _mm512_mul_ps(stht,ez)); 
+	               eph.x = negate_zmm16r4(
+	                            _mm512_mul_ps(sphi,ex));
+	               eph.y = _mm512_mul_ps(cphi,ey);
+	               eph.z = _mm512_setzero_ps();     
 	       }
 	       
 	       
-	           __ATTR_ALWAYS_INLINE__
-	           __ATTR_HOT__
-	           __ATTR_ALIGN__(32)
-                   __ATTR_VECTORCALL__
-	           static inline
-	           void spher_unitv_zmm16r4_a(const float * __restrict __ATTR_ALIGN__(64) pex,
-	                                      const float * __restrict __ATTR_ALIGN__(64) pey,
-	                                      const float * __restrict __ATTR_ALIGN__(64) pez,
-	                                      const float * __restrict __ATTR_ALIGN__(64) ptht,
-	                                      const float * __restrict __ATTR_ALIGN__(64) pphi,
-	                                      float * __restrict __ATTR_ALIGN__(64)  er,
-	                                      float * __restrict __ATTR_ALIGN__(64)  eth,
-	                                      float * __restrict __ATTR_ALIGN__(64)  eph) {
-	               
-	               using namespace gms::math;     
-	               register __m512 ex = _mm512_load_ps(&pex[0]);
-	               register __m512 ey = _mm512_load_ps(&pey[0]);
-	               register __m512 ez = _mm512_load_ps(&pez[0]);              
-	               register __m512 stht,sphi,cphi,ctht;
-	               register __m512 t0,t1;
-	               stht = xsinf(tht);
-	               sphi = xsinf(phi);
-	               t0   = _mm512_mul_ps(sphi,ey);
-	               cphi = xcosf(phi);
-	               t1   = _mm512_mul_ps(cphi,ex);
-	               ctht = xcosf(tht);
-	               _mm512_store_ps(&er[0]  ,_mm512_fmadd_ps(stht,t1,
-	                                                  _mm512_fmadd_ps(stht,t0,
-	                                                       _mm512_mul_ps(ctht,ez))));    
-	               _mm512_store_ps(&eth[0] ,_mm512_fmadd_ps(ctht,t1,
-	                                                  _mm512_fmsub_ps(ctht,t0,
-	                                                       _mm512_mul_ps(stht,ez))));  
-	               _mm512_store_ps(&eph[0] ,_mm512_fmadd_ps(negate_zmm16r4(sphi),ex,
-	                                                       _mm512_mul_ps(cphi,ey)));                           
-	       }
-	       
-	       
-	           __ATTR_ALWAYS_INLINE__
-	           __ATTR_HOT__
-	           __ATTR_ALIGN__(32)
-                   __ATTR_VECTORCALL__
-	           static inline
-	           void spher_unitv_zmm16r4_u(const float * __restrict  pex,
-	                                      const float * __restrict  pey,
-	                                      const float * __restrict  pez,
-	                                      const float * __restrict  ptht,
-	                                      const float * __restrict  pphi,
-	                                      float * __restrict  er,
-	                                      float * __restrict  eth,
-	                                      float * __restrict  eph) {
-	               
-	               using namespace gms::math;     
-	               register __m512 ex = _mm512_load_ps(&pex[0]);
-	               register __m512 ey = _mm512_load_ps(&pey[0]);
-	               register __m512 ez = _mm512_load_ps(&pez[0]);              
-	               register __m512 stht,sphi,cphi,ctht;
-	               register __m512 t0,t1;
-	               stht = xsinf(tht);
-	               sphi = xsinf(phi);
-	               t0   = _mm512_mul_ps(sphi,ey);
-	               cphi = xcosf(phi);
-	               t1   = _mm512_mul_ps(cphi,ex);
-	               ctht = xcosf(tht);
-	               _mm512_store_ps(&er[0]  ,_mm512_fmadd_ps(stht,t1,
-	                                                  _mm512_fmadd_ps(stht,t0,
-	                                                       _mm512_mul_ps(ctht,ez))));    
-	               _mm512_store_ps(&eth[0] ,_mm512_fmadd_ps(ctht,t1,
-	                                                  _mm512_fmsub_ps(ctht,t0,
-	                                                       _mm512_mul_ps(stht,ez))));  
-	               _mm512_store_ps(&eph[0] ,_mm512_fmadd_ps(negate_zmm16r4(sphi),ex,
-	                                                       _mm512_mul_ps(cphi,ey)));                           
-	       }
+	         
 	       
 	       /*
 	             Function 'N' = Nth(theta,phi)*eth+Nphi(theta,phi)*ephi
@@ -6836,7 +6765,34 @@ namespace gms {
 	        }
 	        
 	     
-	       
+	           __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+	           __m512 f226_zmm16r4_u(const float * __restrict  ptht1,
+	                                 const float * __restrict  ptht2,
+	                                 const float * __restrict  pphi1,
+	                                 const float * __restrict  pphi2) {
+	                 
+	                register __m512 tht1,tht2,phi1,phi2;
+	                register __m512 ctht1,ctht2,stht1,stht2;
+	                register __m512 cdif,cpsi;
+	                tht1  = _mm512_loadu_ps(&ptht1[0]);
+	                tht2  = _mm512_loadu_ps(&ptht2[0]);
+	                phi1  = _mm512_loadu_ps(&pphi1[0]);
+	                phi2  = _mm512_loadu_ps(&pphi2[0]);
+	                dif   = _mm512_sub_ps(phi1,phi2);
+	                stht1 = xsinf(tht1);
+	                ctht1 = xcosf(tht1);
+	                stht2 = xsinf(tht2);
+	                cdif  = xcosf(dif);
+	                ctht2 = xcosf(tht2);
+	                cpsi  = _mm512_fmadd_ps(ctht1,ctht2,
+	                                    _mm512_mul_ps(stht1,
+	                                             _mm512_mul_ps(stht2,cdif)));
+	                return (cpsi);   
+	        }
 	     
 	      
 	     
