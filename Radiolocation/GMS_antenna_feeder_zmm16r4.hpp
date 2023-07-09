a67721f7411cfe65653cce4dd3c68c76cb4e8c09
Bernard Gingold <beniekg@gmail.com>
2023-07-09 08:47:11 +0200

    N_f13_zmm16r4 kernel.

--- Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
+++ Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
@@ -195,80 +195,36 @@ namespace gms {
 	           __ATTR_ALIGN__(32)
                    __ATTR_VECTORCALL__
 	           static inline
-	           void f13_zmm16r4(            const __m512 eth,
-	                                        const __m512 eph,
-	                                        const __m512 nthr,
-	                                        const __m512 nthi,
-	                                        const __m512 nphr,
-	                                        const __m512 nphi,
-	                                        __m512 * __restrict Nthr,
-	                                        __m512 * __restrict Nthi,
-	                                        __m512 * __restrict Nphr,
-	                                        __m512 * __restrict Nphi) {
-	                                      
-	                *Nthr = _mm512_mul_ps(nthr,eth);
-	                *Nphr = _mm512_mul_ps(nphr,eph);
-	                *Nthi = _mm512_mul_ps(nthi,eth);
-	                *Nphi = _mm512_mul_ps(nphi,eph);                             
-	       }
-	       
-	       
-	           __ATTR_ALWAYS_INLINE__
-	           __ATTR_HOT__
-	           __ATTR_ALIGN__(32)
-                   __ATTR_VECTORCALL__
-	           static inline
-	           void f13_zmm16r4_a( const float * __restrict __ATTR_ALIGN__(64) peth,
-	                               const float * __restrict __ATTR_ALIGN__(64) peph,
-	                               const float * __restrict __ATTR_ALIGN__(64) pnthr,
-	                               const float * __restrict __ATTR_ALIGN__(64) pnthi,
-	                               const float * __restrict __ATTR_ALIGN__(64) pnphr,
-	                               const float * __restrict __ATTR_ALIGN__(64) pnphi,
-	                               float * __restrict __ATTR_ALIGN__(64) Nthr,
-	                               float * __restrict __ATTR_ALIGN__(64) Nthi,
-	                               float * __restrict __ATTR_ALIGN__(64) Nphr,
-	                               float * __restrict __ATTR_ALIGN__(64) Nphi) {
+	           void N_f13_zmm16r4(const __m512 ntr,
+	                              const __m512 nti,
+	                              const __m512 npr,
+	                              const __m512 npi,
+	                              const SUV_zmm16r4_t eth,
+	                              const SUV_zmm16r4_t eph,
+	                              __m512 * __restrict Nthr,
+	                              __m512 * __restrict Nthi,
+	                              __m512 * __restrict Nphr,
+	                              __m512 * __restrict Nphi) {
 	                              
-	                 register __m512 eth = _mm512_load_ps(&peth[0]);
-	                 register __m512 eph = _mm512_load_ps(&peph[0]); 
-	                 register __m512 nthr= _mm512_load_ps(&pnthr[0]);  
-	                 register __m512 nthi= _mm512_load_ps(&pnthi[0]);  
-	                 register __m512 nphr= _mm512_load_ps(&pnphr[0]);  
-	                 register __m512 nphi= _mm512_load_ps(&pnphi[0]); 
-	                 _mm512_store_ps(&Nthr[0] ,_mm512_mul_ps(nthr,eth));
-	                 _mm512_store_ps(&Nphr[0] ,_mm512_mul_ps(nphr,eph));
-	                 _mm512_store_ps(&Nthi[0] ,_mm512_mul_ps(nthi,eth));
-	                 _mm512_store_ps(&Nphi[0] ,_mm512_mul_ps(nphi,eph));                             
+	                register __m512 t0r,t0i,t1r,t1i;
+	                t0r   = _mm512_fmadd_ps(ntr,eth.x,
+	                                  _mm512_fmsub_ps(ntr,eth.y,
+	                                              _mm512_mul_ps(ntr,eth.z)));
+	                *Nthr = t0r;
+	                t0i   = _mm512_fmadd_ps(nti,eth.x,
+	                                  _mm512_fmsub_ps(nti,eth.y,
+	                                              _mm512_mul_ps(nti,eth.z)));   
+	                *Nthi = t0i;
+	                t1r   = _mm512_fmadd_ps(npr,eph.x,
+	                                    _mm512_mul_ps(npr,eph.y));
+	                *Nphr = t1r;
+	                t1i   = _mm512_fmadd_ps(npi,eph.x,
+	                                    _mm512_mul_ps(npi,eph.y));   
+	                *Nphi = t1i;              
 	       }
-	       
-	       
-	           __ATTR_ALWAYS_INLINE__
-	           __ATTR_HOT__
-	           __ATTR_ALIGN__(32)
-                   __ATTR_VECTORCALL__
-	           static inline
-	           void f13_zmm16r4_u( const float * __restrict  peth,
-	                               const float * __restrict  peph,
-	                               const float * __restrict  pnthr,
-	                               const float * __restrict  pnthi,
-	                               const float * __restrict  pnphr,
-	                               const float * __restrict  pnphi,
-	                               float * __restrict  Nthr,
-	                               float * __restrict  Nthi,
-	                               float * __restrict  Nphr,
-	                               float * __restrict  Nphi) {
 	                              
-	                 register __m512 eth = _mm512_loadu_ps(&peth[0]);
-	                 register __m512 eph = _mm512_loadu_ps(&peph[0]); 
-	                 register __m512 nthr= _mm512_loadu_ps(&pnthr[0]);  
-	                 register __m512 nthi= _mm512_loadu_ps(&pnthi[0]);  
-	                 register __m512 nphr= _mm512_loadu_ps(&pnphr[0]);  
-	                 register __m512 nphi= _mm512_loadu_ps(&pnphi[0]); 
-	                 _mm512_storeu_ps(&Nthr[0] ,_mm512_mul_ps(nthr,eth));
-	                 _mm512_storeu_ps(&Nphr[0] ,_mm512_mul_ps(nphr,eph));
-	                 _mm512_storeu_ps(&Nthi[0] ,_mm512_mul_ps(nthi,eth));
-	                 _mm512_storeu_ps(&Nphi[0] ,_mm512_mul_ps(nphi,eph));                             
-	       }
+	       
+	        
 	       
 	       
 	       /*
