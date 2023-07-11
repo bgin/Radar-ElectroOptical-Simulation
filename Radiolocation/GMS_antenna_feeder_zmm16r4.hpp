6a96779ef9a166cc104bae90a1bbd522049d63a4
Bernard Gingold <beniekg@gmail.com>
2023-07-11 08:48:02 +0200

    Scp_f238_zmm16r4 kernel.

--- Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
+++ Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
@@ -7640,7 +7640,117 @@ namespace gms {
 	        }
 	       
 	       
+	           __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+	           void Em_f228_zmm16r4_u(const float * __restrict pHtr,
+	                                  const float * __restrict pHti,
+	                                  const float * __restrict pHpr,
+	                                  const float * __restrict pHpi,
+	                                  const SUV_zmm16r4_t er,
+	                                  float eps,
+	                                  float mu,
+	                                  float * __restrict  Etr,
+	                                  float * __restrict  Eti,
+	                                  float * __restrict  Epr,
+	                                  float * __restrict  Epi) {
+	                           
+	                 using namespace gms::math;    
+	                 register __m512 Htr,Hti;
+	                 register __m512 Hpr,Hpi;
+	                 register __m512 t0r,t0i;
+	                 register __m512 t1r,t1i;
+	                 register __m512 t2r,t2i;
+	                 register __m512 frac,C00;
+	                 float tmp;
+	                 Htr  = _mm512_loadu_ps(&pHtr[0]);
+	                 Hti  = _mm512_loadu_ps(&pHti[0]);
+	                 Hpr  = _mm512_loadu_ps(&pHpr[0]);
+	                 Hpi  = _mm512_loadu_ps(&pHpi[0]);
+	                 tmp  = cephes_sqrtf(eps/mu);
+	                 C00  = _mm512_setzero_ps();
+	                 frac = _mm512_set1_ps(tmp);
+	                 t0r  = _mm512_fmsub_ps(Htr,er.z,
+	                                    _mm512_mul_ps(Hpr,er.y));
+	                 t0i  = _mm512_fmsub_ps(Hti,er.z,
+	                                    _mm512_mul_ps(Hpi,er.y));
+	                 _mm512_storeu_ps(&Etr[0] ,
+	                             negate_zmm16r4(_mm512_mul_ps(frac,t0r)));
+	                 _mm512_storeu_ps(&Eti[0] , 
+	                             negate_zmm16r4(_mm512_mul_ps(frac,t0i)));
+	                 t1r  = _mm512_fmsub_ps(Hpr,er.x,C00);
+	                 t1i  = _mm512_fmsub_ps(Hpi,er.x,C00);
+	                 _mm512_storeu_ps(&Epr[0] ,
+	                             negate_zmm16r4(_mm512_mul_ps(frac,t1r)));
+	                 _mm512_storeu_ps(&Epi[0] ,
+	                             negate_zmm16r4(_mm512_mul_ps(frac,t1i)));
+	                 // unused, but shall stay to be correct mathematically. 
+	                 t2r  = _mm512_sub_ps(C00,
+	                                    _mm512_mul_ps(Htr,er.x));
+	                 t2i  = _mm512_sub_ps(C00,
+	                                    _mm512_mul_ps(Hti,er.x));
+	                                                 
+	        }
 	        
+	        
+	        /*
+	              Formula: 2-38, p. 39
+	        */
+	        
+	        
+	           __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   __ATTR_VECTORCALL__
+	           static inline
+	           SUV_zmm16r4_t Scp_f238_zmm16r4(const __m512 Netr,
+	                                          const __m512 Neti,
+	                                          const __m512 Nepr,
+	                                          const __m512 Nepi,
+	                                          const __m512 Nmtr,
+	                                          const __m512 Nmti,
+	                                          const __m512 Nmpr,
+	                                          const __m512 Nmpi,
+	                                          const SUV_zmm16r4_t er,
+	                                          const float args[4]) {
+	               SUV_zmm16r4_t scp;                           
+	               register __m512 cabs1,cabs2,
+	               register __m512 t0r,t0i;
+	               register __m512 t1r,t1i;
+	               register __m512 vfrac,vrat,trm;
+	               float frac,rat,k,R,mu,eps;  
+	               float t0,t1,t2;
+	               constexpr float C0009947183943243458485555235211 = 
+	                                      0.009947183943243458485555235211f;
+	               mu   = args[2];
+	               eps  = args[3];
+	               rat  = cephes_sqrtf(mu/eps);
+	               k    = args[0];
+	               vrat = _mm512_set1_ps(rat);
+	               R    = args[1]; 
+	               t0r  = _mm512_fmadd_ps(vrat,Netr,Nmpr);
+	               t0i  = _mm512_fmadd_ps(vrat,Neti,Nmpi);
+	               cabs1= cabs_zmm16r4(t0r,t0i);
+	               t1r  = _mm512_fmsub_ps(vrat,Nepr,Nmtr);
+	               t1i  = _mm512_fmsub_ps(vrat,Nepi,Nmti);
+	               cabs2= cabs_zmm16r4(t1r,t1i);
+	               t0   = R*R;
+	               t1   = k*k;
+	               t2   = cephes_sqrtf(eps,mu);
+	               trm  = _mm512_fmadd_ps(cabs1,cabs1,
+	                                  _mm512_mul_ps(cabs2,cabs2));
+	               frac = (t1*t2)/(C0009947183943243458485555235211*t0);   
+	               vfrac= _mm512_set1_ps(frac);
+	               t0r  = _mm512_mul_ps(er.x,trm);
+	               scp.x= _mm512_mul_ps(t0r,vfrac);
+	               t0i  = _mm512_mul_ps(er.y,trm);
+	               scp.y= _mm512_mul_ps(t0i,vfrac);
+	               t1r  = _mm512_mul_ps(er.z,trm);
+	               scp.z= _mm512_mul_ps(t1r,vfrac);
+	               return (scp);                       
+	        }
                
         } // radiolocation
 
