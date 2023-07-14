3f06be410254eea960bcd871e8cea5eae0f2adee
Bernard Gingold <beniekg@gmail.com>
2023-07-14 14:12:17 +0200

    N_f256_zmm16r4_avint_dispatch kernel.

--- Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
+++ Radiolocation/GMS_antenna_feeder_zmm16r4.hpp
@@ -10206,6 +10206,120 @@ namespace gms {
 	          }   
 	    
             }
+            
+            
+                   __ATTR_ALWAYS_INLINE__
+	           __ATTR_HOT__
+	           __ATTR_ALIGN__(32)
+                   static inline
+	           void N3D_f256_zmm16r4_avint_dispatch(const float * __restrict  phxr,
+	                                                 const float * __restrict  phxi,
+	                                                 const float * __restrict  phyr,
+	                                                 const float * __restrict  phyi,
+	                                                 const float * __restrict  phzr,
+	                                                 const float * __restrict  phzi,
+	                                                 const float * __restrict  pnx,
+	                                                 const float * __restrict  pny,
+	                                                 const float * __restrict  pnz,
+	                                                 float * __restrict  pxd,
+	                                                 float * __restrict  pyd,
+	                                                 float * __restrict  pzd,
+	                                                 float * __restrict  prho,
+	                                                 float * __restrict  pcst,
+	                                                 fwork_t fw,
+	                                                 const float args[7],
+	                                                 std::complex<float> & Nex,
+	                                                 std::complex<float> & Ney,
+	                                                 std::complex<float> & Nez,
+	                                                 const int32_t n,
+	                                                 const int32_t PF_DIST,
+	                                                 const int32_t RANKSIZE,
+	                                                 const int32_t PAGESIZE,
+	                                                 const int32_t cond,
+	                                                 int32_t & ierr) {
+	                                                 
+	                float k,xa,xb,ya,yb,za,zb;
+                        float sxr,sxi,syr,syi,szr,szi;
+                        int32_t ier1,ier2,ier3,ier4,ier5,ier6;
+                            
+                        k   = args[0];
+                        xa  = args[1];
+                        xb  = args[2];
+                        ya  = args[3];
+                        yb  = args[4];
+                        za  = args[5];
+                        zb  = args[6];
+                        
+                        switch(cond) {
+                            
+                            case : 0 
+                                    f256_integrand_zmm16r4_u6x_a(phxr,phxi,phyr,
+                                                                 phyi,phzr,phzi,
+                                                                 pnx,pny,pnz,
+                                                                 prho,pcst,fw,
+                                                                 k,n,PF_DIST);    
+                                    break;
+                            case : 1 
+                                    f256_integrand_zmm16r4_u6x_u(phxr,phxi,phyr,
+                                                                 phyi,phzr,phzi,
+                                                                 pnx,pny,pnz,
+                                                                 prho,pcst,fw,
+                                                                 k,n,PF_DIST);  
+                              
+                            case : 2 
+                                    f256_integrand_zmm16r4_unroll_jam8x_a(phxr,phxi,phyr,
+                                                                          phyi,phzr,phzi,
+                                                                          pnx,pny,pnz,
+                                                                          prho,pcst,fw,
+                                                                          k,n,RANKSIZE,
+                                                                          PAGESIZE,
+                                                                          PF_DIST);
+                                    break;                                       
+                            case : 3 
+                                  f256_integrand_zmm16r4_unroll_jam4x_a(  phxr,phxi,phyr,
+                                                                          phyi,phzr,phzi,
+                                                                          pnx,pny,pnz,
+                                                                          prho,pcst,fw,
+                                                                          k,n,RANKSIZE,
+                                                                          PAGESIZE,
+                                                                          PF_DIST);  
+                                  break;
+                            default :
+                                   return;
+                              
+                        } 
+                        
+                        sxr = 0.0f;
+                        sxi = sxr;
+                        syi = sxr;
+                        syr = sxr;
+                        szr = sxr;
+                        szi = sxr;  
+                        sxr = avint(&pxd[0],&fw.pxr[0],xa,xb,ier1);
+                        sxi = avint(&pxd[0],&fw.pxi[0],xa,xb,ier2);
+                        if(ier1==3 || ier2==3) {goto ERROR;}
+                           goto CORRECT;
+                        syr = avint(&pyd[0],&fw.pyr[0],ya,yb,ier3);
+                        syi = avint(&pyd[0],&fw.pyi[0],ya,yb,ier4);
+                        if(ier3==3 || ier4==3) {goto ERROR;}
+                           goto CORRECT;
+                        szr = avint(&pzd[0],&fw.pzr[0],za,zb,ier5);
+                        szi = avint(&pzd[0],&fw.pzi[0],za,zb,ier6);
+                        if(ier5==3 || ier6==3) {goto ERROR;}
+                           goto CORRECT;
+                        ERROR:
+                           {
+                               ierr = 3;
+                               return;
+                        }  
+                        CORRECT: {
+                           Nex = {sxr,sxi};
+                           Ney = {syr,syi};
+                           Nez = {szr,szi};  
+                        }                
+                        
+                                                                   
+	   }
 	       
 	    
 	        
