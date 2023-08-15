0c769398bfd4d48a46454d3f00fd1a7cad714c7d
Bernard Gingold <beniekg@gmail.com>
2023-08-15 11:16:03 +0200

    Added SKX_cpu_utilization_t ADT.

--- Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
+++ Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
@@ -92,6 +92,7 @@ namespace gms {
                
                ~SKX_cpu_operating_freq_t() {
                      
+                     using namespace gms::common;
                      if(__builtin_expect(nullptr!=
                         m_cpu_clk_unhalted_thread,0))  gms_mm_free(m_cpu_clk_unhalted_thread);
                      if(__builtin_expect(nullptr!=
@@ -170,13 +171,133 @@ namespace gms {
                                                            fname,data_type);                            
           }
           
-      
-          
-          
-           
-           
+       
                
       };
+      
+      
+      //////////////////////////////////////////////////////////////////////////////
+      
+      /*
+            CPU utilization (percentage) all cores.
+       */
+       
+         template<int32_t len, int32_t lagh>
+         struct   SKX_cpu_utilization_t __ATTR_ALIGN__(64) {
+        
+                
+                 __ATTR_ALIGN__(8) double * __restrict m_cpu_clk_unhalted_ref_tsc;
+                 __ATTR_ALIGN__(8) double * __restrict m_tsc_frequency;
+                 __ATTR_ALIGN__(8) double * __restrict m_cpu_util;
+                            
+                 
+                 SKX_cpu_utilization_t() noexcept(true) {
+                      
+                  
+                      m_cpu_clk_unhalted_ref_tsc  = nullptr;
+                      m_tsc_frequency             = nullptr;
+                      m_cpu_op_util               = nullptr;
+               }
+               
+                 SKX_cpu_utilization_t() noexcept(false) {
+                      
+                      using namespace gms::common;
+                      const std::size_t samp_len = (std::size_t)len;
+                      const std::size_t align    = (std::size_t)64;
+                      m_cpu_clk_unhalted_ref_tsc = (double*)gms_mm_malloc(samp_len,
+                                                                          align);
+                      m_tsc_frequency            = (double*)gms_mm_malloc(samp_len,
+                                                                          align);
+                      m_cpu_util                 = (double*)gms_mm_malloc(samp_len,
+                                                                          align);
+               }
+               
+                 SKX_cpu_utilization_t(const  SKX_cpu_utilization_t &) = delete;
+               
+                 SKX_cpu_utilization_t(SKX_cpu_utilization_t &&) = delete
+               
+                 ~SKX_cpu_utilization_t() {
+                     
+                     using namespace gms::common;
+                     if(__builtin_expect(nullptr!=
+                        m_cpu_clk_unhalted_ref_tsc,0)) gms_mm_free(m_cpu_clk_unhalted_ref_tsc);
+                     if(__builtin_expect(nullptr!=
+                        m_tsc_frequency,0))            gms_mm_free(m_tsc_frequency);
+                     if(__builtin_expect(nullptr!=
+                        m_cpu_util,0))                 gms_mm_free(m_cpu_util);
+               }     
+               
+               
+                 SKX_cpu_utilization_t &
+                 operator=(const SKX_cpu_utilization_t &) = delete;
+               
+                 SKX_cpu_utilization_t &
+                 operator=(SKX_cpu_utilization_t &&) = delete;  
+               
+                 void compute_metric() {
+                    
+                    skx_cpu_utilization_samples(m_cpu_clk_unhalted_ref_tsc,
+                                                m_tsc_frequency,
+                                                m_cpu_util,len);
+                                             
+               }     
+               
+                 void analyze_metric_canarm(const char * __restrict fname,
+                                          const char * __restrict data_type,
+                                          const bool use_omp) {
+                   
+                    cpu_perf_time_series_canarm<len,lagh>(m_cpu_util,fname,
+                                                          data_type,use_omp);
+                                                                            
+             }  
+             
+                 void analyze_metric_unimar(const char * __restrict fname,
+                                        const char * __restrict data_type) {
+                 
+                    cpu_perf_time_series_unimar<len,lagh>(m_cpu_util,
+                                                          fname,data_type);                           
+            } 
+            
+                 void analyze_metric_unibar(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_unibar<len,lagh>(m_cpu_util,
+                                                          fname,data_type);                           
+           }    
+           
+                 void analyze_metric_exsar(const char * __restrict fname,
+                                     const char * __restrict data_type) {
+                                     
+                    cpu_perf_time_series_exsar<len,lagh>(m_cpu_util,
+                                                         fname,data_type);                       
+           }
+           
+                 void analyze_metric_bispec(const char * __restrict fname,
+                                      const char * __restrict data_type,
+                                      const bool use_omp) {
+                                      
+                    cpu_perf_time_series_bispec<len,lagh>(m_cpu_util,
+                                                          fname,data_type,use_omp);                          
+           }
+           
+                 void analyze_metric_thirmo(const char * __restrict fname,
+                                      const char * __restrict data_type) {
+                                      
+                    cpu_perf_time_series_thirmo<len,lagh>(m_cpu_util,
+                                                          fname,data_type);                
+           }
+           
+                 void analyze_metric_autocor(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_autocor<len,lagh>(m_cpu_util,
+                                                           fname,data_type);                            
+          }
+          
+       
+               
+      };
+      
 
 } // gms
 
