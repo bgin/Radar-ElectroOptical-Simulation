2db926fdf591b305aafde29896974ca73ab301c1
Bernard Gingold <beniekg@gmail.com>
2023-08-15 12:37:29 +0200

    Added  SKX_cpi_t.

--- Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
+++ Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
@@ -335,7 +335,7 @@ namespace gms {
                                                                               align);
                }
                
-                 SKX_cpu_utilization__krn_t(const  SKX_cpu_utilization_krn_t &) = delete;
+                 SKX_cpu_utilization_krn_t(const  SKX_cpu_utilization_krn_t &) = delete;
                
                  SKX_cpu_utilization_krn_t(SKX_cpu_utilization_krn_t &&) = delete
                
@@ -359,7 +359,7 @@ namespace gms {
                
                  void compute_metric() {
                     
-                    skx_cpu_utilization_samples(m_cpu_clk_unhalted_ref_tsc_sup,
+                    skx_cpu_utilization_kernel_samples(m_cpu_clk_unhalted_ref_tsc_sup,
                                                 m_tsc_frequency,
                                                 m_cpu_util_krn,len);
                                              
@@ -420,6 +420,129 @@ namespace gms {
        
                
       };
+      
+      /////////////////////////////////////////////////////////////////////////
+      
+      /*
+            Cycles Per Instruction (CPI).
+       */
+       
+         template<int32_t len, int32_t lagh>
+         struct   SKX_cpi_t __ATTR_ALIGN__(64) {
+        
+                
+                 __ATTR_ALIGN__(8) double * __restrict m_cpu_clk_unhalted_thread;
+                 __ATTR_ALIGN__(8) double * __restrict m_inst_retired_any;
+                 __ATTR_ALIGN__(8) double * __restrict m_cpi;
+                            
+                 
+                 SKX_cpi_t() noexcept(true) {
+                      
+                  
+                      m_cpu_clk_unhalted_thread  = nullptr;
+                      m_inst_retired_any         = nullptr;
+                      m_cpi                      = nullptr;
+               }
+               
+                 SKX_cpi_t() noexcept(false) {
+                      
+                      using namespace gms::common;
+                      const std::size_t samp_len = (std::size_t)len;
+                      const std::size_t align    = (std::size_t)64;
+                      m_cpu_clk_unhalted_thread  = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+                      m_inst_retired_any         = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+                      m_cpi                      = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+               }
+               
+                 SKX_cpi_t(const  SKX_cpi_t &) = delete;
+               
+                 SKX_cpi_t(SKX_cpi_t &&) = delete
+               
+                 ~SKX_cpi_t() {
+                     
+                     using namespace gms::common;
+                     if(__builtin_expect(nullptr!=
+                        m_cpu_clk_unhalted_thread,0)) gms_mm_free(m_cpu_clk_unhalted_thread);
+                     if(__builtin_expect(nullptr!=
+                        m_inst_retired_any,0))        gms_mm_free(m_inst_retired_any);
+                     if(__builtin_expect(nullptr!=
+                        m_cpi,0))                     gms_mm_free(m_cpi);
+               }     
+               
+               
+                 SKX_cpi_t &
+                 operator=(const SKX_cpi_t &) = delete;
+               
+                 SKX_cpi_t &
+                 operator=(SKX_cpi_t &&) = delete;  
+               
+                 void compute_metric() {
+                    
+                    skx_cycles_per_instr_samples(m_cpu_clk_unhalted_thread,
+                                                m_inst_retired_any,
+                                                m_cpi,len);
+                                             
+               }     
+               
+                 void analyze_metric_canarm(const char * __restrict fname,
+                                          const char * __restrict data_type,
+                                          const bool use_omp) {
+                   
+                    cpu_perf_time_series_canarm<len,lagh>(m_cpi,fname,
+                                                          data_type,use_omp);
+                                                                            
+             }  
+             
+                 void analyze_metric_unimar(const char * __restrict fname,
+                                        const char * __restrict data_type) {
+                 
+                    cpu_perf_time_series_unimar<len,lagh>(m_cpi,
+                                                          fname,data_type);                           
+            } 
+            
+                 void analyze_metric_unibar(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_unibar<len,lagh>(m_cpi,
+                                                          fname,data_type);                           
+           }    
+           
+                 void analyze_metric_exsar(const char * __restrict fname,
+                                     const char * __restrict data_type) {
+                                     
+                    cpu_perf_time_series_exsar<len,lagh>(m_cpi,
+                                                         fname,data_type);                       
+           }
+           
+                 void analyze_metric_bispec(const char * __restrict fname,
+                                      const char * __restrict data_type,
+                                      const bool use_omp) {
+                                      
+                    cpu_perf_time_series_bispec<len,lagh>(m_cpi,
+                                                          fname,data_type,use_omp);                          
+           }
+           
+                 void analyze_metric_thirmo(const char * __restrict fname,
+                                      const char * __restrict data_type) {
+                                      
+                    cpu_perf_time_series_thirmo<len,lagh>(m_cpi,
+                                                          fname,data_type);                
+           }
+           
+                 void analyze_metric_autocor(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_autocor<len,lagh>(m_cpi,
+                                                           fname,data_type);                            
+          }
+          
+       
+               
+      };
+      
       
       
 
