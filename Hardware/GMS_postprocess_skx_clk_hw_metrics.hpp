6e355d9acb2aa06b4ecef2a0273d8434302fc2a2
Bernard Gingold <beniekg@gmail.com>
2023-08-15 14:39:50 +0200

    Added SKX_branch_mispred_t.

--- Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
+++ Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
@@ -665,6 +665,128 @@ namespace gms {
                
       };
       
+      /////////////////////////////////////////////////////////////////////
+      
+      /*
+         Branch mispredict ratio.
+       */
+      
+         template<int32_t len, int32_t lagh>
+         struct   SKX_branch_mispred_t __ATTR_ALIGN__(64) {
+        
+                
+                 __ATTR_ALIGN__(8) double * __restrict m_br_misp_retired_all_branches;
+                 __ATTR_ALIGN__(8) double * __restrict m_br_inst_retired_all_branches;
+                 __ATTR_ALIGN__(8) double * __restrict m_br_mispred_rat;
+                            
+                 
+                 SKX_branch_mispred_t() noexcept(true) {
+                      
+                  
+                      m_br_misp_retired_all_branches  = nullptr;
+                      m_br_inst_retired_all_branches  = nullptr;
+                      m_br_mispred_rat                = nullptr;
+               }
+               
+                 SKX_branch_mispred_t() noexcept(false) {
+                      
+                      using namespace gms::common;
+                      const std::size_t samp_len = (std::size_t)len;
+                      const std::size_t align    = (std::size_t)64;
+                      m_br_misp_retired_all_branches = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+                      m_br_inst_retired_all_branches = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+                      m_br_mispred_rat               = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+               }
+               
+                 SKX_branch_mispred_t(const  SKX_branch_mispred_t &) = delete;
+               
+                 SKX_branch_mispred_t(SKX_branch_mispred_t &&) = delete
+               
+                 ~SKX_branch_mispred_t() {
+                     
+                     using namespace gms::common;
+                     if(__builtin_expect(nullptr!=
+                        m_br_misp_retired_all_branches,0))   gms_mm_free(m_br_misp_retired_all_branches);
+                     if(__builtin_expect(nullptr!=
+                        m_br_inst_retired_all_branches,0))   gms_mm_free(m_br_inst_retired_all_branches);
+                     if(__builtin_expect(nullptr!=
+                        m_br_mispred_rat,0))                 gms_mm_free(m_br_mispred_rat);
+               }     
+               
+               
+                 SKX_branch_mispred_t &
+                 operator=(const SKX_branch_mispred_t &) = delete;
+               
+                 SKX_branch_mispred_t &
+                 operator=(SKX_branch_mispred_t &&) = delete;  
+               
+                 void compute_metric() {
+                    
+                    skx_branch_mispred_ratio_samples( m_br_misp_retired_all_branches,
+                                                      m_br_inst_retired_all_branches,
+                                                      m_br_mispred_rat,len);
+                                             
+               }     
+               
+                 void analyze_metric_canarm(const char * __restrict fname,
+                                          const char * __restrict data_type,
+                                          const bool use_omp) {
+                   
+                    cpu_perf_time_series_canarm<len,lagh>(m_br_mispred_rat,fname,
+                                                          data_type,use_omp);
+                                                                            
+             }  
+             
+                 void analyze_metric_unimar(const char * __restrict fname,
+                                        const char * __restrict data_type) {
+                 
+                    cpu_perf_time_series_unimar<len,lagh>(m_br_mispred_rat,
+                                                          fname,data_type);                           
+            } 
+            
+                 void analyze_metric_unibar(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_unibar<len,lagh>(m_br_mispred_rat,
+                                                          fname,data_type);                           
+           }    
+           
+                 void analyze_metric_exsar(const char * __restrict fname,
+                                     const char * __restrict data_type) {
+                                     
+                    cpu_perf_time_series_exsar<len,lagh>(m_br_mispred_rat,
+                                                         fname,data_type);                       
+           }
+           
+                 void analyze_metric_bispec(const char * __restrict fname,
+                                      const char * __restrict data_type,
+                                      const bool use_omp) {
+                                      
+                    cpu_perf_time_series_bispec<len,lagh>(m_br_mispred_rat,
+                                                          fname,data_type,use_omp);                          
+           }
+           
+                 void analyze_metric_thirmo(const char * __restrict fname,
+                                      const char * __restrict data_type) {
+                                      
+                    cpu_perf_time_series_thirmo<len,lagh>(m_br_mispred_rat,
+                                                          fname,data_type);                
+           }
+           
+                 void analyze_metric_autocor(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_autocor<len,lagh>(m_br_mispred_rat,
+                                                           fname,data_type);                            
+          }
+          
+       
+               
+      };
+      
       
 
 } // gms
