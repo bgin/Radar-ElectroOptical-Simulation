855289ba56215da16d4fde0ab6ea8aeb02050dad
Bernard Gingold <beniekg@gmail.com>
2023-08-15 13:59:58 +0200

    Added SKX_emon_mux_t.

--- Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
+++ Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
@@ -543,6 +543,127 @@ namespace gms {
                
       };
       
+     ///////////////////////////////////////////////////////////////////////////////////// 
+     
+     /*
+          EMON event multiplexing reliability (>95% --- means satisfying ratio).
+      */    
+      
+         template<int32_t len, int32_t lagh>
+         struct   SKX_emon_mux_t __ATTR_ALIGN__(64) {
+        
+                
+                 __ATTR_ALIGN__(8) double * __restrict m_cpu_clk_unhalted_thread_p;
+                 __ATTR_ALIGN__(8) double * __restrict m_cpu_clk_unhalted_thread;
+                 __ATTR_ALIGN__(8) double * __restrict m_emon;
+                            
+                 
+                 SKX_emon_mux_t() noexcept(true) {
+                      
+                  
+                      m_cpu_clk_unhalted_thread_p  = nullptr;
+                      m_cpu_clk_unhalted_thread    = nullptr;
+                      m_emon                       = nullptr;
+               }
+               
+                 SKX_emon_mux_t() noexcept(false) {
+                      
+                      using namespace gms::common;
+                      const std::size_t samp_len = (std::size_t)len;
+                      const std::size_t align    = (std::size_t)64;
+                      m_cpu_clk_unhalted_thread_p  = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+                      m_cpu_clk_unhalted_thread    = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+                      m_emon                       = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+               }
+               
+                 SKX_emon_mux_t(const  SKX_emon_mux_t &) = delete;
+               
+                 SKX_emon_mux_t(SKX_emon_mux_t &&) = delete
+               
+                 ~SKX_emon_mux_t() {
+                     
+                     using namespace gms::common;
+                     if(__builtin_expect(nullptr!=
+                        m_cpu_clk_unhalted_thread_p,0)) gms_mm_free(m_cpu_clk_unhalted_thread_p);
+                     if(__builtin_expect(nullptr!=
+                        m_cpu_clk_unhalted_thread,0))   gms_mm_free(m_cpu_clk_unhalted_thread);
+                     if(__builtin_expect(nullptr!=
+                        m_emon,0))                      gms_mm_free(m_emon);
+               }     
+               
+               
+                 SKX_emon_mux_t &
+                 operator=(const SKX_emon_mux_t &) = delete;
+               
+                 SKX_emon_mux_t &
+                 operator=(SKX_emon_mux_t &&) = delete;  
+               
+                 void compute_metric() {
+                    
+                    skx_mux_reliability_samples( m_cpu_clk_unhalted_thread_p,
+                                                 m_cpu_clk_unhalted_thread,
+                                                 m_emon,len);
+                                             
+               }     
+               
+                 void analyze_metric_canarm(const char * __restrict fname,
+                                          const char * __restrict data_type,
+                                          const bool use_omp) {
+                   
+                    cpu_perf_time_series_canarm<len,lagh>(m_emon,fname,
+                                                          data_type,use_omp);
+                                                                            
+             }  
+             
+                 void analyze_metric_unimar(const char * __restrict fname,
+                                        const char * __restrict data_type) {
+                 
+                    cpu_perf_time_series_unimar<len,lagh>(m_emon,
+                                                          fname,data_type);                           
+            } 
+            
+                 void analyze_metric_unibar(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_unibar<len,lagh>(m_emon,
+                                                          fname,data_type);                           
+           }    
+           
+                 void analyze_metric_exsar(const char * __restrict fname,
+                                     const char * __restrict data_type) {
+                                     
+                    cpu_perf_time_series_exsar<len,lagh>(m_emon,
+                                                         fname,data_type);                       
+           }
+           
+                 void analyze_metric_bispec(const char * __restrict fname,
+                                      const char * __restrict data_type,
+                                      const bool use_omp) {
+                                      
+                    cpu_perf_time_series_bispec<len,lagh>(m_emon,
+                                                          fname,data_type,use_omp);                          
+           }
+           
+                 void analyze_metric_thirmo(const char * __restrict fname,
+                                      const char * __restrict data_type) {
+                                      
+                    cpu_perf_time_series_thirmo<len,lagh>(m_emon,
+                                                          fname,data_type);                
+           }
+           
+                 void analyze_metric_autocor(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_autocor<len,lagh>(m_emon,
+                                                           fname,data_type);                            
+          }
+          
+       
+               
+      };
       
       
 
