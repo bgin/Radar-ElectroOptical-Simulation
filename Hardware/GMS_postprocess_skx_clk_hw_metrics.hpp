713ca25bb4770a6e0a63e810b8bee5e243c8d400
Bernard Gingold <beniekg@gmail.com>
2023-08-17 08:27:42 +0200

    Added SKX_memops_per_inst_t.

--- Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
+++ Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
@@ -915,7 +915,7 @@ namespace gms {
          Stores per instruction.
       */
       
-        template<int32_t len, int32_t lagh>
+         template<int32_t len, int32_t lagh>
          struct   SKX_stores_per_inst_t __ATTR_ALIGN__(64) {
         
                 
@@ -1024,6 +1024,134 @@ namespace gms {
                                        const char * __restrict data_type) {
                                        
                     cpu_perf_time_series_autocor<len,lagh>(m_spi,
+                                                           fname,data_type);                            
+          }
+          
+       
+               
+      };
+      
+      //////////////////////////////////////////////////////////////////////////
+      
+      /*
+        Memory operations per instruction.
+     */
+     
+        template<int32_t len, int32_t lagh>
+         struct   SKX_memops_per_inst_t __ATTR_ALIGN__(64) {
+        
+         
+                 __ATTR_ALIGN__(8) double * __restrict m_mem_inst_retired_all_loads;       
+                 __ATTR_ALIGN__(8) double * __restrict m_mem_inst_retired_all_stores;
+                 __ATTR_ALIGN__(8) double * __restrict m_inst_retired_any;
+                 __ATTR_ALIGN__(8) double * __restrict m_mpi;
+                            
+                 
+                 SKX_memops_per_inst_t() noexcept(true) {
+                      
+                      m_mem_inst_retired_all_loads  = nullptr;
+                      m_mem_inst_retired_all_stores = nullptr;
+                      m_inst_retired_any            = nullptr;
+                      m_mpi                         = nullptr;
+               }
+               
+                 SKX_memops_per_inst_t() noexcept(false) {
+                      
+                      using namespace gms::common;
+                      const std::size_t samp_len = (std::size_t)len;
+                      const std::size_t align    = (std::size_t)64;
+                      m_mem_inst_retired_all_loads  = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+                      m_mem_inst_retired_all_stores = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+                      m_inst_retired_any            = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+                      m_mpi                         = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+               }
+               
+                 SKX_memops_per_inst_t(const  SKX_memops_per_inst_t &) = delete;
+               
+                 SKX_memops_per_inst_t(SKX_memops_per_inst_t &&) = delete
+               
+                 ~SKX_memops_per_inst_t() {
+                     
+                     using namespace gms::common;
+                     if(__builtin_expect(nullptr!=
+                        m_mem_inst_retired_all_loads,0))   gms_mm_free(m_mem_inst_retired_all_loads);
+                     if(__builtin_expect(nullptr!=
+                        m_mem_inst_retired_all_stores,0))   gms_mm_free(m_mem_inst_retired_all_stores);
+                     if(__builtin_expect(nullptr!=
+                        m_inst_retired_any,0))              gms_mm_free(m_inst_retired_any);
+                     if(__builtin_expect(nullptr!=
+                        m_mpi,0))                           gms_mm_free(m_mpi);
+               }     
+               
+               
+                 SKX_memops_per_inst_t &
+                 operator=(const SKX_memops_per_inst_t &) = delete;
+               
+                 SKX_memops_per_inst_t &
+                 operator=(SKX_memops_per_inst_t &&) = delete;  
+               
+                 void compute_metric() {
+                    
+                    skx_mem_ops_per_instr_samples(     m_mem_inst_retired_all_loads,
+                                                       m_mem_inst_retired_all_stores,
+                                                       m_inst_retired_any,
+                                                       m_mpi,len);
+                                             
+               }     
+               
+                 void analyze_metric_canarm(const char * __restrict fname,
+                                          const char * __restrict data_type,
+                                          const bool use_omp) {
+                   
+                    cpu_perf_time_series_canarm<len,lagh>(m_mpi,fname,
+                                                          data_type,use_omp);
+                                                                            
+             }  
+             
+                 void analyze_metric_unimar(const char * __restrict fname,
+                                        const char * __restrict data_type) {
+                 
+                    cpu_perf_time_series_unimar<len,lagh>(m_mpi,
+                                                          fname,data_type);                           
+            } 
+            
+                 void analyze_metric_unibar(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_unibar<len,lagh>(m_mpi,
+                                                          fname,data_type);                           
+           }    
+           
+                 void analyze_metric_exsar(const char * __restrict fname,
+                                     const char * __restrict data_type) {
+                                     
+                    cpu_perf_time_series_exsar<len,lagh>(m_mpi,
+                                                         fname,data_type);                       
+           }
+           
+                 void analyze_metric_bispec(const char * __restrict fname,
+                                      const char * __restrict data_type,
+                                      const bool use_omp) {
+                                      
+                    cpu_perf_time_series_bispec<len,lagh>(m_mpi,
+                                                          fname,data_type,use_omp);                          
+           }
+           
+                 void analyze_metric_thirmo(const char * __restrict fname,
+                                      const char * __restrict data_type) {
+                                      
+                    cpu_perf_time_series_thirmo<len,lagh>(m_mpi,
+                                                          fname,data_type);                
+           }
+           
+                 void analyze_metric_autocor(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_autocor<len,lagh>(m_mpi,
                                                            fname,data_type);                            
           }
           
