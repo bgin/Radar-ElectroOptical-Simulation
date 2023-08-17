561d5ae694000b945375259293ab1e5aa983d0a1
Bernard Gingold <beniekg@gmail.com>
2023-08-17 07:59:40 +0200

    Added SKX_stores_per_inst_t.

--- Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
+++ Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
@@ -909,6 +909,129 @@ namespace gms {
                
       };
       
+      ///////////////////////////////////////////////////////////////////////////
+      
+      /*
+         Stores per instruction.
+      */
+      
+        template<int32_t len, int32_t lagh>
+         struct   SKX_stores_per_inst_t __ATTR_ALIGN__(64) {
+        
+                
+                 __ATTR_ALIGN__(8) double * __restrict m_mem_inst_retired_all_stores;
+                 __ATTR_ALIGN__(8) double * __restrict m_inst_retired_any;
+                 __ATTR_ALIGN__(8) double * __restrict m_spi;
+                            
+                 
+                 SKX_stores_per_inst_t() noexcept(true) {
+                      
+                  
+                      m_mem_inst_retired_all_stores = nullptr;
+                      m_inst_retired_any            = nullptr;
+                      m_spi                         = nullptr;
+               }
+               
+                 SKX_stores_per_inst_t() noexcept(false) {
+                      
+                      using namespace gms::common;
+                      const std::size_t samp_len = (std::size_t)len;
+                      const std::size_t align    = (std::size_t)64;
+                      m_mem_inst_retired_all_stores = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+                      m_inst_retired_any           = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+                      m_spi                        = (double*)gms_mm_malloc(samp_len,
+                                                                              align);
+               }
+               
+                 SKX_stores_per_inst_t(const  SKX_loads_per_inst_t &) = delete;
+               
+                 SKX_stores_per_inst_t(SKX_loads_per_inst_t &&) = delete
+               
+                 ~SKX_stores_per_inst_t() {
+                     
+                     using namespace gms::common;
+                     if(__builtin_expect(nullptr!=
+                        m_mem_inst_retired_all_stores,0))   gms_mm_free(m_mem_inst_retired_all_stores);
+                     if(__builtin_expect(nullptr!=
+                        m_inst_retired_any,0))              gms_mm_free(m_inst_retired_any);
+                     if(__builtin_expect(nullptr!=
+                        m_spi,0))                           gms_mm_free(m_spi);
+               }     
+               
+               
+                 SKX_stores_per_inst_t &
+                 operator=(const SKX_stores_per_inst_t &) = delete;
+               
+                 SKX_stores_per_inst_t &
+                 operator=(SKX_stores_per_inst_t &&) = delete;  
+               
+                 void compute_metric() {
+                    
+                    skx_stores_per_instr_samples(     m_mem_inst_retired_all_stores,
+                                                      m_inst_retired_any,
+                                                      m_spi,len);
+                                             
+               }     
+               
+                 void analyze_metric_canarm(const char * __restrict fname,
+                                          const char * __restrict data_type,
+                                          const bool use_omp) {
+                   
+                    cpu_perf_time_series_canarm<len,lagh>(m_spi,fname,
+                                                          data_type,use_omp);
+                                                                            
+             }  
+             
+                 void analyze_metric_unimar(const char * __restrict fname,
+                                        const char * __restrict data_type) {
+                 
+                    cpu_perf_time_series_unimar<len,lagh>(m_spi,
+                                                          fname,data_type);                           
+            } 
+            
+                 void analyze_metric_unibar(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_unibar<len,lagh>(m_spi,
+                                                          fname,data_type);                           
+           }    
+           
+                 void analyze_metric_exsar(const char * __restrict fname,
+                                     const char * __restrict data_type) {
+                                     
+                    cpu_perf_time_series_exsar<len,lagh>(m_spi,
+                                                         fname,data_type);                       
+           }
+           
+                 void analyze_metric_bispec(const char * __restrict fname,
+                                      const char * __restrict data_type,
+                                      const bool use_omp) {
+                                      
+                    cpu_perf_time_series_bispec<len,lagh>(m_spi,
+                                                          fname,data_type,use_omp);                          
+           }
+           
+                 void analyze_metric_thirmo(const char * __restrict fname,
+                                      const char * __restrict data_type) {
+                                      
+                    cpu_perf_time_series_thirmo<len,lagh>(m_spi,
+                                                          fname,data_type);                
+           }
+           
+                 void analyze_metric_autocor(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_autocor<len,lagh>(m_spi,
+                                                           fname,data_type);                            
+          }
+          
+       
+               
+      };
+      
+      
       
       
 
