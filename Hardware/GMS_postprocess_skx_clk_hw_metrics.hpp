8d07f0bbd9c197e7f72321eb2decf397de80932c
Bernard Gingold <beniekg@gmail.com>
2023-08-19 08:59:22 +0200

    Added SKX_L1D_hits_per_inst_t.

--- Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
+++ Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
@@ -1294,12 +1294,9 @@ namespace gms {
                  ~SKX_uncached_reads_per_inst_t() {
                      
                      using namespace gms::common;
-                     if(__builtin_expect(nullptr!=
-                        m_unc_cha_tor_inserts_ia_miss_filter1_0x40e33,0))   gms_mm_free(m_unc_cha_tor_inserts_ia_miss_filter1_0x40e33);
-                     if(__builtin_expect(nullptr!=
-                        m_inst_retired_any,0))              gms_mm_free(m_inst_retired_any);
-                     if(__builtin_expect(nullptr!=
-                        m_unc,0))                           gms_mm_free(m_unc);
+                     gms_mm_free(m_unc_cha_tor_inserts_ia_miss_filter1_0x40e33);
+                     gms_mm_free(m_inst_retired_any);
+                     gms_mm_free(m_unc);
                }     
                
                
@@ -1416,12 +1413,9 @@ namespace gms {
                  ~SKX_stream_stores_per_inst_t() {
                      
                      using namespace gms::common;
-                     if(__builtin_expect(nullptr!=
-                        m_unc_cha_tor_inserts_ia_miss_filter1_0x41833,0))   gms_mm_free(m_unc_cha_tor_inserts_ia_miss_filter1_0x41833);
-                     if(__builtin_expect(nullptr!=
-                        m_inst_retired_any,0))              gms_mm_free(m_inst_retired_any);
-                     if(__builtin_expect(nullptr!=
-                        m_sst,0))                           gms_mm_free(m_sst);
+                     gms_mm_free(m_unc_cha_tor_inserts_ia_miss_filter1_0x41833);
+                     gms_mm_free(m_inst_retired_any);
+                     gms_mm_free(m_sst);
                }     
                
                
@@ -1612,9 +1606,127 @@ namespace gms {
                                                            fname,data_type);                            
           }
           
-       
                
       };
+      
+      ////////////////////////////////////////////////////////////////////////////
+      
+      /*
+        L1D$ Demand data read hit per instruction.
+      */
+      
+        template<int32_t len, int32_t lagh>
+         struct   SKX_L1D_hits_per_inst_t __ATTR_ALIGN__(64) {
+        
+                
+                 __ATTR_ALIGN__(8) double * __restrict m_mem_load_retired_l1_hit;
+                 __ATTR_ALIGN__(8) double * __restrict m_inst_retired_any;
+                 __ATTR_ALIGN__(8) double * __restrict m_hits;
+                            
+                 
+                 SKX_L1D_hits_per_inst_t() noexcept(true) {
+                      
+                  
+                      m_mem_load_retired_l1_hit   = nullptr;
+                      m_inst_retired_any          = nullptr;
+                      m_hits                      = nullptr;
+               }
+               
+                 SKX_L1D_hits_per_inst_t() noexcept(false) {
+                      
+                      using namespace gms::common;
+                      const std::size_t samp_len = (std::size_t)len;
+                      const std::size_t align    = (std::size_t)64;
+                      m_mem_load_retired_l1_hit  = (double*)gms_mm_malloc(samp_len,
+                                                                  align);
+                      m_inst_retired_any         = (double*)gms_mm_malloc(samp_len,
+                                                                  align);
+                      m_hits                     = (double*)gms_mm_malloc(samp_len,
+                                                                    align);
+               }
+               
+                 SKX_L1D_hits_per_inst_t(const  SKX_L1D_hits_per_inst_t &) = delete;
+               
+                 SKX_L1D_hits_per_inst_t(SKX_L1D_hits_per_inst_t &&) = delete
+               
+                 ~SKX_L1D_hits_per_inst_t() {
+                     
+                     using namespace gms::common;
+                     gms_mm_free(m_mem_load_retired_l1_hit);
+                     gms_mm_free(m_inst_retired_any);
+                     gms_mm_free(m_hits);
+               }     
+               
+               
+                 SKX_L1D_hits_per_inst_t &
+                 operator=(const SKX_L1D_hits_per_inst_t &) = delete;
+               
+                 SKX_L1D_hits_per_inst_t &
+                 operator=(SKX_L1D_hits_per_inst_t &&) = delete;  
+               
+                 void compute_metric() {
+                    
+                    skx_L1D_hits_instr_samples(             l1d_mem_load_retired_l1_hit,
+                                                             m_inst_retired_any,
+                                                             m_hits,len);
+                                             
+               }     
+               
+                 void analyze_metric_canarm(const char * __restrict fname,
+                                          const char * __restrict data_type,
+                                          const bool use_omp) {
+                   
+                    cpu_perf_time_series_canarm<len,lagh>(m_hits,fname,
+                                                          data_type,use_omp);
+                                                                            
+             }  
+             
+                 void analyze_metric_unimar(const char * __restrict fname,
+                                        const char * __restrict data_type) {
+                 
+                    cpu_perf_time_series_unimar<len,lagh>(m_hits,
+                                                          fname,data_type);                           
+            } 
+            
+                 void analyze_metric_unibar(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_unibar<len,lagh>(m_hits,
+                                                          fname,data_type);                           
+           }    
+           
+                 void analyze_metric_exsar(const char * __restrict fname,
+                                     const char * __restrict data_type) {
+                                     
+                    cpu_perf_time_series_exsar<len,lagh>(m_hits,
+                                                         fname,data_type);                       
+           }
+           
+                 void analyze_metric_bispec(const char * __restrict fname,
+                                      const char * __restrict data_type,
+                                      const bool use_omp) {
+                                      
+                    cpu_perf_time_series_bispec<len,lagh>(m_hits,
+                                                          fname,data_type,use_omp);                          
+           }
+           
+                 void analyze_metric_thirmo(const char * __restrict fname,
+                                      const char * __restrict data_type) {
+                                      
+                    cpu_perf_time_series_thirmo<len,lagh>(m_hits,
+                                                          fname,data_type);                
+           }
+           
+                 void analyze_metric_autocor(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_autocor<len,lagh>(m_hits,
+                                                           fname,data_type);                            
+          }
+          
+               
+      };
+      
       
       
       
