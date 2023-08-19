09c02e222615288f2fe6f6999fd1ada7f04998a0
Bernard Gingold <beniekg@gmail.com>
2023-08-19 11:59:31 +0200

    Added SKX_L2_all_misses_inst_t.

--- Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
+++ Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
@@ -1965,7 +1965,125 @@ namespace gms {
       
       //////////////////////////////////////////////////////////////////////////////
       
-      
+      /*
+       L2 Misses per instruction including:
+       -- code
+       -- data
+       -- read for ownership and prefetches
+     */
+     
+         template<int32_t len, int32_t lagh>
+         struct   SKX_L2_all_misses_inst_t __ATTR_ALIGN__(64) {
+        
+                
+                 __ATTR_ALIGN__(8) double * __restrict m_l2_lines_in_all;
+                 __ATTR_ALIGN__(8) double * __restrict m_inst_retired_any;
+                 __ATTR_ALIGN__(8) double * __restrict m_allm;
+                            
+                 
+                 SKX_L2_all_misses_inst_t() noexcept(true) {
+                      
+                  
+                      m_l2_lines_in_all   = nullptr;
+                      m_inst_retired_any          = nullptr;
+                      m_allm                     = nullptr;
+               }
+               
+                 SKX_L2_all_misses_inst_t() noexcept(false) {
+                      
+                      using namespace gms::common;
+                      const std::size_t samp_len = (std::size_t)len;
+                      const std::size_t align    = (std::size_t)64;
+                      m_l2_lines_in_all   = (double*)gms_mm_malloc(samp_len,
+                                                                  align);
+                      m_inst_retired_any  = (double*)gms_mm_malloc(samp_len,
+                                                                  align);
+                      m_allm              = (double*)gms_mm_malloc(samp_len,
+                                                                    align);
+               }
+               
+                 SKX_L2_all_misses_inst_t(const  SKX_L2_all_misses_inst_t &) = delete;
+               
+                 SKX_L2_all_misses_inst_t(SKX_L2_all_misses_inst_t &&) = delete
+               
+                 ~SKX_L2_all_misses_inst_t() {
+                     
+                     using namespace gms::common;
+                     gms_mm_free(m_l2_lines_all_in);
+                     gms_mm_free(m_inst_retired_any);
+                     gms_mm_free(m_allm);
+               }     
+               
+               
+                 SKX_L2_all_misses_inst_t &
+                 operator=(const SKX_L2_all_misses_inst_t &) = delete;
+               
+                 SKX_L2_all_misses_inst_t &
+                 operator=(SKX_L2_all_misses_inst_t &&) = delete;  
+               
+                 void compute_metric() {
+                    
+                   skx_L2_all_misses_instr_samples(          m_l2_lines_in_all,
+                                                             m_inst_retired_any,
+                                                             m_allm,len);
+                                             
+               }     
+               
+                 void analyze_metric_canarm(const char * __restrict fname,
+                                          const char * __restrict data_type,
+                                          const bool use_omp) {
+                   
+                    cpu_perf_time_series_canarm<len,lagh>(m_allm,fname,
+                                                          data_type,use_omp);
+                                                                            
+             }  
+             
+                 void analyze_metric_unimar(const char * __restrict fname,
+                                        const char * __restrict data_type) {
+                 
+                    cpu_perf_time_series_unimar<len,lagh>(m_allm,
+                                                          fname,data_type);                           
+            } 
+            
+                 void analyze_metric_unibar(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_unibar<len,lagh>(m_allm,
+                                                          fname,data_type);                           
+           }    
+           
+                 void analyze_metric_exsar(const char * __restrict fname,
+                                     const char * __restrict data_type) {
+                                     
+                    cpu_perf_time_series_exsar<len,lagh>(m_allm,
+                                                         fname,data_type);                       
+           }
+           
+                 void analyze_metric_bispec(const char * __restrict fname,
+                                      const char * __restrict data_type,
+                                      const bool use_omp) {
+                                      
+                    cpu_perf_time_series_bispec<len,lagh>(m_allm,
+                                                          fname,data_type,use_omp);                          
+           }
+           
+                 void analyze_metric_thirmo(const char * __restrict fname,
+                                      const char * __restrict data_type) {
+                                      
+                    cpu_perf_time_series_thirmo<len,lagh>(m_allm,
+                                                          fname,data_type);                
+           }
+           
+                 void analyze_metric_autocor(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_autocor<len,lagh>(m_allm,
+                                                           fname,data_type);                            
+          }
+          
+               
+      };
+       
       
       
 
