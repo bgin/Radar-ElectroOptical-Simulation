d079386400e1ffb8cc24d95c48a45c96e52d5bc3
Bernard Gingold <beniekg@gmail.com>
2023-08-19 11:05:43 +0200

    Added SKX_L2_data_read_hits_inst_t.

--- Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
+++ Hardware/GMS_postprocess_skx_clk_hw_metrics.hpp
@@ -1733,7 +1733,7 @@ namespace gms {
         L1$ code read misses (including prefetches) per instruction.
        */
       
-        template<int32_t len, int32_t lagh>
+         template<int32_t len, int32_t lagh>
          struct   SKX_L1I_read_misses_inst_t __ATTR_ALIGN__(64) {
         
                 
@@ -1844,6 +1844,126 @@ namespace gms {
           
                
       };
+      
+      ///////////////////////////////////////////////////////////////////////////
+      
+      /*
+        L2 Demand data read hits per instruction.
+      */
+      
+         template<int32_t len, int32_t lagh>
+         struct   SKX_L2_data_read_hits_inst_t __ATTR_ALIGN__(64) {
+        
+                
+                 __ATTR_ALIGN__(8) double * __restrict m_mem_load_retired_l2_hit;
+                 __ATTR_ALIGN__(8) double * __restrict m_inst_retired_any;
+                 __ATTR_ALIGN__(8) double * __restrict m_dhits;
+                            
+                 
+                 SKX_L2_data_read_hits_inst_t() noexcept(true) {
+                      
+                  
+                      m_mem_load_retired_l2_hit   = nullptr;
+                      m_inst_retired_any          = nullptr;
+                      m_dhits                     = nullptr;
+               }
+               
+                 SKX_L2_data_read_hits_inst_t() noexcept(false) {
+                      
+                      using namespace gms::common;
+                      const std::size_t samp_len = (std::size_t)len;
+                      const std::size_t align    = (std::size_t)64;
+                      m_mem_load_retired_l2_hit  = (double*)gms_mm_malloc(samp_len,
+                                                                  align);
+                      m_inst_retired_any         = (double*)gms_mm_malloc(samp_len,
+                                                                  align);
+                      m_dhits                    = (double*)gms_mm_malloc(samp_len,
+                                                                    align);
+               }
+               
+                 SKX_L2_data_read_hits_inst_t(const  SKX_L2_data_read_hits_inst_t &) = delete;
+               
+                 SKX_L2_data_read_hits_inst_t(SKX_L2_data_read_hits_inst_t &&) = delete
+               
+                 ~SKX_L2_data_read_hits_inst_t() {
+                     
+                     using namespace gms::common;
+                     gms_mm_free(m_mem_load_retired_l2_hit);
+                     gms_mm_free(m_inst_retired_any);
+                     gms_mm_free(m_dhits);
+               }     
+               
+               
+                 SKX_L2_data_read_hits_inst_t &
+                 operator=(const SKX_L2_data_read_hits_inst_t &) = delete;
+               
+                 SKX_L2_data_read_hits_inst_t &
+                 operator=(SKX_L2_data_read_hits_inst_t &&) = delete;  
+               
+                 void compute_metric() {
+                    
+                    skx_L2_data_read_hits_instr_samples(     m_mem_load_retired_l2_hits,
+                                                             m_inst_retired_any,
+                                                             m_dhits,len);
+                                             
+               }     
+               
+                 void analyze_metric_canarm(const char * __restrict fname,
+                                          const char * __restrict data_type,
+                                          const bool use_omp) {
+                   
+                    cpu_perf_time_series_canarm<len,lagh>(m_dhits,fname,
+                                                          data_type,use_omp);
+                                                                            
+             }  
+             
+                 void analyze_metric_unimar(const char * __restrict fname,
+                                        const char * __restrict data_type) {
+                 
+                    cpu_perf_time_series_unimar<len,lagh>(m_dhits,
+                                                          fname,data_type);                           
+            } 
+            
+                 void analyze_metric_unibar(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_unibar<len,lagh>(m_dhits,
+                                                          fname,data_type);                           
+           }    
+           
+                 void analyze_metric_exsar(const char * __restrict fname,
+                                     const char * __restrict data_type) {
+                                     
+                    cpu_perf_time_series_exsar<len,lagh>(m_dhits,
+                                                         fname,data_type);                       
+           }
+           
+                 void analyze_metric_bispec(const char * __restrict fname,
+                                      const char * __restrict data_type,
+                                      const bool use_omp) {
+                                      
+                    cpu_perf_time_series_bispec<len,lagh>(m_dhits,
+                                                          fname,data_type,use_omp);                          
+           }
+           
+                 void analyze_metric_thirmo(const char * __restrict fname,
+                                      const char * __restrict data_type) {
+                                      
+                    cpu_perf_time_series_thirmo<len,lagh>(m_dhits,
+                                                          fname,data_type);                
+           }
+           
+                 void analyze_metric_autocor(const char * __restrict fname,
+                                       const char * __restrict data_type) {
+                                       
+                    cpu_perf_time_series_autocor<len,lagh>(m_dhits,
+                                                           fname,data_type);                            
+          }
+          
+               
+      };
+      
+      //////////////////////////////////////////////////////////////////////////////
       
       
       
