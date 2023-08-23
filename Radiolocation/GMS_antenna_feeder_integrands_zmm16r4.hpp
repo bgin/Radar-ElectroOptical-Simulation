020c375c102827825157d186ff56b20476e1d05f
Bernard Gingold <beniekg@gmail.com>
2023-08-23 15:32:38 +0200

    Splitting integrands to different file.

---
+++ Radiolocation/GMS_antenna_feeder_integrands_zmm16r4.hpp
@@ -0,0 +1,49 @@
+
+
+#ifndef __GMS_ANTENNA_FEEDER_INTEGRANDS_ZMM16R4_HPP__
+#define __GMS_ANTENNA_FEEDER_INTEGRANDS_ZMM16R4_HPP__
+
+
+/*MIT License
+Copyright (c) 2020 Bernard Gingold
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+namespace file_version {
+
+    const unsigned int GMS_ANTENNA_FEEDER_INTEGRANDS_ZMM16R4_MAJOR = 1U;
+    const unsigned int GMS_ANTENNA_FEEDER_INTEGRANDS_ZMM16R4_MINOR = 0U;
+    const unsigned int GMS_ANTENNA_FEEDER_INTEGRANDS_ZMM16R4_MICRO = 0U;
+    const unsigned int GMS_ANTENNA_FEEDER_INTEGRANDS_ZMM16R4_FULLVER =
+      1000U*GMS_ANTENNA_FEEDER_INTEGRANDS_ZMM16R4_MAJOR+
+      100U*GMS_ANTENNA_FEEDER_INTEGRANDS_ZMM16R4_MINOR+
+      10U*GMS_ANTENNA_FEEDER_INTEGRANDS_ZMM16R4_MICRO;
+    const char * const GMS_ANTENNA_FEEDER_INTEGRANDS_ZMM16R4_CREATION_DATE = "23-08-2023 15:31 PM +00200 (WED 23 AUG 2023 GMT+2)";
+    const char * const GMS_ANTENNA_FEEDER_INTEGRANDS_ZMM16R4_BUILD_DATE    = __DATE__ ":" __TIME__;
+    const char * const GMS_ANTENNA_FEEDER_INTEGRANDS_ZMM16R4_AUTHOR        = "Programmer: Bernard Gingold, contact: beniekg@gmail.com";
+    const char * const GMS_ANTENNA_FEEDER_INTEGRANDS_ZMM16R4_DESCRIPTION   = "AVX512 (single) optimized antenna and feeder integrands.";
+
+}
+
+
+
+
+
+
+
+#endif /*__GMS_ANTENNA_FEEDER_INTEGRANDS_ZMM16R4*/
