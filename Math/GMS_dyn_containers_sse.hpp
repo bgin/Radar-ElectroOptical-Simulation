867d52051232428fb48a15ec8e34083c1f68a5dd
Bernard Gingold <beniekg@gmail.com>
2024-01-04 09:42:54 +0100

    Added: an explicit specifier.

--- Math/GMS_dyn_containers_sse.hpp
+++ Math/GMS_dyn_containers_sse.hpp
@@ -395,7 +395,7 @@ namespace gms {
                          
                     } 
                           
-                     inline DC1D_xmm2c8_t(const std::size_t _mnx) {
+                     inline explicit DC1D_xmm2c8_t(const std::size_t _mnx) {
                                  
                           using namespace gms::common;
                           this->mnx = _mnx;
@@ -865,7 +865,7 @@ namespace gms {
                          
                     } 
                           
-                     inline DC1D_xmm4c4_t(const std::size_t _mnx) {
+                     inline explicit DC1D_xmm4c4_t(const std::size_t _mnx) {
                                  
                           using namespace gms::common;
                           this->mnx = _mnx;
@@ -990,7 +990,7 @@ namespace gms {
                          
                     } 
                           
-                     inline DC1D_m128_t(const std::size_t _mnx) {
+                     inline explicit DC1D_m128_t(const std::size_t _mnx) {
                                  
                           using namespace gms::common;
                           this->mnx = _mnx;
@@ -1116,7 +1116,7 @@ namespace gms {
                          
                     } 
                           
-                     inline DC1D_m128d_t(const std::size_t _mnx) {
+                     inline explicit DC1D_m128d_t(const std::size_t _mnx) {
                                  
                           using namespace gms::common;
                           this->mnx = _mnx;
