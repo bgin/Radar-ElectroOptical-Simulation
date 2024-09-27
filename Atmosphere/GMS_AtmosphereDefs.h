0efc5e717084d433bcb75c679b201b423fbe62a5
Bernard Gingold <beniekg@gmail.com>
2024-09-27 20:32:55 +0200

    Atmo76.

---
+++ Atmosphere/GMS_AtmosphereDefs.h
@@ -0,0 +1,22 @@
+
+#ifndef __GMS_ATMOSPHERE_DEFS_H__
+#define __GMS_ATMOSPHERE_DEFS_H___
+
+
+
+
+
+
+
+#ifndef USE_OPENMP
+#define USE_OPENMP 0x1
+#include <omp.h>
+#endif
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <limits>
+
+
+#endif /*__GMS_ATMOSPHERE_DEFS_H__*/
