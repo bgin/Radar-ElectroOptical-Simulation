83b673ad21e6e7be04489309046eb95c3783cfc3
Bernard Gingold <beniekg@gmail.com>
2025-03-18 18:41:11 +0100

    Slightly refactored.

--- Statistics/GMS_mkl_weibullrng.cpp
+++ Statistics/GMS_mkl_weibullrng.cpp
@@ -1,14 +1,15 @@
 
 #include "GMS_mkl_weibullrng.h"
+#include "GMS_common.h"
 #include "GMS_malloc.h"
-#include "GMS_error_macros.h"
-#include "GMS_constants.h"
-#endif
+#include "GMS_simd_memops.h" 
+
+
 //
 //	Implementation
 //
 
-gms::math::stat::
+gms::math::
 MKLWeibullRNG::
 MKLWeibullRNG() {
 	using namespace gms::math::constants;
@@ -24,7 +25,7 @@ MKLWeibullRNG() {
 
 
 
-gms::math::stat::
+gms::math::
 MKLWeibullRNG::
 MKLWeibullRNG(const MKL_INT nvalues,
 	      const MKL_UINT brng,
@@ -32,10 +33,12 @@ MKLWeibullRNG(const MKL_INT nvalues,
 	      const double alpha,
 	      const double a,
 	      const double beta) {
-	      using namespace gms::common;
+	using namespace gms::common;
 
-              data.m_rvec = (double*)gma_mm_malloc(static_cast<size_t>(nvalues), align64B);
-   
+        constexpr size_t align64b = 64ULL;
+        data.m_rvec = gms_mm_malloc(static_cast<size_t>(nvalues),align64b ); // <-- Upon touching m_rvec load 64byte(whole object) to cache
+    
+
 	data.m_alpha   = alpha;
 	data.m_a       = a;
 	data.m_beta    = beta;
@@ -43,12 +46,12 @@ MKLWeibullRNG(const MKL_INT nvalues,
 	data.m_brng    = brng;
 	data.m_seed    = seed;
 	data.m_error   = 0;
-#if (GMS_INIT_ARRAYS) == 1
 #if defined __AVX512F__
         avx512_init_unroll8x_pd(&data.m_rvec[0], static_cast<int64_t>(data.m_nvalues), 0.0);
 #elif defined __AVX__
 	avx256_init_unroll8x_pd(&data.m_rvec[0], static_cast<int64_t>(data.m_nvalues), 0.0); // m_rvec should be in cache
-#endif
+#else
+#error Unsupported SIMD ISA
 #endif
 }
 	
@@ -57,12 +60,13 @@ MKLWeibullRNG(const MKL_INT nvalues,
 
 
 
-gms::math::stat::
+gms::math::
 MKLWeibullRNG::
 MKLWeibullRNG(const MKLWeibullRNG &x) {
 	using namespace gms::common;
-
-        data.m_rvec = (double*)gms_mm_malloc(static_cast<size_t>(x.m_nvalues), align64B);
+        
+        constexpr size_t align64b = 64ULL;
+        data.m_rvec = gms_mm_malloc(static_cast<size_t>(x.m_nvalues), align64b );
    
 	data.m_alpha   = x.data.m_alpha;
 	data.m_a       = x.data.m_a;
@@ -73,16 +77,18 @@ MKLWeibullRNG(const MKLWeibullRNG &x) {
 	data.m_error   = x.data.m_error;
 #if defined __AVX512F__
      #if (USE_NT_STORES) == 1
-	   avx512_memcpy8x_nt_pd(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(data.m_nvalues));
+	 avx512_uncached_memmove(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(data.m_nvalues));
      #else
-	   avx512_memcpy8x_pd(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(data.m_nvalues));
+	 avx512_cached_memmove(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(data.m_nvalues));
+     #endif
+#elif defined __AVX__
+     #if (USE_NT_STORES) == 1
+	   avx256_uncached_memmove(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(data.m_nvalues));
+     #else
+	   avx256_cached_memmove(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(data.m_nvalues));
      #endif
 #else
-     #if (USE_NT_STORES) == 1
-	   avx256_memcpy8x_nt_pd(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(data.m_nvalues));
-     #else
-	   avx256_memcpy8x_pd(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(data.m_nvalues));
-     #endif
+#error Unsupported SIMD ISA
 #endif
 }	
 	
@@ -90,7 +96,7 @@ MKLWeibullRNG(const MKLWeibullRNG &x) {
 
 
 
-gms::math::stat::
+gms::math::
 MKLWeibullRNG::
 MKLWeibullRNG(MKLWeibullRNG &&x){
 	
@@ -110,31 +116,29 @@ MKLWeibullRNG(MKLWeibullRNG &&x){
 
 
 
-gms::math::stat::
+gms::math::
 MKLWeibullRNG::
 ~MKLWeibullRNG() {
-
-        if (NULL != data.m_rvec) gms_mm_free(data.m_rvec); data.m_rvec = NULL;
-#endif
+       if (NULL != data.m_rvec) gms_mm_free(data.m_rvec); data.m_rvec = NULL;
 }		
 		
 	
 
 
-gms::math::stat::MKLWeibullRNG &
-gms::math::stat::MKLWeibullRNG::
+gms::math::MKLWeibullRNG &
+gms::math::MKLWeibullRNG::
 operator=(const MKLWeibullRNG &x){
     using namespace gms::common;
 	if (this == &x) return (*this);
 	if (data.m_nvalues != x.data.m_nvalues) { // Handle size mismatch
-           gms_mm_free(data.m_rvec);
-  	   data.m_alpha = 0.0; data.m_a = 0.0;
-	   data.m_beta = 0.0;  data.m_nvalues = 0;
-	   data.m_brng = 0;    data.m_seed = 0;
-	   data.m_error = 1;
-
-           data.m_rvec = gms_mm_malloc(static_cast<size_t>(x.data.m_nvalues), align64B);
-    
+	     constexpr size_t align64b = 64ULL;
+             gms_mm_free(data.m_rvec);
+	     data.m_alpha = 0.0; data.m_a = 0.0;
+	     data.m_beta = 0.0;  data.m_nvalues = 0;
+	     data.m_brng = 0;    data.m_seed = 0;
+	     data.m_error = 1;
+             data.m_rvec = gms_mm_malloc(static_cast<size_t>(x.data.m_nvalues), align64b);
+ 
 	}
 	else {
 		// Copy state
@@ -144,16 +148,18 @@ operator=(const MKLWeibullRNG &x){
 		data.m_error = x.data.m_error;
 #if defined __AVX512F__
       #if (USE_NT_STORES) == 1
-		avx512_memcpy8x_nt_pd(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(x.data.m_nvalues));
+		avx512_uncached_memmove(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(x.data.m_nvalues));
       #else
-		avx512_memcpy8x_pd(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(x.data.m_nvalues));
+		avx512_cached_memove(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(x.data.m_nvalues));
+      #endif
+#elif defined __AVX__
+      #if (USE_NT_STORES) == 1
+		avx256_uncached_memmove(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(x.data.m_nvalues));
+      #else
+		avx256_cached_memove(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(x.data.m_nvalues));
       #endif
 #else
-      #if (USE_NT_STORES) == 1
-		avx256_memcpy8x_nt_pd(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(x.data.m_nvalues));
-      #else
-		avx256_memcpy8x_pd(&data.m_rvec[0], &x.data.m_rvec[0], static_cast<size_t>(x.data.m_nvalues));
-      #endif
+#error Unsupported SIMD ISA
 #endif
 	}
 	return (*this);
@@ -169,31 +175,30 @@ operator=(const MKLWeibullRNG &x){
 
 
 
-gms::math::stat::MKLWeibullRNG &
-gms::math::stat::MKLWeibullRNG::
+gms::math::MKLWeibullRNG &
+gms::math::MKLWeibullRNG::
 operator=(MKLWeibullRNG &&x) {
 	if (this == &x) return (*this);
 
-         gms_mm_free(data.m_rvec);
-
-	data.m_rvec      = &x.data.m_rvec[0];
-	data.m_alpha     = x.data.m_alpha;
-	data.m_a         = x.data.m_a;
-	data.m_beta      = x.data.m_beta;
-	data.m_nvalues   = x.data.m_nvalues;
-	data.m_brng      = x.data.m_brng;
-	data.m_seed      = x.data.m_seed;
-	data.m_error     = x.data.m_error;
-	x.data.m_nvalues = 0;
-	x.data.m_rvec    = NULL;
-	return (*this);
+          gms_mm_free(data.m_rvec);
+	  data.m_rvec      = &x.data.m_rvec[0];
+	  data.m_alpha     = x.data.m_alpha;
+	  data.m_a         = x.data.m_a;
+	  data.m_beta      = x.data.m_beta;
+	  data.m_nvalues   = x.data.m_nvalues;
+	  data.m_brng      = x.data.m_brng;
+	  data.m_seed      = x.data.m_seed;
+	  data.m_error     = x.data.m_error;
+	  x.data.m_nvalues = 0;
+	  x.data.m_rvec    = NULL;
+	  return (*this);
 }	
 	
 	
 
 
 void
-gms::math::stat::MKLWeibullRNG::
+gms::math::MKLWeibullRNG::
 compute_rand_distribution(const MKL_INT method) {
 	VSLStreamStatePtr stream;
 	data.m_error = vslNewStream(&stream,data.m_brng,data.m_seed);
@@ -217,7 +222,7 @@ compute_rand_distribution(const MKL_INT method) {
 }
 
 void
-gms::math::stat::MKLWeibullRNG::
+gms::math::MKLWeibullRNG::
 compute_rand_distribution(VSLStreamStatePtr stream, 
 		          const MKL_INT method) {
 	// Stream must be deallocated (deinitialized) by the caller of this procedure.
