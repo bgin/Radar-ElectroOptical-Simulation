1e1c9ef40c9f0d3804cfbf0be772b229c34c72d7
Bernard Gingold <beniekg@gmail.com>
2025-03-25 18:53:10 +0100

    Minor refactoring.

--- Statistics/GMS_mkl_laplacerng.cpp
+++ Statistics/GMS_mkl_laplacerng.cpp
@@ -1,15 +1,13 @@
 
 #include "GMS_mkl_laplacerng.h"
-#include "GMS_common.h"
 #include "GMS_malloc.h"
-#include "GMS_error_macros.h"
-#include "GMS_constants"
+#include "GMS_simd_memops.h"
 
 //
 //	Implementation
 //
 
-gms::math::stat::
+gms::math::
 MKLLaplaceRNG::
 MKLLaplaceRNG() {
 	using namespace gms::math::constants;
@@ -24,7 +22,7 @@ MKLLaplaceRNG() {
 
 
 
-gms::math::stat::
+gms::math::
 MKLLaplaceRNG::
 MKLLaplaceRNG(const MKL_INT nvalues,
 	      const MKL_UINT brng,
@@ -32,7 +30,7 @@ MKLLaplaceRNG(const MKL_INT nvalues,
 	      const double a,
               const double beta) {
 	using namespace gms::common;
-
+        constexpr size_t align64B = 64ULL;
 	m_rvec =  gms_mm_malloc(static_cast<size_t>(nvalues), align64B);
 
 	m_a    = a;
@@ -41,20 +39,19 @@ MKLLaplaceRNG(const MKL_INT nvalues,
 	m_brng    = brng;
 	m_seed    = seed;
 	m_error   = 1;
-#if (GMS_INIT_ARRAYS) == 1
 #if defined __AVX512F__
         avx512_init_unroll8x_pd(&m_rvec[0], static_cast<int64_t>(m_nvalues), 0.0);
 #else
 	avx256_init_unroll8x_pd(&m_rvec[0], static_cast<int64_t>(m_nvalues), 0.0);
 #endif
-#endif
 }
 
 
-gms::math::stat::
+gms::math::
 MKLLaplaceRNG::
 MKLLaplaceRNG(const MKLLaplaceRNG &x) {
 	using namespace gms::common;
+	constexpr size_t align64B = 64ULL;
 	m_rvec = lam_edmalloca(static_cast<size_t>(x.m_nvalues), align64B);
 	m_a    = x.m_a;
 	m_beta = x.m_beta;
@@ -64,21 +61,21 @@ MKLLaplaceRNG(const MKLLaplaceRNG &x) {
 	m_error   = x.m_error;
 #if defined __AVX512F__
        #if (USE_NT_STORES) == 1
-	    avx512_memcpy8x_nt_pd(&m_rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+	    avx512_uncached_memmove(&m_rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
        #else
-	    avx512_memcpy8x_pd(&m_rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+	    avx512_cached_memmove(&m_rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
        #endif
 #else
        #if (USE_NT_STORES) == 1
-	    avx256_memcpy8x_nt_pd(&m_rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+	    avx256_uncached_memmove(&m_rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
        #else
-	    avx256_memcpy8x_pd(&m_rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+	    avx256_cached_memmove(&m_rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
        #endif
 #endif
 }
 
 
-gms::math::stat::
+gms::math::
 MKLLaplaceRNG::
 MKLLaplaceRNG(MKLLaplaceRNG &&x) {
 	m_rvec = &x.m_rvec[0];
@@ -93,39 +90,41 @@ MKLLaplaceRNG(MKLLaplaceRNG &&x) {
 }
 
 
-gms::math::stat::
+gms::math::
 MKLLaplaceRNG::
 ~MKLLaplaceRNG() {
+        using namespace gms::common;
 	if (NULL != m_rvec) gms_mm_free(m_rvec);	m_rvec = NULL;
 }		
 
 
-gms::math::stat::MKLLaplaceRNG &
-gms::math::stat::MKLLaplaceRNG::
+gms::math::MKLLaplaceRNG &
+gms::math::MKLLaplaceRNG::
 operator=(const MKLLaplaceRNG &x) {
-	using namespace lam::common;
-	if (this == &x) return (*this);
-	_mm_free(m_rvec);
+	using namespace gms::common;
+       if (this == &x) return (*this);
+	gms_mm_free(m_rvec);
 	m_a = x.m_a;
 	m_beta = x.m_beta;
 	m_nvalues = x.m_nvalues;
 	m_brng = x.m_brng;
 	m_seed = x.m_seed;
 	m_error = x.m_error;
+	constexpr size_t align64B = 64ULL;
+        double * __restrict 
+	    rvec{ gms_mm_malloc(static_cast<size_t>(m_nvalues), align64B) };
 
-        double * __restrict rvec = gms_mm_malloc(static_cast<size_t>(m_nvalues), align64B);
-	   
 #if defined __AVX512F__
       #if (USE_NT_STORES) == 1
-	    avx512_memcpy8x_nt_pd(&rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+	    avx512_uncached_memmove(&rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
       #else
-	    avx512_memcpy8x_pd(&rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+	    avx512_cached_memmove(&rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
       #endif
 #else
       #if (USE_NT_STORES) == 1
-	    avx256_memcpy8x_nt_pd(&rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+	    avx256_uncached_memmove(&rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
       #else
-	    avx256_memcpy8x_pd(&rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
+	    avx256_cached_memove(&rvec[0], &x.m_rvec[0], static_cast<size_t>(m_nvalues));
       #endif
 #endif
 	m_rvec = &rvec[0];
@@ -134,11 +133,12 @@ operator=(const MKLLaplaceRNG &x) {
 	
 
 
-gms::math::stat::MKLLaplaceRNG &
-gms::math::stat::MKLLaplaceRNG::
+gms::math::MKLLaplaceRNG &
+gms::math::MKLLaplaceRNG::
 operator=(MKLLaplaceRNG &&x) {
+        using namespace gms::common;
 	if (this == &x) return(*this);
-	_mm_free(m_rvec);
+	gms_mm_free(m_rvec);
 	m_rvec = &x.m_rvec[0];
 	m_a = x.m_a;
 	m_beta = x.m_beta;
@@ -155,7 +155,7 @@ operator=(MKLLaplaceRNG &&x) {
 
 
 void
-gms::math::stat::MKLLaplaceRNG::
+gms::math::MKLLaplaceRNG::
 compute_rand_distribution(const MKL_INT method) {
 	VSLStreamStatePtr stream;
 	m_error = vslNewStream(&stream,m_brng,m_seed);
@@ -179,7 +179,7 @@ compute_rand_distribution(const MKL_INT method) {
 }
 
 void
-gms::math::stat::MKLLaplaceRNG::
+gms::math::MKLLaplaceRNG::
 compute_rand_distribution(VSLStreamStatePtr stream, 
 		          const MKL_INT method) {
 	// Stream must be deallocated (deinitialized) by the caller of this procedure.
@@ -198,7 +198,7 @@ compute_rand_distribution(VSLStreamStatePtr stream,
 }
 
 std::ostream &
-gms::math::stat::operator<<(std::ostream &os,
+gms::math::operator<<(std::ostream &os,
 			    const MKLLaplaceRNG &x) {
 
 	for (MKL_INT i = 0; i != x.m_nvalues; ++i) {
