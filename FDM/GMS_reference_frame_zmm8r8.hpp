8bad9c790f26daac4a910f1bbbbf97b117ebbd6a
Bernard Gingold <beniekg@gmail.com>
2025-04-18 14:06:29 +0200

    Removed: deep copy ctor.

--- FDM/GMS_reference_frame_zmm8r8.hpp
+++ FDM/GMS_reference_frame_zmm8r8.hpp
@@ -218,56 +218,7 @@ namespace gms {
                              }
                         }
 
-                        inline ReferenceFrame_zmm8r8_t(const std::size_t nx,
-                                                      const std::size_t ny,
-                                                      const std::size_t nz,
-                                                      const __m512d * __restrict FI_x,
-                                                      const __m512d * __restrict FI_y,
-                                                      const __m512d * __restrict FI_z,
-                                                      const __m512d * __restrict dFI_x,
-                                                      const __m512d * __restrict dFI_y,
-                                                      const __m512d * __restrict dFI_z,
-                                                      const __m512d * __restrict ddFI_x,
-                                                      const __m512d * __restrict ddFI_y,
-                                                      const __m512d * __restrict ddFI_z,
-                                                      const double orig_x,
-                                                      const double orig_y,
-                                                      const double orig_z,
-                                                      const double      dt) noexcept(false)
-                        {
-                             using namespace gms::common;
-                             this->mnx     = nx;
-                             this->mny     = ny;
-                             this->mnz     = nz;
-                             allocate();
-                             this->morig_x = orig_x;
-                             this->morig_y = orig_y;
-                             this->morig_z = orig_z;
-                             this->mdt     = dt;
-                             this->mismmap = false;
-#if (USE_GMS_REFERENCE_FRAME_ZMM8R8_NT_STORES) == 1
-                             avx512_uncached_memmove(&this->mFI_x[0], &FI_x[0], this->mnx);
-                             avx512_uncached_memmove(&this->mFI_y[0], &FI_y[0], this->mny);
-                             avx512_uncached_memmove(&this->mFI_z[0], &FI_z[0], this->mnz);
-                             avx512_uncached_memmove(&this->mdFI_x[0], &dFI_x[0], this->mnx);
-                             avx512_uncached_memmove(&this->mdFI_y[0], &dFI_y[0], this->mny);
-                             avx512_uncached_memmove(&this->mdFI_z[0], &dFI_z[0], this->mnz);
-                             avx512_uncached_memmove(&this->mddFI_x[0], &ddFI_x[0], this->mnx);
-                             avx512_uncached_memmove(&this->mddFI_y[0], &ddFI_y[0], this->mny);
-                             avx512_uncached_memmove(&this->mddFI_z[0], &ddFI_z[0], this->mnz);
-#else
-                             avx512_cached_memmove(&this->mFI_x[0], &FI_x[0], this->mnx);
-                             avx512_cached_memmove(&this->mFI_y[0], &FI_y[0], this->mny);
-                             avx512_cached_memmove(&this->mFI_z[0], &FI_z[0], this->mnz);
-                             avx512_cached_memmove(&this->mdFI_x[0], &dFI_x[0], this->mnx);
-                             avx512_cached_memmove(&this->mdFI_y[0], &dFI_y[0], this->mny);
-                             avx512_cached_memmove(&this->mdFI_z[0], &dFI_z[0], this->mnz);
-                             avx512_cached_memmove(&this->mddFI_x[0], &ddFI_x[0], this->mnx);
-                             avx512_cached_memmove(&this->mddFI_y[0], &ddFI_y[0], this->mny);
-                             avx512_cached_memmove(&this->mddFI_z[0], &ddFI_z[0], this->mnz);
-#endif
-                        }
-
+                       
                         inline ReferenceFrame_zmm8r8_t(ReferenceFrame_zmm8r8_t && rhs) noexcept(true)
                         {
                             this->mnx      = rhs.mnx;
