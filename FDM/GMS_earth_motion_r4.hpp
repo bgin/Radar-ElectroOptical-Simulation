2fbfaa75ac90b32e699fabf48a94ba20bb0c12ee
Bernard Gingold <beniekg@gmail.com>
2025-04-30 06:54:29 +0200

    Added: minor refactoring.

--- FDM/GMS_earth_motion_r4.hpp
+++ FDM/GMS_earth_motion_r4.hpp
@@ -39,9 +39,12 @@ namespace file_info {
 #include <cstdint>
 #include <immintrin.h>
 #include <cstdlib>
+#include <cassert>
 #include "GMS_config.h"
 #include "GMS_malloc.h"
-
+#if (USE_PMC_INSTRUMENTATION) == 1
+#include "GMS_hw_perf_macros.h"
+#endif
 
 // Enable non-temporal stores for this class only( used with free-standing operators)
 // defaulted to 0.
@@ -66,37 +69,63 @@ namespace gms {
                      float * __restrict mcosLon; // cosine of Longitude
                      float * __restrict msinLon; // sine of Longtitude
                      std::size_t        mnsec;   // number of seconds of Earth's rotation
+                     bool               mmalloc_type; // true for gms_mm_malloc, false for gms_tbb_malloc
+                     bool               mfree_type;   // true for gms_mm_free,   false for gms_tbb_free
 #if (USE_STRUCT_PADDING) == 1
-                      PAD_TO(0,16)
+                      PAD_TO(0,14)
 #endif
                      EarthMotion_r4_t() = delete;
 
-                     inline explicit EarthMotion_r4_t(const std::size_t nsec) noexcept(false)
+                     inline  EarthMotion_r4_t(const std::size_t nsec,
+                                              const bool        malloc_type,
+                                              const bool        free_type) noexcept(false)
                      {
+                          assert(nsec>0ULL);
                           this->mnsec = nsec;
+                          this->mmalloc_type = malloc_type;
+                          this->mfree_type   = free_type;
+#if (USE_PMC_INSTRUMENTATION) == 1
+                                   HW_PMC_COLLECTION_PROLOGE_BODY   
+#endif                           
                           allocate();
+#if (USE_PMC_INSTRUMENTATION) == 1
+                                   HW_PMC_COLLECTION_EPILOGE_BODY
+#endif
                      }
 
                      EarthMotion_r4_t(const EarthMotion_r4_t &) = delete;
 
                      inline EarthMotion_r4_t(EarthMotion_r4_t && rhs) noexcept(true)
                      {
-                          this->mnsec    = rhs.mnsec;
-                          this->mOE      = &rhs.mOE[0];
-                          this->mcosLat  = &rhs.mcosLat[0];
-                          this->msinLat  = &rhs.msinLat[0];
-                          this->mcosLon  = &rhs.mcosLon[0];
-                          this->msinLon  = &rhs.msinLon[0];
+                          this->mnsec        = rhs.mnsec;
+                          this->mmalloc_type = rhs.mmalloc_type;
+                          this->mfree_type   = rhs.mfree_type;
+                          this->mOE          = &rhs.mOE[0];
+                          this->mcosLat      = &rhs.mcosLat[0];
+                          this->msinLat      = &rhs.msinLat[0];
+                          this->mcosLon      = &rhs.mcosLon[0];
+                          this->msinLon      = &rhs.msinLon[0];
                      }
 
                      inline ~EarthMotion_r4_t() noexcept(true)
                      {
                          using namespace gms::common;
-                         gms_mm_free(this->msinLon); this->msinLon = NULL;
-                         gms_mm_free(this->mcosLon); this->mcosLon = NULL;
-                         gms_mm_free(this->msinLat); this->msinLat = NULL;
-                         gms_mm_free(this->mcosLat); this->mcosLat = NULL;
-                         gms_mm_free(this->mOE);     this->mOe     = NULL;
+                         if(this->mfree_type==true)
+                         {
+                             gms_mm_free(this->msinLon); this->msinLon = NULL;
+                             gms_mm_free(this->mcosLon); this->mcosLon = NULL;
+                             gms_mm_free(this->msinLat); this->msinLat = NULL;
+                             gms_mm_free(this->mcosLat); this->mcosLat = NULL;
+                             gms_mm_free(this->mOE);     this->mOe     = NULL;
+                         }
+                         else 
+                         {
+                             gms_tbb_free(this->msinLon); this->msinLon = NULL;
+                             gms_tbb_free(this->mcosLon); this->mcosLon = NULL;
+                             gms_tbb_free(this->msinLat); this->msinLat = NULL;
+                             gms_tbb_free(this->mcosLat); this->mcosLat = NULL;
+                             gms_tbb_free(this->mOE);     this->mOe     = NULL;
+                         }
                      }
 
                      EarthMotion_r4_t & operator=(const EarthMotion_r4_t &) = delete;
@@ -106,28 +135,43 @@ namespace gms {
                           using namespace gms::commom;
                           if(this==&rhs) return (*this)
                           gms_swap(this->mnsec,  rhs.mnsec);
+                          gms_swap(this->mmalloc_type, rhs.mmalloc_type);
+                          gms_swap(this->mfree_type,   rhs.mfree_type);
                           gms_swap(this->mOE,    rhs.mOE);
                           gms_swap(this->mcosLat,rhs.mcosLat);
                           gms_swap(this->msinLat,rhs.msinLat);
                           gms_swap(this->mcosLat,rhs.mcosLat);
                           return (*this);
                      }
+
+                     inline std::size_t size_nsecbytes() const noexcept(true)
+                     {
+                         return static_cast<std::size_t>(this->mnsec*sizeof(float));
+                     }
                      
                      inline void allocate() noexcept(false)
                      {
                           using namespace gms::common;
                           const std::size_t mnsecbytes{size_nsecbytes()};
-                          this->mOE{reinterpret_cast<float __restrict*>(gms_mm_malloc(mnsecbytes,64ULL))};
-                          this->mcosLat{reinterpret_cast<float __restrict*>(gms_mm_malloc(mnsecbytes,64ULL))};
-                          this->msinLat{reinterpret_cast<float __restrict*>(gms_mm_malloc(mnsecbytes,64ULL))};
-                          this->mcosLon{reinterpret_cast<float __restrict*>(gms_mm_malloc(mnsecbytes,64ULL))};
-                          this->msinLon{reinterpret_cast<float __restrict*>(gms_mm_malloc(mnsecbytes,64ULL))};
+                          if(this->mmalloc_type==true)
+                          {
+                              this->mOE{reinterpret_cast<float * __restrict>(gms_mm_malloc(mnsecbytes,64ULL))};
+                              this->mcosLat{reinterpret_cast<float * __restrict>(gms_mm_malloc(mnsecbytes,64ULL))};
+                              this->msinLat{reinterpret_cast<float * __restrict>(gms_mm_malloc(mnsecbytes,64ULL))};
+                              this->mcosLon{reinterpret_cast<float * __restrict>(gms_mm_malloc(mnsecbytes,64ULL))};
+                              this->msinLon{reinterpret_cast<float * __restrict>(gms_mm_malloc(mnsecbytes,64ULL))};
+                          }
+                          else 
+                          {
+                              this->mOE{reinterpret_cast<float * __restrict>(gms_tbb_malloc(mnsecbytes,64ULL))};
+                              this->mcosLat{reinterpret_cast<float * __restrict>(gms_tbb_malloc(mnsecbytes,64ULL))};
+                              this->msinLat{reinterpret_cast<float * __restrict>(gms_tbb_malloc(mnsecbytes,64ULL))};
+                              this->mcosLon{reinterpret_cast<float * __restrict>(gms_tbb_malloc(mnsecbytes,64ULL))};
+                              this->msinLon{reinterpret_cast<float * __restrict>(gms_tbb_malloc(mnsecbytes,64ULL))};
+                          }
                      }
                      
-                     inline std::size_t size_nsecbytes() const noexcept(true)
-                     {
-                         return static_cast<std::size_t>(this->mnsec*sizeof(float));
-                     }
+                     
                      
               };
     }
