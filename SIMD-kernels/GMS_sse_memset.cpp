0b059ceb6ec1de7b34b4a59d7bff6fa1c3acabaa
Bernard Gingold <beniekg@gmail.com>
2025-05-30 15:33:17 +0200

    Added: different SIMD implementation.

--- SIMD-kernels/GMS_sse_memset.cpp
+++ SIMD-kernels/GMS_sse_memset.cpp
@@ -27,14 +27,14 @@ sse_memset_unroll8x_ps(float * __restrict__ dst,const float filler,std::size_t s
     while(size >= 32ULL)
     {
          
-        _mm_store_ps(p_dst+0, vfiller);
-        _mm_store_ps(p_dst+4, vfiller);
-        _mm_store_ps(p_dst+8, vfiller);
-        _mm_store_ps(p_dst+12,vfiller);
-        _mm_store_ps(p_dst+16,vfiller);
-        _mm_store_ps(p_dst+20,vfiller);
-        _mm_store_ps(p_dst+24,vfiller);
-        _mm_store_ps(p_dst+28,vfiller);
+        _mm_storeu_ps(p_dst+0, vfiller);
+        _mm_storeu_ps(p_dst+4, vfiller);
+        _mm_storeu_ps(p_dst+8, vfiller);
+        _mm_storeu_ps(p_dst+12,vfiller);
+        _mm_storeu_ps(p_dst+16,vfiller);
+        _mm_storeu_ps(p_dst+20,vfiller);
+        _mm_storeu_ps(p_dst+24,vfiller);
+        _mm_storeu_ps(p_dst+28,vfiller);
          
         size  -= 32ULL;
         p_dst += 32ULL;
@@ -42,12 +42,12 @@ sse_memset_unroll8x_ps(float * __restrict__ dst,const float filler,std::size_t s
 
     while(size >= 24ULL)
     {
-        _mm_store_ps(p_dst+0, vfiller);
-        _mm_store_ps(p_dst+4, vfiller);
-        _mm_store_ps(p_dst+8, vfiller);
-        _mm_store_ps(p_dst+12,vfiller);
-        _mm_store_ps(p_dst+16,vfiller);
-        _mm_store_ps(p_dst+20,vfiller);
+        _mm_storeu_ps(p_dst+0, vfiller);
+        _mm_storeu_ps(p_dst+4, vfiller);
+        _mm_storeu_ps(p_dst+8, vfiller);
+        _mm_storeu_ps(p_dst+12,vfiller);
+        _mm_storeu_ps(p_dst+16,vfiller);
+        _mm_storeu_ps(p_dst+20,vfiller);
 
         size  -= 24ULL;
         p_dst += 24ULL;
@@ -55,10 +55,10 @@ sse_memset_unroll8x_ps(float * __restrict__ dst,const float filler,std::size_t s
 
     while(size >= 16ULL)
     {
-        _mm_store_ps(p_dst+0, vfiller);
-        _mm_store_ps(p_dst+4, vfiller);
-        _mm_store_ps(p_dst+8, vfiller);
-        _mm_store_ps(p_dst+12,vfiller);
+        _mm_storeu_ps(p_dst+0, vfiller);
+        _mm_storeu_ps(p_dst+4, vfiller);
+        _mm_storeu_ps(p_dst+8, vfiller);
+        _mm_storeu_ps(p_dst+12,vfiller);
 
         size  -= 16ULL;
         p_dst += 16ULL;
@@ -66,8 +66,8 @@ sse_memset_unroll8x_ps(float * __restrict__ dst,const float filler,std::size_t s
 
     while(size >= 8ULL)
     {
-        _mm_store_ps(p_dst+0, vfiller);
-        _mm_store_ps(p_dst+4, vfiller);
+        _mm_storeu_ps(p_dst+0, vfiller);
+        _mm_storeu_ps(p_dst+4, vfiller);
 
         size  -= 8ULL;
         p_dst += 8ULL;
@@ -75,7 +75,7 @@ sse_memset_unroll8x_ps(float * __restrict__ dst,const float filler,std::size_t s
 
     while(size >= 4ULL)
     {
-        _mm_store_ps(p_dst+0, vfiller);
+        _mm_storeu_ps(p_dst+0, vfiller);
 
         size  -= 4ULL;
         p_dst += 4ULL;
@@ -100,119 +100,94 @@ gms::common::
 sse_memset_unroll16x_ps(float * __restrict__ dst,const float filler,std::size_t size)
 {
     const __m128 vfiller{_mm_setr_ps(filler,filler,filler,filler)};
-    float * __restrict__ p_dst{dst};
+    float * __restrict__ p_dst = dst;
+    std::size_t __i;
     const float cfiller{filler};
 
-    while(((uintptr_t)p_dst & 15) && size)
+   
+    for(__i = 0ull; (__i+63ull) < size; __i += 64ull)
     {
-        *p_dst++ = cfiller;
-         --size;
+          _mm_storeu_ps(&p_dst[__i+0ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+4ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+8ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+12ull], vfiller);
+          _mm_storeu_ps(&p_dst[__i+16ull], vfiller);
+          _mm_storeu_ps(&p_dst[__i+20ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+24ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+28ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+32ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+36ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+40ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+44ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+48ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+52ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+56ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+60ULL], vfiller);
     }
 
-    while(size >= 64ULL)
+    for(; (__i+47ull) < size;  __i += 48ull)
     {
-          _mm_store_ps(p_dst+0ULL,  vfiller);
-          _mm_store_ps(p_dst+4ULL,  vfiller);
-          _mm_store_ps(p_dst+8ULL,  vfiller);
-          _mm_store_ps(p_dst+12ULL, vfiller);
-          _mm_store_ps(p_dst+16ULL, vfiller);
-          _mm_store_ps(p_dst+20ULL, vfiller);
-          _mm_store_ps(p_dst+24ULL, vfiller);
-          _mm_store_ps(p_dst+28ULL, vfiller);
-          _mm_store_ps(p_dst+32ULL, vfiller);
-          _mm_store_ps(p_dst+36ULL, vfiller);
-          _mm_store_ps(p_dst+40ULL, vfiller);
-          _mm_store_ps(p_dst+44ULL, vfiller);
-          _mm_store_ps(p_dst+48ULL, vfiller);
-          _mm_store_ps(p_dst+52ULL, vfiller);
-          _mm_store_ps(p_dst+56ULL, vfiller);
-          _mm_store_ps(p_dst+60ULL, vfiller);
-
-          size  -= 64ULL;
-          p_dst += 64ULL;
+          _mm_storeu_ps(&p_dst[__i+0ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+4ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+8ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+12ull], vfiller);
+          _mm_storeu_ps(&p_dst[__i+16ull], vfiller);
+          _mm_storeu_ps(&p_dst[__i+20ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+24ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+28ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+32ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+36ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+40ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+44ULL], vfiller);
     }
 
-    while(size >= 48ULL)
+    for(; (__i+31ull) < size; __i += 32ull)
     {
-          _mm_store_ps(p_dst+0ULL,  vfiller);
-          _mm_store_ps(p_dst+4ULL,  vfiller);
-          _mm_store_ps(p_dst+8ULL,  vfiller);
-          _mm_store_ps(p_dst+12ULL, vfiller);
-          _mm_store_ps(p_dst+16ULL, vfiller);
-          _mm_store_ps(p_dst+20ULL, vfiller);
-          _mm_store_ps(p_dst+24ULL, vfiller);
-          _mm_store_ps(p_dst+28ULL, vfiller);
-          _mm_store_ps(p_dst+32ULL, vfiller);
-          _mm_store_ps(p_dst+36ULL, vfiller);
-          _mm_store_ps(p_dst+40ULL, vfiller);
-          _mm_store_ps(p_dst+44ULL, vfiller);
-
-          size  -= 48ULL;
-          p_dst += 48ULL;
+          _mm_storeu_ps(&p_dst[__i+0ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+4ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+8ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+12ull], vfiller);
+          _mm_storeu_ps(&p_dst[__i+16ull], vfiller);
+          _mm_storeu_ps(&p_dst[__i+20ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+24ULL], vfiller);
+          _mm_storeu_ps(&p_dst[__i+28ULL], vfiller);
     }
 
-    while(size >= 32ULL)
+    for(; (__i+23ull) < size; __i += 24ull)
     {
-          _mm_store_ps(p_dst+0ULL,  vfiller);
-          _mm_store_ps(p_dst+4ULL,  vfiller);
-          _mm_store_ps(p_dst+8ULL,  vfiller);
-          _mm_store_ps(p_dst+12ULL, vfiller);
-          _mm_store_ps(p_dst+16ULL, vfiller);
-          _mm_store_ps(p_dst+20ULL, vfiller);
-          _mm_store_ps(p_dst+24ULL, vfiller);
-          _mm_store_ps(p_dst+28ULL, vfiller);
-
-          size  -= 32ULL;
-          p_dst += 32ULL;
+          _mm_storeu_ps(&p_dst[__i+0ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+4ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+8ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+12ull], vfiller);
+          _mm_storeu_ps(&p_dst[__i+16ull], vfiller);
+          _mm_storeu_ps(&p_dst[__i+20ULL], vfiller);
     }
 
-    while(size >= 24ULL)
+    for(; (__i+15ull) < size; __i += 16ull)
     {
-          _mm_store_ps(p_dst+0ULL,  vfiller);
-          _mm_store_ps(p_dst+4ULL,  vfiller);
-          _mm_store_ps(p_dst+8ULL,  vfiller);
-          _mm_store_ps(p_dst+12ULL, vfiller);
-          _mm_store_ps(p_dst+16ULL, vfiller);
-          _mm_store_ps(p_dst+20ULL, vfiller);
-
-          size  -= 24ULL;
-          p_dst += 24ULL;
+          _mm_storeu_ps(&p_dst[__i+0ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+4ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+8ull],  vfiller);
+          _mm_storeu_ps(&p_dst[__i+12ull], vfiller);
     }
 
-    while(size >= 16ULL)
+    for(; (__i+7ull) < size; __i += 8ull)
     {
-          _mm_store_ps(p_dst+0ULL,  vfiller);
-          _mm_store_ps(p_dst+4ULL,  vfiller);
-          _mm_store_ps(p_dst+8ULL,  vfiller);
-          _mm_store_ps(p_dst+12ULL, vfiller);
-
-          size  -= 16ULL;
-          p_dst += 16ULL;
+           _mm_storeu_ps(&p_dst[__i+0ull],  vfiller);
+           _mm_storeu_ps(&p_dst[__i+4ull],  vfiller);
     }
 
-    while(size >= 8ULL)
+    for(; (__i+3ull) < size; __i += 4ull)
     {
-          _mm_store_ps(p_dst+0ULL,  vfiller);
-          _mm_store_ps(p_dst+4ULL,  vfiller);
-
-          size  -= 8ULL;
-          p_dst += 8ULL;
+           _mm_storeu_ps(&p_dst[__i+0ull],  vfiller);
     }
 
-    while(size >= 4ULL)
+    for(; (__i+0ull) < size; __i += 1ull)
     {
-          _mm_store_ps(p_dst+0ULL,  vfiller);
-
-          size  -= 4ULL;
-          p_dst += 4ULL;
+          p_dst[__i] = cfiller;
     }
 
-    while(size)
-    {
-        *p_dst = cfiller;
-        size--;
-        p_dst++;
-    }
+    
 }
 
 #if defined(__INTEL_COMPILER) || defined(__ICC)
@@ -247,8 +222,9 @@ sse_memset_unroll8x_pd(double * __restrict__ dst,const double filler,std::size_t
         _mm_store_pd(p_dst+12ULL,vfiller);
         _mm_store_pd(p_dst+14ULL,vfiller);
          
-        size  -= 16ULL;
         p_dst += 16ULL;
+        size  -= 16ULL;
+        
     }
 
     while(size >= 12ULL)
@@ -259,9 +235,10 @@ sse_memset_unroll8x_pd(double * __restrict__ dst,const double filler,std::size_t
         _mm_store_pd(p_dst+6ULL, vfiller);
         _mm_store_pd(p_dst+8ULL, vfiller);
         _mm_store_pd(p_dst+10ULL,vfiller);
-
-        size  -= 12ULL;
+        
         p_dst += 12ULL;
+        size  -= 12ULL;
+        
     }
 
     while(size >= 8ULL)
@@ -270,33 +247,37 @@ sse_memset_unroll8x_pd(double * __restrict__ dst,const double filler,std::size_t
         _mm_store_pd(p_dst+2ULL, vfiller);
         _mm_store_pd(p_dst+4ULL, vfiller);
         _mm_store_pd(p_dst+6ULL, vfiller);
-
-        size  -= 8ULL;
+        
         p_dst += 8ULL;
+        size  -= 8ULL;
+       
     }
 
     while(size >= 4ULL)
     {
         _mm_store_pd(p_dst+0ULL, vfiller);
         _mm_store_pd(p_dst+2ULL, vfiller);
-
-        size  -= 4ULL;
+        
         p_dst += 4ULL;
+        size  -= 4ULL;
+        
     }
 
     while(size >= 2ULL)
     {
         _mm_store_pd(p_dst+0ULL, vfiller);
-
-        size  -= 2ULL;
+        
         p_dst += 2ULL;
+        size  -= 2ULL;
+        
     }
 
     while(size)
     {
         *p_dst = cfiller;
-        size--;
-        p_dst++;
+         p_dst++;
+         size--;
+        
     }
 }
 
