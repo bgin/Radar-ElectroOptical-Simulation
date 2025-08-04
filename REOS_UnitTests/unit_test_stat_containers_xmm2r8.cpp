581bb17cd9583bc902e7d361f4850b0c87c7472b
Bernard Gingold <beniekg@gmail.com>
2025-08-04 12:29:36 +0200

    Added: two unit tests (for xmm2c8_t type).

---
+++ REOS_UnitTests/unit_test_stat_containers_xmm2r8.cpp
@@ -0,0 +1,107 @@
+
+
+
+#include <cstdio>
+#include <cstdlib>
+#include "GMS_stat_containers_xmm2r8.h"
+#include "GMS_sse_memset.h"
+#include "GMS_sse_uncached_memcpy.h"
+#include "GMS_sse_memcpy.h"
+#include "GMS_malloc.h"
+
+/*
+   icpc -o unit_test_stat_containers_xmm2r8 -std=c++17 -fp-model fast=2 -fno-exceptions -qopt-zmm-usage=high -ftz -ggdb -ipo -march=skylake-avx512 -mavx512f -falign-functions=32 -w1 -qopt-report=5  \
+   GMS_config.h GMS_malloc.h GMS_sse_memset.h GMS_sse_memset.cpp GMS_sse_memcpy.h GMS_sse_memcpy.cpp GMS_sse_uncached_memcpy.h GMS_sse_uncached_memcpy.cpp GMS_simd_utils.h GMS_complex_xmm2r8.h GMS_stat_containers_xmm2r8.h unit_test_stat_containers_xmm2r8.cpp
+   ASM: 
+   icpc -S -fverbose-asm -masm=intel  -march=skylake-avx512 -mavx512f -fno-exceptions -falign-functions=32 -qopt-zmm-usage=high \ 
+   GMS_config.h GMS_malloc.h GMS_sse_memset.h GMS_sse_memset.cpp GMS_sse_memcpy.h GMS_sse_memcpy.cpp GMS_sse_uncached_memcpy.h GMS_sse_uncached_memcpy.cpp GMS_simd_utils.h GMS_complex_xmm2r8.h GMS_stat_containers_xmm2r8.h unit_test_stat_containers_xmm2r8.cpp
+
+*/
+
+void unit_test_stat_containers_SC1D_xmm2c8_t_nt();
+
+void unit_test_stat_containers_SC1D_xmm2c8_t_nt()
+{
+    using namespace gms;
+    using namespace gms::common;
+
+     
+     
+     constexpr std::size_t size_1024{1024ULL};
+     constexpr std::size_t size_4000{4000ULL};
+     constexpr std::size_t size_65536{65536ull};
+     constexpr double fill{3.14159265358979323846264338328};
+     gms::math::xmm2c8_t data65536_src[size_65536] = {_mm_set1_pd(fill),_mm_set1_pd(fill)};
+     gms::math::xmm2c8_t data4000_src[size_4000]   = {_mm_set1_pd(fill),_mm_set1_pd(fill)};
+     gms::math::xmm2c8_t data1024_src[size_1024]   = {_mm_set1_pd(fill),_mm_set1_pd(fill)};
+     
+     SC1D_xmm2c8_t<size_65536,1> IQ_size65536;
+     SC1D_xmm2c8_t<size_4000,1> IQ_size4000;
+     SC1D_xmm2c8_t<size_1024,1> IQ_size1024;
+     
+             
+     
+     printf("[UNIT-TEST]: %s ---> STARTED\n", __PRETTY_FUNCTION__);
+     printf("[UNIT-TEST--#1]: -- fill buffer of size: %llu with value=%.17f\n",size_65536,fill);
+     IQ_size65536.copy_fill(&data65536_src[0],size_65536);
+
+     printf("[UNIT-TEST--#2]: -- fill buffer of size: %llu with value=%.17f\n",size_4000,fill);
+     IQ_size4000.copy_fill(&data4000_src[0],size_4000);
+
+     printf("[UNIT-TEST--#3]: -- fill buffer of size: %llu with value=%.17f\n",size_1024,fill);
+     IQ_size1024.copy_fill(&data1024_src[0],size_1024);
+       
+     
+     printf("[UNIT-TEST]: %s ---> ENDED CORRECTLY\n", __PRETTY_FUNCTION__);
+}
+
+
+void unit_test_stat_containers_SC1D_xmm2c8_t();
+
+void unit_test_stat_containers_SC1D_xmm2c8_t()
+{
+    using namespace gms;
+    using namespace gms::common;
+
+     
+     
+     constexpr std::size_t size_1024{1024ULL};
+     constexpr std::size_t size_4000{4000ULL};
+     constexpr std::size_t size_65536{65536ull};
+     constexpr double fill{3.14159265358979323846264338328};
+     gms::math::xmm2c8_t data65536_src[size_65536] = {_mm_set1_pd(fill), _mm_set1_pd(fill)};
+     gms::math::xmm2c8_t data4000_src[size_4000]   = {_mm_set1_pd(fill), _mm_set1_pd(fill)};
+     gms::math::xmm2c8_t data1024_src[size_1024]   = {_mm_set1_pd(fill), _mm_set1_pd(fill)};
+     
+     SC1D_xmm2c8_t<size_65536,0> IQ_size65536;
+     SC1D_xmm2c8_t<size_4000,0> IQ_size4000;
+     SC1D_xmm2c8_t<size_1024,0> IQ_size1024;
+     
+       
+     
+     printf("[UNIT-TEST]: %s ---> STARTED\n", __PRETTY_FUNCTION__);
+     printf("[UNIT-TEST--#1]: -- fill buffer of size: %llu with value=%.17f\n",size_65536,fill);
+     IQ_size65536.copy_fill(&data65536_src[0],size_65536);
+
+     printf("[UNIT-TEST--#2]: -- fill buffer of size: %llu with value=%.17f\n",size_4000,fill);
+    
+     IQ_size4000.copy_fill(&data4000_src[0],size_4000);
+
+     printf("[UNIT-TEST--#3]: -- fill buffer of size: %llu with value=%.17f\n",size_1024,fill);
+     IQ_size1024.copy_fill(&data1024_src[0],size_1024);
+     
+       
+     printf("[UNIT-TEST]: %s ---> ENDED CORRECTLY\n", __PRETTY_FUNCTION__);
+}
+
+
+
+
+int main()
+{
+    
+    unit_test_stat_containers_SC1D_xmm2c8_t_nt();
+    unit_test_stat_containers_SC1D_xmm2c8_t();
+   
+    return 0;
+}
