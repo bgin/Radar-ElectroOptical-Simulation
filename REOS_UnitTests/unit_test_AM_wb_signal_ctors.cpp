9c0973f05af6ccf17e94dee1ed84af6115b561c8
Bernard Gingold <beniekg@gmail.com>
2025-09-03 13:41:34 +0200

    Added: two unit tests.

--- REOS_UnitTests/unit_test_AM_wb_signal_ctors.cpp
+++ REOS_UnitTests/unit_test_AM_wb_signal_ctors.cpp
@@ -16,9 +16,9 @@
 
 __attribute__((hot))
 __attribute__((noinline))
-void unit_test_AM_wideband_signal_construct();
+void unit_test_AM_wideband_signal_ctor_2();
 
-void unit_test_AM_wideband_signal_construct()
+void unit_test_AM_wideband_signal_ctor_2()
 {
      using namespace gms::radiolocation;
      constexpr std::size_t T{128ull};
@@ -33,15 +33,14 @@ void unit_test_AM_wideband_signal_construct()
      float fc{3.1256e+9f};
      const bool sym_dep{false};
      const bool layout{true};
-     int32_t order;
-     if(layout) order = 1;
-     else order = 2;
+     const bool set_order{true};
          
      int32_t status{};
      //__asm__ ("int3");
+     printf("[UNIT_TEST]: function=%s -- **START**\n", __PRETTY_FUNCTION__);
      AM_wb_signal_t __symbol_id_1__ = AM_wb_signal_t(Ac,A0,baude_rate,N,T,
                                                      nfreqs,nomegs,nthets,
-                                                     id,order,fc,sym_dep);
+                                                     set_order,id,fc,sym_dep);
 
      char * ctor_name{gms::common::demangle(typeid(__symbol_id_1__).name(),status)};
      if(status==0 && ctor_name != NULL)
@@ -73,11 +72,151 @@ void unit_test_AM_wideband_signal_construct()
      __symbol_id_1__.m_signal_i.info_size_alignment();
      __symbol_id_1__.m_signal_q.info_size_alignment();
     std::printf("[UNIT-TEST]: Finished a dump of array members.\n");
+    printf("[UNIT_TEST]: function=%s -- **END**\n", __PRETTY_FUNCTION__);
 
 }
 
+
+__attribute__((hot))
+__attribute__((noinline))
+void unit_test_AM_wideband_signal_ctor_3();
+
+void unit_test_AM_wideband_signal_ctor_3()
+{
+     using namespace gms::radiolocation;
+     constexpr std::size_t T{128ull};
+     constexpr std::size_t N{128ull};
+     constexpr std::size_t baude_rate{N};
+     constexpr std::size_t nfreqs{512ull};
+     constexpr std::size_t nomegs{128ull};
+     constexpr std::size_t nthets{128ull};
+     AM_wb_signal_darray_params_t dp;
+     std::complex<float> Ac{1.0f,0.0f};
+     std::complex<float> A0{1.25f,1.25f};
+     const int32_t id{1};
+     float fc{3.1256e+9f};
+     const bool sym_dep{false};
+     const bool layout{true};
+     const bool set_order{true};
+     dp.prot   = PROT_READ | PROT_WRITE;
+     dp.flags  = MAP_ANONYMOUS | MAP_PRIVATE;
+     dp.fd     = -1;
+     dp.offset = 0;
+     dp.fsize  = 0;
+     int32_t status{};
+     //__asm__ ("int3");
+     printf("[UNIT_TEST]: function=%s -- **START**\n", __PRETTY_FUNCTION__);
+     AM_wb_signal_t __symbol_id_1__ = AM_wb_signal_t(Ac,A0,dp,baude_rate,N,T,
+                                                     nfreqs,nomegs,nthets,
+                                                     set_order,id,fc,sym_dep);
+
+     char * ctor_name{gms::common::demangle(typeid(__symbol_id_1__).name(),status)};
+     if(status==0 && ctor_name != NULL)
+     {
+          printf("[UNIT-TEST]: Instantiation of object Constructor of type: %s\n\n", ctor_name);
+          gms::common::gms_mm_free(ctor_name);
+     }
+     else
+     {
+          printf("[UNIT-TEST]: Instantiation of object Constructor of type: %s\n\n", typeid(__symbol_id_1__).name());
+          if(ctor_name != NULL) gms::common::gms_mm_free(ctor_name);
+     }
+     std::printf("[UNIT-TEST:] -- Dumping an array members info and alignment.\n");
+     __symbol_id_1__.m_ambig.info_size_alignment();
+     __symbol_id_1__.m_carrier.info_size_alignment();
+     __symbol_id_1__.m_carrier_i.info_size_alignment();
+     __symbol_id_1__.m_carrier_q.info_size_alignment();
+     __symbol_id_1__.m_cenv.info_size_alignment();
+     __symbol_id_1__.m_cenv_corr.info_size_alignment();
+     __symbol_id_1__.m_cenv_corr_i.info_size_alignment();
+     __symbol_id_1__.m_cenv_corr_q.info_size_alignment();
+     __symbol_id_1__.m_cenv_i.info_size_alignment();
+     __symbol_id_1__.m_cenv_q.info_size_alignment();
+     __symbol_id_1__.m_cenv_spec.info_size_alignment();
+     __symbol_id_1__.m_code_seq.info_size_alignment();
+     __symbol_id_1__.m_mod_ambig.info_size_alignment();
+     __symbol_id_1__.m_sig_samp.info_size_alignment();
+     __symbol_id_1__.m_signal.info_size_alignment();
+     __symbol_id_1__.m_signal_i.info_size_alignment();
+     __symbol_id_1__.m_signal_q.info_size_alignment();
+    std::printf("[UNIT-TEST]: Finished a dump of array members.\n");
+    printf("[UNIT_TEST]: function=%s -- **END**\n", __PRETTY_FUNCTION__);
+}
+
+__attribute__((hot))
+__attribute__((noinline))
+void unit_test_AM_wideband_signal_move_ctor();
+
+void unit_test_AM_wideband_signal_move_ctor()
+{
+     using namespace gms::radiolocation;
+     constexpr std::size_t T{128ull};
+     constexpr std::size_t N{128ull};
+     constexpr std::size_t baude_rate{N};
+     constexpr std::size_t nfreqs{512ull};
+     constexpr std::size_t nomegs{128ull};
+     constexpr std::size_t nthets{128ull};
+     AM_wb_signal_darray_params_t dp;
+     std::complex<float> Ac{1.0f,0.0f};
+     std::complex<float> A0{1.25f,1.25f};
+     const int32_t id{1};
+     float fc{3.1256e+9f};
+     const bool sym_dep{false};
+     const bool layout{true};
+     const bool set_order{true};
+     dp.prot   = PROT_READ | PROT_WRITE;
+     dp.flags  = MAP_ANONYMOUS | MAP_PRIVATE;
+     dp.fd     = -1;
+     dp.offset = 0;
+     dp.fsize  = 0;
+     int32_t status{};
+     //__asm__ ("int3");
+     printf("[UNIT_TEST]: function=%s -- **START**\n", __PRETTY_FUNCTION__);
+     AM_wb_signal_t __symbol_id_1__ = AM_wb_signal_t(Ac,A0,dp,baude_rate,N,T,
+                                                     nfreqs,nomegs,nthets,
+                                                     set_order,id,fc,sym_dep);
+
+     AM_wb_signal_t __symbol_id_1_cpy__{std::move(__symbol_id_1__)};
+
+     char * ctor_name{gms::common::demangle(typeid(__symbol_id_1_cpy__).name(),status)};
+     if(status==0 && ctor_name != NULL)
+     {
+          printf("[UNIT-TEST]: Instantiation of object Constructor of type: %s\n\n", ctor_name);
+          gms::common::gms_mm_free(ctor_name);
+     }
+     else
+     {
+          printf("[UNIT-TEST]: Instantiation of object Constructor of type: %s\n\n", typeid(__symbol_id_1_cpy__).name());
+          if(ctor_name != NULL) gms::common::gms_mm_free(ctor_name);
+     }
+     std::printf("[UNIT-TEST:] -- Dumping an array members info and alignment.\n");
+     __symbol_id_1_cpy__.m_ambig.info_size_alignment();
+     __symbol_id_1_cpy__.m_carrier.info_size_alignment();
+     __symbol_id_1_cpy__.m_carrier_i.info_size_alignment();
+     __symbol_id_1_cpy__.m_carrier_q.info_size_alignment();
+     __symbol_id_1_cpy__.m_cenv.info_size_alignment();
+     __symbol_id_1_cpy__.m_cenv_corr.info_size_alignment();
+     __symbol_id_1_cpy__.m_cenv_corr_i.info_size_alignment();
+     __symbol_id_1_cpy__.m_cenv_corr_q.info_size_alignment();
+     __symbol_id_1_cpy__.m_cenv_i.info_size_alignment();
+     __symbol_id_1_cpy__.m_cenv_q.info_size_alignment();
+     __symbol_id_1_cpy__.m_cenv_spec.info_size_alignment();
+     __symbol_id_1_cpy__.m_code_seq.info_size_alignment();
+     __symbol_id_1_cpy__.m_mod_ambig.info_size_alignment();
+     __symbol_id_1_cpy__.m_sig_samp.info_size_alignment();
+     __symbol_id_1_cpy__.m_signal.info_size_alignment();
+     __symbol_id_1_cpy__.m_signal_i.info_size_alignment();
+     __symbol_id_1_cpy__.m_signal_q.info_size_alignment();
+    std::printf("[UNIT-TEST]: Finished a dump of array members.\n");
+    printf("[UNIT_TEST]: function=%s -- **END**\n", __PRETTY_FUNCTION__);
+}
+
+
+
 int main()
 {
-    unit_test_AM_wideband_signal_construct();
+    //unit_test_AM_wideband_signal_ctor_2();
+    //unit_test_AM_wideband_signal_ctor_3();
+    unit_test_AM_wideband_signal_move_ctor();
     return 0;
 }
---
+++ REOS_UnitTests/unit_test_AM_wb_signal_operators.cpp
@@ -0,0 +1,104 @@
+
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <functional>
+#include "GMS_AM_wideband_signal.h"
+
+/*
+   icpc -o unit_test_AM_wb_signal_operators -fp-model -std=c++17 fast=2 -ftz -ggdb -ipo -march=skylake-avx512 -mavx512f -falign-functions=32 -w1 -qopt-report=5  \
+   GMS_config.h GMS_malloc.h GMS_fast_pmc_access.h  GMS_dyn_array.h GMS_AM_wideband_signal.h GMS_AM_wideband_signal.cpp unit_test_AM_wb_signal_operators.cpp
+   ASM: 
+   icpc -S -fverbose-asm -masm=intel  -std=c++17 -march=skylake-avx512 -mavx512f -falign-functions=32 GMS_config.h GMS_malloc.h GMS_fast_pmc_access.h  GMS_dyn_array.h GMS_AM_wideband_signal.h GMS_AM_wideband_signal.cpp unit_test_AM_wb_signal_operators.cpp
+
+*/
+
+__attribute__((hot))
+__attribute__((noinline))
+void unit_test_AM_wideband_signal_move_op();
+
+void unit_test_AM_wideband_signal_move_op()
+{
+     using namespace gms::radiolocation;
+     constexpr std::size_t T{128ull};
+     constexpr std::size_t N{128ull};
+     constexpr std::size_t baude_rate{N};
+     constexpr std::size_t nfreqs{512ull};
+     constexpr std::size_t nomegs{128ull};
+     constexpr std::size_t nthets{128ull};
+     std::complex<float> Ac{1.0f,0.0f};
+     std::complex<float> A0{1.25f,1.25f};
+     const int32_t id{1};
+     float fc{3.1256e+9f};
+     const bool sym_dep{false};
+     const bool layout{true};
+     const bool set_order{true};
+     
+     constexpr std::size_t T2{128ull};
+     constexpr std::size_t N2{32ull};
+     constexpr std::size_t baude_rate2{N};
+     constexpr std::size_t nfreqs2{512ull};
+     constexpr std::size_t nomegs2{128ull};
+     constexpr std::size_t nthets2{512ull};
+     std::complex<float> Ac2{0.5f,0.5f};
+     std::complex<float> A02{1.5f,1.25f};
+     const int32_t id2{2};
+     float fc2{3.0456e+9f};
+     const bool sym_dep2{false};
+     const bool layout2{true};
+     const bool set_order2{true};    
+     int32_t status{};
+     //__asm__ ("int3");
+     printf("[UNIT_TEST]: function=%s -- **START**\n", __PRETTY_FUNCTION__);
+     AM_wb_signal_t __symbol_id_1__ = AM_wb_signal_t(Ac,A0,baude_rate,N,T,
+                                                     nfreqs,nomegs,nthets,
+                                                     set_order,id,fc,sym_dep);
+
+     AM_wb_signal_t __symbol_id_2__ = AM_wb_signal_t(Ac2,A02,baude_rate2,N2,T2,
+                                                     nfreqs2,nomegs2,nthets2,
+                                                     set_order2,id2,fc2,sym_dep2);
+     
+     
+     __symbol_id_2__.operator=(std::move(__symbol_id_1__));
+
+     char * ctor_name{gms::common::demangle(typeid(__symbol_id_2__).name(),status)};
+     if(status==0 && ctor_name != NULL)
+     {
+          printf("[UNIT-TEST]: Instantiation of object Constructor of type: %s\n\n", ctor_name);
+          gms::common::gms_mm_free(ctor_name);
+     }
+     else
+     {
+          printf("[UNIT-TEST]: Instantiation of object Constructor of type: %s\n\n", typeid(__symbol_id_2__).name());
+          if(ctor_name != NULL) gms::common::gms_mm_free(ctor_name);
+     }
+     std::printf("[UNIT-TEST:] -- Dumping an array members info and alignment.\n");
+     __symbol_id_2__.m_ambig.info_size_alignment();
+     __symbol_id_2__.m_carrier.info_size_alignment();
+     __symbol_id_2__.m_carrier_i.info_size_alignment();
+     __symbol_id_2__.m_carrier_q.info_size_alignment();
+     __symbol_id_2__.m_cenv.info_size_alignment();
+     __symbol_id_2__.m_cenv_corr.info_size_alignment();
+     __symbol_id_2__.m_cenv_corr_i.info_size_alignment();
+     __symbol_id_2__.m_cenv_corr_q.info_size_alignment();
+     __symbol_id_2__.m_cenv_i.info_size_alignment();
+     __symbol_id_2__.m_cenv_q.info_size_alignment();
+     __symbol_id_2__.m_cenv_spec.info_size_alignment();
+     __symbol_id_2__.m_code_seq.info_size_alignment();
+     __symbol_id_2__.m_mod_ambig.info_size_alignment();
+     __symbol_id_2__.m_sig_samp.info_size_alignment();
+     __symbol_id_2__.m_signal.info_size_alignment();
+     __symbol_id_2__.m_signal_i.info_size_alignment();
+     __symbol_id_2__.m_signal_q.info_size_alignment();
+    std::printf("[UNIT-TEST]: Finished a dump of array members.\n");
+    printf("[UNIT_TEST]: function=%s -- **END**\n", __PRETTY_FUNCTION__);
+
+}
+
+
+int main()
+{
+    unit_test_AM_wideband_signal_move_op();
+    return 0;
+}
