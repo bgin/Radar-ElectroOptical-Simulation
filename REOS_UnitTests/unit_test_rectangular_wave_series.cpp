a9f9b8ba0576186c99c328bf366344e2b448bea5
Bernard Gingold <beniekg@gmail.com>
2025-09-18 15:35:40 +0200

    Added: rectangular waveform test.

---
+++ REOS_UnitTests/unit_test_rectangular_wave_series.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <functional>
+#include "GMS_rectangular_waveform.h"
+
+/*
+   icpc -o unit_test_rect_wave_series -fp-model -std=c++17 fast=2 -ftz -ggdb -ipo -march=skylake-avx512 -mavx512f -falign-functions=32 -w1 -qopt-report=5  \
+   GMS_config.h GMS_malloc.h GMS_fast_pmc_access.h  GMS_dyn_array.h GMS_sse_memset.h GMS_sse_memset.cpp GMS_rectangular_waveform.h GMS_rectangular_waveform.cpp unit_test_rectangular_wave_series.cpp
+   ASM: 
+   icpc -S -fverbose-asm -masm=intel  -std=c++17 -march=skylake-avx512 -mavx512f -falign-functions=32 GMS_config.h GMS_malloc.h GMS_fast_pmc_access.h  GMS_dyn_array.h GMS_sse_memset.h GMS_sse_memset.cpp GMS_rectangular_waveform.h GMS_rectangular_waveform.cpp unit_test_rectangular_wave_series.cpp
+
+*/
+
+__attribute__((hot))
+__attribute__((noinline))
+void unit_test_rectangw_series();
+
+void unit_test_rectangw_series()
+{
+     using namespace gms::radiolocation;
+     constexpr std::size_t   n_samples{1024ull};
+     constexpr std::uint32_t n_waves{32ull};
+     constexpr float T{256.0f};
+     constexpr float A{1.0f};
+     constexpr float rho{8.0f};
+     int32_t status{};
+     //__asm__ ("int3");
+     printf("[UNIT_TEST]: function=%s -- **START**\n", __PRETTY_FUNCTION__);
+     rectangular_waveform_t __rectw_1__  = rectangular_waveform_t(n_samples,n_waves,
+                                                      T,rho,A);
+     char * ctor_name{gms::common::demangle(typeid(__rectw_1__).name(),status)};
+     if(status==0 && ctor_name != NULL)
+     {
+          printf("[UNIT-TEST]: Instantiation of object Constructor of type: %s\n\n", ctor_name);
+          gms::common::gms_mm_free(ctor_name);
+     }
+     else
+     {
+          printf("[UNIT-TEST]: Instantiation of object Constructor of type: %s\n\n", typeid(__rectw_1__).name());
+          if(ctor_name != NULL) gms::common::gms_mm_free(ctor_name);
+     }   
+     std::printf("[UNIT-TEST:] -- Dumping an array members info and alignment.\n");
+     __rectw_1__.__rw_samples__.info_size_alignment();
+     std::printf("[UNIT-TEST:] -- Creating single trapezoid waveform.\n");
+     __rectw_1__.fourier_series_expansion();
+     std::printf("[UNIT-TEST:] -- Creating gnuplot plotting command file.\n");
+     rectangular_waveform_t::create_signal_plot(__rectw_1__.__n_samples__,__rectw_1__.__rw_samples__.m_data,nullptr,
+                                              "rectangular_wave_expanded_test_4_","Rectangular_Waveform_Expansion",false);
+     printf("[UNIT_TEST]: function=%s -- **END**\n", __PRETTY_FUNCTION__);
+}
+
+int main()
+{
+     unit_test_rectangw_series();
+     return 0;
+}
